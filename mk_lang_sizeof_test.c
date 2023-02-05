9b8c81f0409c8ba9e128b883210438fe120d25b5
Marek Knápek <knapek.mar@gmail.com>
2023-02-05 07:13:35 +0100

    Add mk_lang_sizeof_test.

---
+++ mk_lang_sizeof_test.c
@@ -0,0 +1,29 @@
+#include "mk_lang_sizeof_test.h"
+
+#include "mk_lang_bi.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_sizeof.h"
+#include "mk_lang_static_assert.h"
+
+
+mk_lang_jumbo void mk_lang_sizeof_test(void)
+{
+	mk_lang_static_assert(mk_lang_sizeof_bi_pchar_t == sizeof(mk_lang_bi_pchar_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_uchar_t == sizeof(mk_lang_bi_uchar_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_schar_t == sizeof(mk_lang_bi_schar_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_ushort_t == sizeof(mk_lang_bi_ushort_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_sshort_t == sizeof(mk_lang_bi_sshort_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_uint_t == sizeof(mk_lang_bi_uint_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_sint_t == sizeof(mk_lang_bi_sint_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_ulong_t == sizeof(mk_lang_bi_ulong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_slong_t == sizeof(mk_lang_bi_slong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_ullong_t == sizeof(mk_lang_bi_ullong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_sllong_t == sizeof(mk_lang_bi_sllong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_ulllong_t == sizeof(mk_lang_bi_ulllong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_slllong_t == sizeof(mk_lang_bi_slllong_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_uintptr_t == sizeof(mk_lang_bi_uintptr_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_sintptr_t == sizeof(mk_lang_bi_sintptr_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_uintmax_t == sizeof(mk_lang_bi_uintmax_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_sintmax_t == sizeof(mk_lang_bi_sintmax_t));
+	mk_lang_static_assert(mk_lang_sizeof_bi_size_t == sizeof(mk_lang_bi_size_t));
+}
---
+++ mk_lang_sizeof_test.h
@@ -0,0 +1,11 @@
+#ifndef mk_include_guard_lang_sizeof_test
+#define mk_include_guard_lang_sizeof_test
+
+
+#include "mk_lang_jumbo.h"
+
+
+mk_lang_jumbo void mk_lang_sizeof_test(void);
+
+
+#endif
