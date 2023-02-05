8930902b736a6d4856e3b47d060afd10508927a9
Marek Knápek <knapek.mar@gmail.com>
2023-02-05 07:01:21 +0100

    Add mk_lang_bi_info_print_sizes.

---
+++ mk_lang_bi_info.c
@@ -0,0 +1,29 @@
+#include "mk_lang_bi_info.h"
+
+#include "mk_lang_bi.h"
+#include "mk_lang_jumbo.h"
+
+#include <stdio.h> /* printf */
+
+
+mk_lang_jumbo void mk_lang_bi_info_print_sizes(void)
+{
+	printf("sizeof(mk_lang_bi_pchar_t) == %d\n", ((int)(sizeof(mk_lang_bi_pchar_t))));
+	printf("sizeof(mk_lang_bi_uchar_t) == %d\n", ((int)(sizeof(mk_lang_bi_uchar_t))));
+	printf("sizeof(mk_lang_bi_schar_t) == %d\n", ((int)(sizeof(mk_lang_bi_schar_t))));
+	printf("sizeof(mk_lang_bi_ushort_t) == %d\n", ((int)(sizeof(mk_lang_bi_ushort_t))));
+	printf("sizeof(mk_lang_bi_sshort_t) == %d\n", ((int)(sizeof(mk_lang_bi_sshort_t))));
+	printf("sizeof(mk_lang_bi_uint_t) == %d\n", ((int)(sizeof(mk_lang_bi_uint_t))));
+	printf("sizeof(mk_lang_bi_sint_t) == %d\n", ((int)(sizeof(mk_lang_bi_sint_t))));
+	printf("sizeof(mk_lang_bi_ulong_t) == %d\n", ((int)(sizeof(mk_lang_bi_ulong_t))));
+	printf("sizeof(mk_lang_bi_slong_t) == %d\n", ((int)(sizeof(mk_lang_bi_slong_t))));
+	printf("sizeof(mk_lang_bi_ullong_t) == %d\n", ((int)(sizeof(mk_lang_bi_ullong_t))));
+	printf("sizeof(mk_lang_bi_sllong_t) == %d\n", ((int)(sizeof(mk_lang_bi_sllong_t))));
+	printf("sizeof(mk_lang_bi_ulllong_t) == %d\n", ((int)(sizeof(mk_lang_bi_ulllong_t))));
+	printf("sizeof(mk_lang_bi_slllong_t) == %d\n", ((int)(sizeof(mk_lang_bi_slllong_t))));
+	printf("sizeof(mk_lang_bi_uintptr_t) == %d\n", ((int)(sizeof(mk_lang_bi_uintptr_t))));
+	printf("sizeof(mk_lang_bi_sintptr_t) == %d\n", ((int)(sizeof(mk_lang_bi_sintptr_t))));
+	printf("sizeof(mk_lang_bi_uintmax_t) == %d\n", ((int)(sizeof(mk_lang_bi_uintmax_t))));
+	printf("sizeof(mk_lang_bi_sintmax_t) == %d\n", ((int)(sizeof(mk_lang_bi_sintmax_t))));
+	printf("sizeof(mk_lang_bi_size_t) == %d\n", ((int)(sizeof(mk_lang_bi_size_t))));
+}
---
+++ mk_lang_bi_info.h
@@ -0,0 +1,11 @@
+#ifndef mk_include_guard_lang_bi_info
+#define mk_include_guard_lang_bi_info
+
+
+#include "mk_lang_jumbo.h"
+
+
+mk_lang_jumbo void mk_lang_bi_info_print_sizes(void);
+
+
+#endif
