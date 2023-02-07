fcfe4c59dd3f811af4ed8e271ffc9e7cf98c7943
Marek Knápek <knapek.mar@gmail.com>
2023-02-07 22:27:08 +0100

    Swap __WATCOM_CPLUSPLUS__ and __WATCOMC__.

--- mk_lang_llong.h
+++ mk_lang_llong.h
@@ -20,13 +20,13 @@
 #define mk_lang_sllong_t signed __int64
 #define mk_lang_ullong_c(x) x ## ui64
 #define mk_lang_sllong_c(x) x ## i64
-#elif defined __WATCOMC__ && __WATCOMC__ >= 1100
+#elif defined __WATCOM_CPLUSPLUS__ && __WATCOM_CPLUSPLUS__ >= 1100
 #define mk_lang_llong_has 1
 #define mk_lang_ullong_t unsigned __int64
 #define mk_lang_sllong_t signed __int64
 #define mk_lang_ullong_c(x) x ## ui64
 #define mk_lang_sllong_c(x) x ## i64
-#elif defined __WATCOM_CPLUSPLUS__ && __WATCOM_CPLUSPLUS__ >= 1100
+#elif defined __WATCOMC__ && __WATCOMC__ >= 1100
 #define mk_lang_llong_has 1
 #define mk_lang_ullong_t unsigned __int64
 #define mk_lang_sllong_t signed __int64
