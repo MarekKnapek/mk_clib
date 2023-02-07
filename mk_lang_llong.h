9f3c496269a6b9b735c489327b92265474dc29bb
Marek Knápek <knapek.mar@gmail.com>
2023-02-07 22:28:37 +0100

    Open Watcom 2 supports long long int.

--- mk_lang_llong.h
+++ mk_lang_llong.h
@@ -20,6 +20,18 @@
 #define mk_lang_sllong_t signed __int64
 #define mk_lang_ullong_c(x) x ## ui64
 #define mk_lang_sllong_c(x) x ## i64
+#elif defined __WATCOM_CPLUSPLUS__ && __WATCOM_CPLUSPLUS__ >= 1300
+#define mk_lang_llong_has 1
+#define mk_lang_ullong_t unsigned long long int
+#define mk_lang_sllong_t signed long long int
+#define mk_lang_ullong_c(x) x ## ull
+#define mk_lang_sllong_c(x) x ## ll
+#elif defined __WATCOMC__ && __WATCOMC__ >= 1300
+#define mk_lang_llong_has 1
+#define mk_lang_ullong_t unsigned long long int
+#define mk_lang_sllong_t signed long long int
+#define mk_lang_ullong_c(x) x ## ull
+#define mk_lang_sllong_c(x) x ## ll
 #elif defined __WATCOM_CPLUSPLUS__ && __WATCOM_CPLUSPLUS__ >= 1100
 #define mk_lang_llong_has 1
 #define mk_lang_ullong_t unsigned __int64
