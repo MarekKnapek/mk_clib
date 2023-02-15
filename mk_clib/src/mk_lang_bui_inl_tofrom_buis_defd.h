1c4d80e6b99cefe43fd6ad7180bb6163679e2fbf
Marek Knápek <knapek.mar@gmail.com>
2023-02-15 18:55:24 +0100

    Fix bui endian.

--- mk_clib/src/mk_lang_bui_inl_tofrom_buis_defd.h
+++ mk_clib/src/mk_lang_bui_inl_tofrom_buis_defd.h
@@ -1,3 +1,4 @@
+#include "mk_lang_assert.h"
 #include "mk_lang_bi.h"
 #include "mk_lang_concat.h"
 #include "mk_lang_div_roundup.h"
@@ -8,8 +9,4 @@
 #define mk_lang_bui_inl_tofrom_buis_defd_fn_from mk_lang_concat(mk_lang_concat(mk_lang_concat(mk_lang_concat(mk_lang_concat(mk_lang_bui_, mk_lang_bui_inl_defd_name), _from_buis_), mk_lang_forb_bui_tn), _), mk_lang_forb_endian_name)
 #define mk_lang_bui_inl_tofrom_buis_defd_sizeof_bui_t mk_lang_concat(mk_lang_concat(mk_lang_sizeof_bi_, mk_lang_forb_bui_tn), _t)
 #define mk_lang_bui_inl_tofrom_buis_defd_count mk_lang_div_roundup(mk_lang_bui_inl_defd_sizeof, mk_lang_bui_inl_tofrom_buis_defd_sizeof_bui_t)
-#if mk_lang_forb_endian_value == mk_lang_endian_little
-#define mk_lang_bui_inl_tofrom_buis_defd_idx(x) (x)
-#else mk_lang_forb_endian_value == mk_lang_endian_big
-#define mk_lang_bui_inl_tofrom_buis_defd_idx(x) (x)
-#endif
+#define mk_lang_bui_inl_tofrom_buis_defd_idx(x) ((mk_lang_forb_endian_value == mk_lang_endian_little) ? (x) : ((mk_lang_forb_endian_value == mk_lang_endian_big) ? (((mk_lang_bui_inl_tofrom_buis_defd_count) - 1) - (x)) : (mk_lang_assert(0), 0)))
