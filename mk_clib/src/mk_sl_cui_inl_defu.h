266bc23b2a1b8e2e6f6f935493bc3a31d409c73d
Marek Knápek <knapek.mar@gmail.com>
2023-02-16 20:50:54 +0100

    mk_sl_cui part 1

---
+++ mk_clib/src/mk_sl_cui_inl_defu.h
@@ -0,0 +1,137 @@
+#undef mk_sl_cui_inl_defd_name
+#undef mk_sl_cui_inl_defd_base_type
+#undef mk_sl_cui_inl_defd_base_name
+#undef mk_sl_cui_inl_defd_base_bits
+#undef mk_sl_cui_inl_defd_count
+#undef mk_sl_cui_inl_defd_endian
+
+#undef mk_sl_cui_inl_defd_base_set_zero
+#undef mk_sl_cui_inl_defd_base_set_max
+#undef mk_sl_cui_inl_defd_base_set_one
+#undef mk_sl_cui_inl_defd_base_set_bit
+#undef mk_sl_cui_inl_defd_base_set_mask
+#undef mk_sl_cui_inl_defd_base_is_zero
+#undef mk_sl_cui_inl_defd_base_is_max
+#undef mk_sl_cui_inl_defd_base_eq
+#undef mk_sl_cui_inl_defd_base_ne
+#undef mk_sl_cui_inl_defd_base_lt
+#undef mk_sl_cui_inl_defd_base_le
+#undef mk_sl_cui_inl_defd_base_gt
+#undef mk_sl_cui_inl_defd_base_ge
+#undef mk_sl_cui_inl_defd_base_inc2
+#undef mk_sl_cui_inl_defd_base_dec2
+#undef mk_sl_cui_inl_defd_base_inc1
+#undef mk_sl_cui_inl_defd_base_dec1
+#undef mk_sl_cui_inl_defd_base_not2
+#undef mk_sl_cui_inl_defd_base_or3
+#undef mk_sl_cui_inl_defd_base_and3
+#undef mk_sl_cui_inl_defd_base_xor3
+#undef mk_sl_cui_inl_defd_base_not1
+#undef mk_sl_cui_inl_defd_base_or2
+#undef mk_sl_cui_inl_defd_base_and2
+#undef mk_sl_cui_inl_defd_base_xor2
+#undef mk_sl_cui_inl_defd_base_shl3
+#undef mk_sl_cui_inl_defd_base_shr3
+#undef mk_sl_cui_inl_defd_base_rotl3
+#undef mk_sl_cui_inl_defd_base_rotr3
+#undef mk_sl_cui_inl_defd_base_shl2
+#undef mk_sl_cui_inl_defd_base_shr2
+#undef mk_sl_cui_inl_defd_base_rotl2
+#undef mk_sl_cui_inl_defd_base_rotr2
+#undef mk_sl_cui_inl_defd_base_add3_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_base_add3_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_base_add3_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_base_add3_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_base_add2_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_base_add2_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_base_add2_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_base_add2_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_base_sub3_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_base_sub3_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_base_sub3_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_base_sub3_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_base_sub2_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_base_sub2_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_base_sub2_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_base_sub2_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_base_mul3_wrap_lo
+#undef mk_sl_cui_inl_defd_base_mul3_wrap_hi
+#undef mk_sl_cui_inl_defd_base_mul4_wrap_wi
+#undef mk_sl_cui_inl_defd_base_mul2_wrap_lo
+#undef mk_sl_cui_inl_defd_base_mul2_wrap_hi
+#undef mk_sl_cui_inl_defd_base_mul2_wrap_wi
+
+#undef mk_sl_cui_inl_defd_s
+#undef mk_sl_cui_inl_defd_t
+
+#undef mk_sl_cui_inl_defd_idx
+
+#undef mk_sl_cui_inl_defd_set_zero
+#undef mk_sl_cui_inl_defd_set_max
+#undef mk_sl_cui_inl_defd_set_one
+#undef mk_sl_cui_inl_defd_set_bit
+#undef mk_sl_cui_inl_defd_set_mask
+
+#undef mk_sl_cui_inl_defd_is_zero
+#undef mk_sl_cui_inl_defd_is_max
+#undef mk_sl_cui_inl_defd_eq
+#undef mk_sl_cui_inl_defd_ne
+#undef mk_sl_cui_inl_defd_lt
+#undef mk_sl_cui_inl_defd_le
+#undef mk_sl_cui_inl_defd_gt
+#undef mk_sl_cui_inl_defd_ge
+
+#undef mk_sl_cui_inl_defd_inc2
+#undef mk_sl_cui_inl_defd_dec2
+#undef mk_sl_cui_inl_defd_inc1
+#undef mk_sl_cui_inl_defd_dec1
+
+#undef mk_sl_cui_inl_defd_not2
+#undef mk_sl_cui_inl_defd_or3
+#undef mk_sl_cui_inl_defd_and3
+#undef mk_sl_cui_inl_defd_xor3
+#undef mk_sl_cui_inl_defd_not1
+#undef mk_sl_cui_inl_defd_or2
+#undef mk_sl_cui_inl_defd_and2
+#undef mk_sl_cui_inl_defd_xor2
+
+#undef mk_sl_cui_inl_defd_shl3
+#undef mk_sl_cui_inl_defd_shr3
+#undef mk_sl_cui_inl_defd_rotl3
+#undef mk_sl_cui_inl_defd_rotr3
+#undef mk_sl_cui_inl_defd_shl2
+#undef mk_sl_cui_inl_defd_shr2
+#undef mk_sl_cui_inl_defd_rotl2
+#undef mk_sl_cui_inl_defd_rotr2
+
+#undef mk_sl_cui_inl_defd_add3_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_add3_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_add3_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_add3_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_add2_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_add2_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_add2_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_add2_wrap_cie_coe
+
+#undef mk_sl_cui_inl_defd_sub3_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_sub3_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_sub3_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_sub3_wrap_cie_coe
+#undef mk_sl_cui_inl_defd_sub2_wrap_cid_cod
+#undef mk_sl_cui_inl_defd_sub2_wrap_cid_coe
+#undef mk_sl_cui_inl_defd_sub2_wrap_cie_cod
+#undef mk_sl_cui_inl_defd_sub2_wrap_cie_coe
+
+#undef mk_sl_cui_inl_defd_mul3_wrap_lo_restrict
+#undef mk_sl_cui_inl_defd_mul3_wrap_lo_alias
+#undef mk_sl_cui_inl_defd_mul3_wrap_hi_restrict
+#undef mk_sl_cui_inl_defd_mul3_wrap_hi_alias
+#undef mk_sl_cui_inl_defd_mul4_wrap_wi_restrict
+#undef mk_sl_cui_inl_defd_mul4_wrap_wi_alias
+
+#undef mk_sl_cui_inl_defd_mul3_wrap_lo
+#undef mk_sl_cui_inl_defd_mul3_wrap_hi
+#undef mk_sl_cui_inl_defd_mul4_wrap_wi
+#undef mk_sl_cui_inl_defd_mul2_wrap_lo
+#undef mk_sl_cui_inl_defd_mul2_wrap_hi
+#undef mk_sl_cui_inl_defd_mul2_wrap_wi
