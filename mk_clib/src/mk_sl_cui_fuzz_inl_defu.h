4f77fc1fb0f5d7a4bffdff594fed2d84b964a2a7
Marek Knápek <knapek.mar@gmail.com>
2023-02-20 20:39:34 +0100

    mk_sl_cui_inl_defd_add3_wrap_cie_cod + fuzz

--- mk_clib/src/mk_sl_cui_fuzz_inl_defu.h
+++ mk_clib/src/mk_sl_cui_fuzz_inl_defu.h
@@ -31,6 +31,7 @@
 #undef mk_sl_cui_fuzz_inl_defd_would_overflow_mul_restrict
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_cod_restrict
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_coe_restrict
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod_restrict
 
 #undef mk_sl_cui_fuzz_inl_defd_eq_alias
 #undef mk_sl_cui_fuzz_inl_defd_ne_alias
@@ -75,6 +76,10 @@
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_coe_alias_ac
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_coe_alias_bc
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_coe_alias_abc
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod_alias_ab
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod_alias_ac
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod_alias_bc
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod_alias_abc
 
 #undef mk_sl_cui_fuzz_inl_defd_set_zero
 #undef mk_sl_cui_fuzz_inl_defd_set_max
@@ -118,3 +123,4 @@
 #undef mk_sl_cui_fuzz_inl_defd_would_overflow_mul
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_cod
 #undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cid_coe
+#undef mk_sl_cui_fuzz_inl_defd_add3_wrap_cie_cod
--- mk_clib/src/mk_sl_cui_inl_filec.h
+++ mk_clib/src/mk_sl_cui_inl_filec.h
@@ -947,4 +947,29 @@ mk_lang_jumbo void mk_sl_cui_inl_defd_add3_wrap_cid_coe(mk_sl_cui_inl_defd_t con
 #endif
 }
 
+mk_lang_jumbo void mk_sl_cui_inl_defd_add3_wrap_cie_cod(mk_sl_cui_inl_defd_t const* const a, mk_sl_cui_inl_defd_t const* const b, mk_lang_bool_t const ci, mk_sl_cui_inl_defd_t* const c) mk_lang_noexcept
+{
+#if mk_sl_cui_inl_defd_count == 1
+	mk_lang_assert(a);
+	mk_lang_assert(b);
+	mk_lang_assert(c);
+
+	mk_sl_cui_inl_defd_base_add3_wrap_cie_cod(&a->m_data[0], &b->m_data[0], ci, &c->m_data[0]);
+#else
+	mk_lang_bool_t cf;
+	int i;
+
+	mk_lang_assert(a);
+	mk_lang_assert(b);
+	mk_lang_assert(c);
+
+	mk_sl_cui_inl_defd_base_add3_wrap_cie_coe(&a->m_data[mk_sl_cui_inl_defd_idx(0)], &b->m_data[mk_sl_cui_inl_defd_idx(0)], ci, &c->m_data[mk_sl_cui_inl_defd_idx(0)], &cf);
+	for(i = 1; i != mk_sl_cui_inl_defd_count - 1; ++i)
+	{
+		mk_sl_cui_inl_defd_base_add3_wrap_cie_coe(&a->m_data[mk_sl_cui_inl_defd_idx(i)], &b->m_data[mk_sl_cui_inl_defd_idx(i)], cf, &c->m_data[mk_sl_cui_inl_defd_idx(i)], &cf);
+	}
+	mk_sl_cui_inl_defd_base_add3_wrap_cie_cod(&a->m_data[mk_sl_cui_inl_defd_idx(i)], &b->m_data[mk_sl_cui_inl_defd_idx(i)], cf, &c->m_data[mk_sl_cui_inl_defd_idx(i)]);
+#endif
+}
+
 #include "mk_sl_cui_inl_defu.h"
