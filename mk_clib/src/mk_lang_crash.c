8d6b8427bf32d29da7a70a56973cd121d709ef63
Marek Knápek <knapek.mar@gmail.com>
2023-02-25 13:38:21 +0100

    Better crash / abort / noreturn.

--- mk_clib/src/mk_lang_crash.c
+++ mk_clib/src/mk_lang_crash.c
@@ -5,6 +5,8 @@
 #include "mk_lang_null.h"
 #include "mk_lang_noexcept.h"
 
+#include <stdlib.h> /* abort */
+
 
 mk_lang_noreturn mk_lang_jumbo void mk_lang_crash(void) mk_lang_noexcept
 {
@@ -12,4 +14,22 @@ mk_lang_noreturn mk_lang_jumbo void mk_lang_crash(void) mk_lang_noexcept
 
 	ptr = mk_lang_null;
 	*ptr = 0;
+
+	#if defined _MSC_VER /* todo version */
+	__debugbreak();
+	#endif
+
+	#if defined __has_builtin
+	#if __has_builtin(__builtin_debugtrap)
+	__builtin_debugtrap();
+	#endif
+	#endif
+
+	#if defined __has_builtin
+	#if __has_builtin(__builtin_trap)
+	__builtin_trap();
+	#endif
+	#endif
+
+	abort();
 }
