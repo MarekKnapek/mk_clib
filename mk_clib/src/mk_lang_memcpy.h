c87a707f550c4097e40515720aca0c0d45608a30
Marek Knápek <knapek.mar@gmail.com>
2023-02-26 05:21:41 +0100

    mk_lang_memcpy

---
+++ mk_clib/src/mk_lang_memcpy.h
@@ -0,0 +1,17 @@
+#ifndef mk_include_guard_lang_memcpy
+#define mk_include_guard_lang_memcpy
+
+
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_restrict.h"
+#include "mk_lang_sizet.h"
+
+
+mk_lang_jumbo void mk_lang_memcpy(void* const mk_lang_restrict dst, void const* const mk_lang_restrict src, mk_lang_size_t const cnt) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_want == 1
+#include "mk_lang_memcpy.c"
+#endif
+#endif
