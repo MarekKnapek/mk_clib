872ff21c38fff8e5845e66f8458459f12763ea62
Marek Knápek <knapek.mar@gmail.com>
2023-02-28 22:29:24 +0100

    Better assert.

--- mk_clib/src/mk_lang_assert.h
+++ mk_clib/src/mk_lang_assert.h
@@ -2,7 +2,29 @@
 #define mk_include_guard_lang_assert
 
 
-#if defined NDEBUG
+#define mk_lang_assert_mode_test_crash 1
+#define mk_lang_assert_mode_assume 2
+#define mk_lang_assert_mode_assert 3
+
+#if defined NDEBUG && defined mk_lang_assert_want && mk_lang_assert_want == 1
+#define mk_lang_assert_mode mk_lang_assert_mode_test_crash
+#elif defined NDEBUG && defined mk_lang_assert_want && mk_lang_assert_want == 0
+#define mk_lang_assert_mode mk_lang_assert_mode_assume
+#elif defined NDEBUG && !defined mk_lang_assert_want
+#define mk_lang_assert_mode mk_lang_assert_mode_assume
+#elif !defined NDEBUG && defined mk_lang_assert_want && mk_lang_assert_want == 1
+#define mk_lang_assert_mode mk_lang_assert_mode_assert
+#elif !defined NDEBUG && defined mk_lang_assert_want && mk_lang_assert_want == 0
+#define mk_lang_assert_mode mk_lang_assert_mode_assume
+#elif !defined NDEBUG && !defined mk_lang_assert_want
+#define mk_lang_assert_mode mk_lang_assert_mode_assert
+#endif
+
+#if mk_lang_assert_mode == mk_lang_assert_mode_test_crash
+#include "mk_lang_crash.h"
+#include "mk_lang_likely.h"
+#define mk_lang_assert(x) ((void)((x) ? ((void)(0)) : ((void)(mk_lang_unlikely mk_lang_crash()))))
+#elif mk_lang_assert_mode == mk_lang_assert_mode_assume
 #if defined __cplusplus && __cplusplus >= 202299l /* todo c++ 23 */
 #define mk_lang_assert(x) [[assume(x)]]
 #elif defined __cpp_lib_unreachable && __cpp_lib_unreachable >= 202202l
@@ -24,7 +46,7 @@
 #else
 #define mk_lang_assert(x) ((void)(0))
 #endif
-#else
+#elif mk_lang_assert_mode == mk_lang_assert_mode_assert
 #if defined __cplusplus
 #include <cassert> /* assert */
 #define mk_lang_assert assert
