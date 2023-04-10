891eea9361131866ef1bc4f4380114975046ff0d
Marek Knápek <knapek.mar@gmail.com>
2023-04-10 23:51:40 +0200

    mk_lang_constexpr_static

--- mk_clib/src/mk_lang_constexpr.h
+++ mk_clib/src/mk_lang_constexpr.h
@@ -2,18 +2,26 @@
 #define mk_include_guard_lang_constexpr
 
 
-#if defined __cplusplus && __cplusplus >= 201402l
+#if defined __cplusplus && __cplusplus >= 202299l /* todo c++23 */
 #define mk_lang_constexpr_has 1
 #define mk_lang_constexpr constexpr
 #define mk_lang_constexpr_init {}
+#define mk_lang_constexpr_static static constexpr
+#elif defined __cplusplus && __cplusplus >= 201402l
+#define mk_lang_constexpr_has 1
+#define mk_lang_constexpr constexpr
+#define mk_lang_constexpr_init {}
+#define mk_lang_constexpr_static constexpr
 #elif defined _MSVC_LANG && _MSVC_LANG >= 201402l
 #define mk_lang_constexpr_has 1
 #define mk_lang_constexpr constexpr
 #define mk_lang_constexpr_init {}
+#define mk_lang_constexpr_static constexpr
 #else
 #define mk_lang_constexpr_has 0
 #define mk_lang_constexpr
 #define mk_lang_constexpr_init
+#define mk_lang_constexpr_static static
 #endif
 
 
