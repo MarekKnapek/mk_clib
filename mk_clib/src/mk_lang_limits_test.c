5129608dc681894e93625d0bcd5d91ec9ddd9f76
Marek Knápek <knapek.mar@gmail.com>
2023-06-10 21:04:11 +0200

    Test for 128bit integers.

--- mk_clib/src/mk_lang_limits_test.c
+++ mk_clib/src/mk_lang_limits_test.c
@@ -11,6 +11,16 @@ mk_lang_jumbo void mk_lang_limits_test(void) mk_lang_noexcept
 {
 	/* Beware, might fail on different platform than mine. */
 
+#if mk_lang_lllong_has
+#define ulllong_max ((mk_lang_ulllong_t)(((mk_lang_ulllong_t)(((mk_lang_ulllong_t)(mk_lang_ullong_c(0xffffffffffffffff))) << 64)) | ((mk_lang_ulllong_t)(mk_lang_ullong_c(0xffffffffffffffff)))))
+#define slllong_min ((mk_lang_slllong_t)(((mk_lang_slllong_t)(((mk_lang_slllong_t)(mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1))) << 64)) | ((mk_lang_slllong_t)(mk_lang_ullong_c(0x0000000000000000)))))
+#define slllong_max ((mk_lang_slllong_t)(((mk_lang_slllong_t)(((mk_lang_slllong_t)(mk_lang_ullong_c(9223372036854775807))) << 64)) | ((mk_lang_slllong_t)(mk_lang_ullong_c(0xffffffffffffffff)))))
+#else
+#define ulllong_max (mk_lang_ullong_c(0xffffffffffffffff))
+#define slllong_min ((mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)))
+#define slllong_max (mk_lang_ullong_c(9223372036854775807))
+#endif
+
 #if mk_lang_bitness == mk_lang_bitness_16
 	todo
 #elif mk_lang_bitness == mk_lang_bitness_32
@@ -69,7 +79,7 @@ mk_lang_jumbo void mk_lang_limits_test(void) mk_lang_noexcept
 	mk_lang_static_assert(mk_lang_limits_uint_max    == mk_lang_ullong_c(0xffffffff));
 	mk_lang_static_assert(mk_lang_limits_ulong_max   == mk_lang_ullong_c(0xffffffff));
 	mk_lang_static_assert(mk_lang_limits_ullong_max  == mk_lang_ullong_c(0xffffffffffffffff));
-	mk_lang_static_assert(mk_lang_limits_ulllong_max == mk_lang_ullong_c(0xffffffffffffffff));
+	mk_lang_static_assert(mk_lang_limits_ulllong_max == ulllong_max);
 	mk_lang_static_assert(mk_lang_limits_uintptr_max == mk_lang_ullong_c(0xffffffffffffffff));
 	mk_lang_static_assert(mk_lang_limits_uintmax_max == mk_lang_ullong_c(0xffffffffffffffff));
 	mk_lang_static_assert(mk_lang_limits_usize_max   == mk_lang_ullong_c(0xffffffffffffffff));
@@ -79,7 +89,7 @@ mk_lang_jumbo void mk_lang_limits_test(void) mk_lang_noexcept
 	mk_lang_static_assert(mk_lang_limits_sint_min    == (mk_lang_sllong_c(-2147483647)          - mk_lang_sllong_c(1)));
 	mk_lang_static_assert(mk_lang_limits_slong_min   == (mk_lang_sllong_c(-2147483647)          - mk_lang_sllong_c(1)));
 	mk_lang_static_assert(mk_lang_limits_sllong_min  == (mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)));
-	mk_lang_static_assert(mk_lang_limits_slllong_min == (mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)));
+	mk_lang_static_assert(mk_lang_limits_slllong_min == slllong_min);
 	mk_lang_static_assert(mk_lang_limits_sintptr_min == (mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)));
 	mk_lang_static_assert(mk_lang_limits_sintmax_min == (mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)));
 	mk_lang_static_assert(mk_lang_limits_ssize_min   == (mk_lang_sllong_c(-9223372036854775807) - mk_lang_sllong_c(1)));
@@ -89,7 +99,7 @@ mk_lang_jumbo void mk_lang_limits_test(void) mk_lang_noexcept
 	mk_lang_static_assert(mk_lang_limits_sint_max    == mk_lang_sllong_c(2147483647));
 	mk_lang_static_assert(mk_lang_limits_slong_max   == mk_lang_sllong_c(2147483647));
 	mk_lang_static_assert(mk_lang_limits_sllong_max  == mk_lang_sllong_c(9223372036854775807));
-	mk_lang_static_assert(mk_lang_limits_slllong_max == mk_lang_sllong_c(9223372036854775807));
+	mk_lang_static_assert(mk_lang_limits_slllong_max == slllong_max);
 	mk_lang_static_assert(mk_lang_limits_sintptr_max == mk_lang_sllong_c(9223372036854775807));
 	mk_lang_static_assert(mk_lang_limits_sintmax_max == mk_lang_sllong_c(9223372036854775807));
 	mk_lang_static_assert(mk_lang_limits_ssize_max   == mk_lang_sllong_c(9223372036854775807));
