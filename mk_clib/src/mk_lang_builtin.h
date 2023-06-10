65d7eb402f118111539e4eedd652abaaf5fdbc90
Marek Knápek <knapek.mar@gmail.com>
2023-06-10 17:55:32 +0200

    mk_lang_builtin

---
+++ mk_clib/src/mk_lang_builtin.h
@@ -0,0 +1,26 @@
+#ifndef mk_include_guard_mk_lang_builtin
+#define mk_include_guard_mk_lang_builtin
+
+
+#if defined __has_builtin
+#if __has_builtin(__builtin_assume)
+#define mk_lang_builtin_has_assume 1
+#else
+#define mk_lang_builtin_has_assume 0
+#endif
+#else
+#define mk_lang_builtin_has_assume 0
+#endif
+
+#if defined __has_builtin
+#if __has_builtin(__builtin_unreachable)
+#define mk_lang_builtin_has_unreachable 1
+#else
+#define mk_lang_builtin_has_unreachable 0
+#endif
+#else
+#define mk_lang_builtin_has_unreachable 0
+#endif
+
+
+#endif
