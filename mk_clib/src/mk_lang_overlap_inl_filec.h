9225874ef140c13fca073e45614e5a947c825668
Marek Knápek <knapek.mar@gmail.com>
2023-06-11 20:56:39 +0200

    Better overlap.

--- mk_clib/src/mk_lang_overlap_inl_filec.h
+++ mk_clib/src/mk_lang_overlap_inl_filec.h
@@ -19,6 +19,7 @@
 
 mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_bool_t mk_lang_overlap_fn(mk_lang_overlap_t const* const arr1, mk_lang_size_t const len1, mk_lang_overlap_t const* const arr2, mk_lang_size_t const len2) mk_lang_noexcept
 {
+#if mk_lang_constexpr_has
 	mk_lang_size_t i mk_lang_constexpr_init;
 	mk_lang_size_t j mk_lang_constexpr_init;
 
@@ -36,6 +37,26 @@ mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_bool_t mk_lang_overlap
 		}
 	}
 	return mk_lang_false;
+#else
+	unsigned char const* p1;
+	unsigned char const* p2;
+	mk_lang_size_t l1;
+	mk_lang_size_t l2;
+	mk_lang_bool_t ret;
+
+	mk_lang_assert(arr1);
+	mk_lang_assert(arr2);
+
+	p1 = ((unsigned char const*)(arr1));
+	p2 = ((unsigned char const*)(arr2));
+	l1 = len1 * sizeof(mk_lang_overlap_t);
+	l2 = len2 * sizeof(mk_lang_overlap_t);
+
+	ret =
+		(p1 >= p2 && p1 < p2 + l2) ||
+		(p2 >= p1 && p2 < p1 + l1);
+	return ret;
+#endif
 }
 
 
