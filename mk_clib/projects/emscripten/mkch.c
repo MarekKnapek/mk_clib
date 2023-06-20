9fcefadcaca4ca7f2e1b083e8ac24b90d8f61c1b
Marek Knápek <knapek.mar@gmail.com>
2023-06-21 01:44:02 +0200

    Use name instead of id for WASM.

--- mk_clib/projects/emscripten/mkch.c
+++ mk_clib/projects/emscripten/mkch.c
@@ -1,6 +1,9 @@
+#include "../../src/mk_lang_assert.h"
+#include "../../src/mk_lang_constexpr.h"
 #include "../../src/mk_lang_cpp.h"
 #include "../../src/mk_lang_inline.h"
 #include "../../src/mk_lang_likely.h"
+#include "../../src/mk_lang_nodiscard.h"
 #include "../../src/mk_lang_noexcept.h"
 #include "../../src/mk_lang_types.h"
 #include "../../src/mk_lib_crypto_hash_stream_md2.h"
@@ -60,6 +63,106 @@ enum hash_id_e
 };
 typedef enum hash_id_e hash_id_t;
 
+#define hash_name_dn_hash_md2          "md2"
+#define hash_name_dn_hash_md4          "md4"
+#define hash_name_dn_hash_md5          "md5"
+#define hash_name_dn_hash_sha0         "sha0"
+#define hash_name_dn_hash_sha1         "sha1"
+#define hash_name_dn_hash_sha2_224     "sha2_224"
+#define hash_name_dn_hash_sha2_256     "sha2_256"
+#define hash_name_dn_hash_sha2_384     "sha2_384"
+#define hash_name_dn_hash_sha2_512     "sha2_512"
+#define hash_name_dn_hash_sha2_512_224 "sha2_512_224"
+#define hash_name_dn_hash_sha2_512_256 "sha2_512_256"
+#define hash_name_dn_hash_sha3_224     "sha3_224"
+#define hash_name_dn_hash_sha3_256     "sha3_256"
+#define hash_name_dn_hash_sha3_384     "sha3_384"
+#define hash_name_dn_hash_sha3_512     "sha3_512"
+#define hash_name_dn_hash_tiger2_128   "tiger2_128"
+#define hash_name_dn_hash_tiger2_160   "tiger2_160"
+#define hash_name_dn_hash_tiger2_192   "tiger2_192"
+#define hash_name_dn_hash_tiger_128    "tiger_128"
+#define hash_name_dn_hash_tiger_160    "tiger_160"
+#define hash_name_dn_hash_tiger_192    "tiger_192"
+#define hash_name_dn_xof_shake_128     "shake_128"
+#define hash_name_dn_xof_shake_256     "shake_256"
+
+#define hash_name_dl_hash_md2          ((int)(sizeof(hash_name_dn_hash_md2         ) / sizeof(hash_name_dn_hash_md2         [0]) - 1))
+#define hash_name_dl_hash_md4          ((int)(sizeof(hash_name_dn_hash_md4         ) / sizeof(hash_name_dn_hash_md4         [0]) - 1))
+#define hash_name_dl_hash_md5          ((int)(sizeof(hash_name_dn_hash_md5         ) / sizeof(hash_name_dn_hash_md5         [0]) - 1))
+#define hash_name_dl_hash_sha0         ((int)(sizeof(hash_name_dn_hash_sha0        ) / sizeof(hash_name_dn_hash_sha0        [0]) - 1))
+#define hash_name_dl_hash_sha1         ((int)(sizeof(hash_name_dn_hash_sha1        ) / sizeof(hash_name_dn_hash_sha1        [0]) - 1))
+#define hash_name_dl_hash_sha2_224     ((int)(sizeof(hash_name_dn_hash_sha2_224    ) / sizeof(hash_name_dn_hash_sha2_224    [0]) - 1))
+#define hash_name_dl_hash_sha2_256     ((int)(sizeof(hash_name_dn_hash_sha2_256    ) / sizeof(hash_name_dn_hash_sha2_256    [0]) - 1))
+#define hash_name_dl_hash_sha2_384     ((int)(sizeof(hash_name_dn_hash_sha2_384    ) / sizeof(hash_name_dn_hash_sha2_384    [0]) - 1))
+#define hash_name_dl_hash_sha2_512     ((int)(sizeof(hash_name_dn_hash_sha2_512    ) / sizeof(hash_name_dn_hash_sha2_512    [0]) - 1))
+#define hash_name_dl_hash_sha2_512_224 ((int)(sizeof(hash_name_dn_hash_sha2_512_224) / sizeof(hash_name_dn_hash_sha2_512_224[0]) - 1))
+#define hash_name_dl_hash_sha2_512_256 ((int)(sizeof(hash_name_dn_hash_sha2_512_256) / sizeof(hash_name_dn_hash_sha2_512_256[0]) - 1))
+#define hash_name_dl_hash_sha3_224     ((int)(sizeof(hash_name_dn_hash_sha3_224    ) / sizeof(hash_name_dn_hash_sha3_224    [0]) - 1))
+#define hash_name_dl_hash_sha3_256     ((int)(sizeof(hash_name_dn_hash_sha3_256    ) / sizeof(hash_name_dn_hash_sha3_256    [0]) - 1))
+#define hash_name_dl_hash_sha3_384     ((int)(sizeof(hash_name_dn_hash_sha3_384    ) / sizeof(hash_name_dn_hash_sha3_384    [0]) - 1))
+#define hash_name_dl_hash_sha3_512     ((int)(sizeof(hash_name_dn_hash_sha3_512    ) / sizeof(hash_name_dn_hash_sha3_512    [0]) - 1))
+#define hash_name_dl_hash_tiger2_128   ((int)(sizeof(hash_name_dn_hash_tiger2_128  ) / sizeof(hash_name_dn_hash_tiger2_128  [0]) - 1))
+#define hash_name_dl_hash_tiger2_160   ((int)(sizeof(hash_name_dn_hash_tiger2_160  ) / sizeof(hash_name_dn_hash_tiger2_160  [0]) - 1))
+#define hash_name_dl_hash_tiger2_192   ((int)(sizeof(hash_name_dn_hash_tiger2_192  ) / sizeof(hash_name_dn_hash_tiger2_192  [0]) - 1))
+#define hash_name_dl_hash_tiger_128    ((int)(sizeof(hash_name_dn_hash_tiger_128   ) / sizeof(hash_name_dn_hash_tiger_128   [0]) - 1))
+#define hash_name_dl_hash_tiger_160    ((int)(sizeof(hash_name_dn_hash_tiger_160   ) / sizeof(hash_name_dn_hash_tiger_160   [0]) - 1))
+#define hash_name_dl_hash_tiger_192    ((int)(sizeof(hash_name_dn_hash_tiger_192   ) / sizeof(hash_name_dn_hash_tiger_192   [0]) - 1))
+#define hash_name_dl_xof_shake_128     ((int)(sizeof(hash_name_dn_xof_shake_128    ) / sizeof(hash_name_dn_xof_shake_128    [0]) - 1))
+#define hash_name_dl_xof_shake_256     ((int)(sizeof(hash_name_dn_xof_shake_256    ) / sizeof(hash_name_dn_xof_shake_256    [0]) - 1))
+
+mk_lang_constexpr_static_inline char const s_hash_names[] =
+	hash_name_dn_hash_md2
+	hash_name_dn_hash_md4
+	hash_name_dn_hash_md5
+	hash_name_dn_hash_sha0
+	hash_name_dn_hash_sha1
+	hash_name_dn_hash_sha2_224
+	hash_name_dn_hash_sha2_256
+	hash_name_dn_hash_sha2_384
+	hash_name_dn_hash_sha2_512
+	hash_name_dn_hash_sha2_512_224
+	hash_name_dn_hash_sha2_512_256
+	hash_name_dn_hash_sha3_224
+	hash_name_dn_hash_sha3_256
+	hash_name_dn_hash_sha3_384
+	hash_name_dn_hash_sha3_512
+	hash_name_dn_hash_tiger2_128
+	hash_name_dn_hash_tiger2_160
+	hash_name_dn_hash_tiger2_192
+	hash_name_dn_hash_tiger_128
+	hash_name_dn_hash_tiger_160
+	hash_name_dn_hash_tiger_192
+	hash_name_dn_xof_shake_128
+	hash_name_dn_xof_shake_256
+;
+
+mk_lang_constexpr_static_inline mk_lang_types_uchar_t const s_hash_lens[] =
+{
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_md2         )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_md4         )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_md5         )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha0        )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha1        )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_224    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_256    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_384    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_512    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_512_224)),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha2_512_256)),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha3_224    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha3_256    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha3_384    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_sha3_512    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger2_128  )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger2_160  )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger2_192  )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger_128   )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger_160   )),
+	((mk_lang_types_uchar_t)(hash_name_dl_hash_tiger_192   )),
+	((mk_lang_types_uchar_t)(hash_name_dl_xof_shake_128    )),
+	((mk_lang_types_uchar_t)(hash_name_dl_xof_shake_256    )),
+};
 
 union hash_u
 {
@@ -225,18 +328,45 @@ static mk_lang_inline void copy(hash_id_t const id, int const xof_len, digest_al
 	}
 }
 
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_bool_t compare(mk_lang_types_pchar_prct const a, mk_lang_types_pchar_prct const b, int const len) mk_lang_noexcept
+{
+	int i;
+
+	mk_lang_assert(a);
+	mk_lang_assert(b);
+	mk_lang_assert(a != b);
+	mk_lang_assert(!(a >= b && a < b + len));
+	mk_lang_assert(!(b >= a && b < a + len));
+	mk_lang_assert(len >= 1);
+	mk_lang_assert(len <= 0xff);
+
+	for(i = 0; i != len; ++i)
+	{
+		if(a[i] != b[i])
+		{
+			return mk_lang_false;
+		}
+	}
+	return mk_lang_true;
+}
+
 
 #define check(x) if(!(x)){ mk_lang_unlikely return 0; }
 
 
-mk_lang_extern_c EMSCRIPTEN_KEEPALIVE int mkch(int const hash_id, int const xof_len, mk_lang_types_uchar_pct data, int const data_len, mk_lang_types_uchar_pt const digest, int const digest_len, mk_lang_types_sint_pt const ret_len) mk_lang_noexcept
+mk_lang_extern_c EMSCRIPTEN_KEEPALIVE int mkch(mk_lang_types_pchar_pct const hash_name, int const hash_name_len, int const xof_len, mk_lang_types_uchar_pct data, int const data_len, mk_lang_types_uchar_pt const digest, int const digest_len) mk_lang_noexcept
 {
+	mk_lang_types_pchar_pct ptr;
+	int n;
+	int i;
 	hash_id_t id;
 	hash_t hash;
 	digest_aligned16k_t aligned_digest;
+	int ret;
 
-	check(hash_id >= hash_id_e_hash_md2);
-	check(hash_id <= hash_id_e_xof_shake_256);
+	check(hash_name);
+	check(hash_name_len >= 1);
+	check(hash_name_len <= 0xff);
 	check(xof_len >= 1);
 	check(xof_len <= 16 * 1024);
 	check(data || data_len == 0);
@@ -244,15 +374,36 @@ mk_lang_extern_c EMSCRIPTEN_KEEPALIVE int mkch(int const hash_id, int const xof_
 	check(data_len <= 1 * 1024 * 1024);
 	check(digest);
 	check(digest_len >= 16 * 1024);
-	check(ret_len);
 
-	*ret_len = 0;
-	id = ((hash_id_t)(hash_id));
+	ptr = s_hash_names;
+	n = ((int)(sizeof(s_hash_lens) / sizeof(s_hash_lens[0])));
+	for(i = 0; i != n; ++i)
+	{
+		if(hash_name_len == s_hash_lens[i])
+		{
+			if(compare(hash_name, ptr, hash_name_len))
+			{
+				break;
+			}
+			else
+			{
+				ptr += s_hash_lens[i];
+			}
+		}
+		else
+		{
+			ptr += s_hash_lens[i];
+		}
+	}
+	check(i != n);
+	check(i >= hash_id_e_hash_md2);
+	check(i <= hash_id_e_xof_shake_256);
+	id = ((hash_id_t)(i));
 	init(&hash, id);
 	append(&hash, id, data, data_len);
 	finish(&hash, id, xof_len, &aligned_digest);
-	copy(id, xof_len, &aligned_digest, digest, ret_len);
-	return 1;
+	copy(id, xof_len, &aligned_digest, digest, &ret);
+	return ret;
 }
 
 
