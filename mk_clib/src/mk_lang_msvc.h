09f65408c9ee4b23ab4f365bd1872b0e5bf0dee2
Marek Knápek <knapek.mar@gmail.com>
2023-07-08 00:54:08 +0200

    MSVC

---
+++ mk_clib/src/mk_lang_msvc.h
@@ -0,0 +1,151 @@
+#ifndef mk_include_guard_mk_lang_msvc
+#define mk_include_guard_mk_lang_msvc
+
+
+#define mk_lang_msvc_ver_2022_17_3_4 1933l
+#define mk_lang_msvc_ver_2022_17_2_2 1932l
+#define mk_lang_msvc_ver_2022_17_0_2 1930l
+#define mk_lang_msvc_ver_2022_17_0_1 1930l
+
+#define mk_lang_msvc_ver_2019_u_1_16_11_2 1929l
+#define mk_lang_msvc_ver_2019_u_9_16_9_2  1928l
+#define mk_lang_msvc_ver_2019_u_8_16_8_2  1928l
+#define mk_lang_msvc_ver_2019_u_8_16_8_1  1928l
+#define mk_lang_msvc_ver_2019_u_7_16_7    1927l
+#define mk_lang_msvc_ver_2019_u_6_16_6_2  1926l
+#define mk_lang_msvc_ver_2019_u_5_16_5_1  1925l
+#define mk_lang_msvc_ver_2019_u_4_16_4_0  1924l
+#define mk_lang_msvc_ver_2019_u_3_16_3_2  1923l
+#define mk_lang_msvc_ver_2019_u_2_16_2_3  1922l
+#define mk_lang_msvc_ver_2019_u_1_16_1_2  1921l
+#define mk_lang_msvc_ver_2019_16_0_0      1920l
+
+#define mk_lang_msvc_ver_2017_u_9_15_9_11 1916l
+#define mk_lang_msvc_ver_2017_u_9_15_9_7  1916l
+#define mk_lang_msvc_ver_2017_u_9_15_9_5  1916l
+#define mk_lang_msvc_ver_2017_u_9_15_9_4  1916l
+#define mk_lang_msvc_ver_2017_u_9_15_9_1  1916l
+#define mk_lang_msvc_ver_2017_u_9_15_9_0  1916l
+#define mk_lang_msvc_ver_2017_u_8_15_8_0  1915l
+#define mk_lang_msvc_ver_2017_u_7_15_7_5  1914l
+#define mk_lang_msvc_ver_2017_u_7_15_7_3  1914l
+#define mk_lang_msvc_ver_2017_u_7_15_7_2  1914l
+#define mk_lang_msvc_ver_2017_u_7_15_7_1  1914l
+#define mk_lang_msvc_ver_2017_u_6_15_6_7  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_6  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_4  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_3  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_2  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_1  1913l
+#define mk_lang_msvc_ver_2017_u_6_15_6_0  1913l
+#define mk_lang_msvc_ver_2017_u_5_15_5_7  1912l
+#define mk_lang_msvc_ver_2017_u_5_15_5_6  1912l
+#define mk_lang_msvc_ver_2017_u_5_15_5_4  1912l
+#define mk_lang_msvc_ver_2017_u_5_15_5_3  1912l
+#define mk_lang_msvc_ver_2017_u_5_15_5_2  1912l
+#define mk_lang_msvc_ver_2017_u_4_15_4_5  1911l
+#define mk_lang_msvc_ver_2017_u_4_15_4_4  1911l
+#define mk_lang_msvc_ver_2017_u_3_15_3_3  1911l
+#define mk_lang_msvc_ver_2017_u_15_2      1910l
+#define mk_lang_msvc_ver_2017_u_15_1      1910l
+#define mk_lang_msvc_ver_2017_15_0        1910l
+
+#define mk_lang_msvc_ver_2015_u_3 1900l
+#define mk_lang_msvc_ver_2015_u_2 1900l
+#define mk_lang_msvc_ver_2015_u_1 1900l
+#define mk_lang_msvc_ver_2015     1900l
+
+#define mk_lang_msvc_ver_2013_u_5 1800l
+#define mk_lang_msvc_ver_2013_u_4 1800l
+#define mk_lang_msvc_ver_2013_u_3 1800l
+#define mk_lang_msvc_ver_2013_u_2 1800l
+#define mk_lang_msvc_ver_2013_u_1 1800l
+#define mk_lang_msvc_ver_2013     1800l
+
+#define mk_lang_msvc_ver_2012_u_4 1700l
+#define mk_lang_msvc_ver_2012_u_3 1700l
+#define mk_lang_msvc_ver_2012_u_2 1700l
+#define mk_lang_msvc_ver_2012_u_1 1700l
+#define mk_lang_msvc_ver_2012     1700l
+
+#define mk_lang_msvc_ver_2010_sp_1 1600l
+#define mk_lang_msvc_ver_2010      1600l
+
+#define mk_lang_msvc_ver_2008_sp_1 1500l
+#define mk_lang_msvc_ver_2008      1500l
+
+
+#define mk_lang_msvc_full_ver_2022_17_3_4 193331630l
+#define mk_lang_msvc_full_ver_2022_17_2_2 193231329l
+#define mk_lang_msvc_full_ver_2022_17_0_2 193030706l
+#define mk_lang_msvc_full_ver_2022_17_0_1 193030705l
+
+#define mk_lang_msvc_full_ver_2019_u_1_16_11_2 192930133l
+#define mk_lang_msvc_full_ver_2019_u_9_16_9_2  192829913l
+#define mk_lang_msvc_full_ver_2019_u_8_16_8_2  192829334l
+#define mk_lang_msvc_full_ver_2019_u_8_16_8_1  192829333l
+#define mk_lang_msvc_full_ver_2019_u_7_16_7    192729112l
+#define mk_lang_msvc_full_ver_2019_u_6_16_6_2  192628806l
+#define mk_lang_msvc_full_ver_2019_u_5_16_5_1  192528611l
+#define mk_lang_msvc_full_ver_2019_u_4_16_4_0  192428314l
+#define mk_lang_msvc_full_ver_2019_u_3_16_3_2  192328105l
+#define mk_lang_msvc_full_ver_2019_u_2_16_2_3  192227905l
+#define mk_lang_msvc_full_ver_2019_u_1_16_1_2  192127702l
+#define mk_lang_msvc_full_ver_2019_16_0_0      192027508l
+
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_11 191627030l
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_7  191627027l
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_5  191627026l
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_4  191627025l
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_1  191627023l
+#define mk_lang_msvc_full_ver_2017_u_9_15_9_0
+#define mk_lang_msvc_full_ver_2017_u_8_15_8_0
+#define mk_lang_msvc_full_ver_2017_u_7_15_7_5  191426433l
+#define mk_lang_msvc_full_ver_2017_u_7_15_7_3  191426430l
+#define mk_lang_msvc_full_ver_2017_u_7_15_7_2  191426429l
+#define mk_lang_msvc_full_ver_2017_u_7_15_7_1  191426428l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_7  191326132l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_6  191326131l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_4  191326129l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_3  191326129l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_2  191326128l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_1  191326128l
+#define mk_lang_msvc_full_ver_2017_u_6_15_6_0  191326128l
+#define mk_lang_msvc_full_ver_2017_u_5_15_5_7  191225835l
+#define mk_lang_msvc_full_ver_2017_u_5_15_5_6  191225835l
+#define mk_lang_msvc_full_ver_2017_u_5_15_5_4  191225834l
+#define mk_lang_msvc_full_ver_2017_u_5_15_5_3  191225834l
+#define mk_lang_msvc_full_ver_2017_u_5_15_5_2  191225831l
+#define mk_lang_msvc_full_ver_2017_u_4_15_4_5  191125547l
+#define mk_lang_msvc_full_ver_2017_u_4_15_4_4  191125542l
+#define mk_lang_msvc_full_ver_2017_u_3_15_3_3  191125507l
+#define mk_lang_msvc_full_ver_2017_u_15_2      191025017l
+#define mk_lang_msvc_full_ver_2017_u_15_1      191025017l
+#define mk_lang_msvc_full_ver_2017_15_0        191025017l
+
+#define mk_lang_msvc_full_ver_2015_u_3 190024210l
+#define mk_lang_msvc_full_ver_2015_u_2 190023918l
+#define mk_lang_msvc_full_ver_2015_u_1 190023506l
+#define mk_lang_msvc_full_ver_2015     190023026l
+
+#define mk_lang_msvc_full_ver_2013_u_5 180040629l
+#define mk_lang_msvc_full_ver_2013_u_4 180031101l
+#define mk_lang_msvc_full_ver_2013_u_3 180030723l
+#define mk_lang_msvc_full_ver_2013_u_2 180030501l
+#define mk_lang_msvc_full_ver_2013_u_1 180021005l
+#define mk_lang_msvc_full_ver_2013     180021005l
+
+#define mk_lang_msvc_full_ver_2012_u_4 170061030l
+#define mk_lang_msvc_full_ver_2012_u_3 170060610l
+#define mk_lang_msvc_full_ver_2012_u_2 170060315l
+#define mk_lang_msvc_full_ver_2012_u_1 170051106l
+#define mk_lang_msvc_full_ver_2012     170050727l
+
+#define mk_lang_msvc_full_ver_2010_sp_1 160040219l
+#define mk_lang_msvc_full_ver_2010      160030319l
+
+#define mk_lang_msvc_full_ver_2008_sp_1 150030729l
+#define mk_lang_msvc_full_ver_2008      150021022l
+
+
+#endif
