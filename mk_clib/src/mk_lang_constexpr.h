686cdeed150796d0167f2815adaa14e976d7da7b
Marek Knápek <knapek.mar@gmail.com>
2023-07-08 00:53:15 +0200

    if consteval

--- mk_clib/src/mk_lang_constexpr.h
+++ mk_clib/src/mk_lang_constexpr.h
@@ -50,4 +50,13 @@
 #endif
 
 
+#if mk_lang_version_at_least_cpp_23
+#define mk_lang_constexpr_if_consteval_has 1
+#define mk_lang_constexpr_if_consteval_test if consteval
+#else
+#define mk_lang_constexpr_if_consteval_has 0
+#define mk_lang_constexpr_if_consteval_test if(0)
+#endif
+
+
 #endif
