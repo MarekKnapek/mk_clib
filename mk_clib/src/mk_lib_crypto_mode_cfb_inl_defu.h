8464071bf26137c64d5f52193a177feb40f9eecb
Marek Knápek <knapek.mar@gmail.com>
2023-07-08 01:49:58 +0200

    CFB template.

---
+++ mk_clib/src/mk_lib_crypto_mode_cfb_inl_defu.h
@@ -0,0 +1,72 @@
+
+
+
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_name
+
+
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_s_bits
+#undef mk_lib_crypto_mode_cfb_inl_defd_s_bytes
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_constexpr
+
+
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_prefix
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_len_m
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_len_e
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_len_v
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_len_m
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_len_e
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_len_v
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_key_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_msg_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_encrypt
+#undef mk_lib_crypto_mode_cfb_inl_defd_alg_decrypt
+
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_prefix
+
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_len_m
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_len_e
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_len_v
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_len_m
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_len_e
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_len_v
+#undef mk_lib_crypto_mode_cfb_inl_defd_aligned_msg_u
+#undef mk_lib_crypto_mode_cfb_inl_defd_aligned_msg_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_aligned_msg_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_aligned_msg_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_aligned_msg_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_key_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_s
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_msg_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_s
+#undef mk_lib_crypto_mode_cfb_inl_defd_t
+#undef mk_lib_crypto_mode_cfb_inl_defd_ct
+#undef mk_lib_crypto_mode_cfb_inl_defd_pt
+#undef mk_lib_crypto_mode_cfb_inl_defd_pct
+#undef mk_lib_crypto_mode_cfb_inl_defd_init
+#undef mk_lib_crypto_mode_cfb_inl_defd_encrypt
+#undef mk_lib_crypto_mode_cfb_inl_defd_decrypt
