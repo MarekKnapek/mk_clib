3687b8820dc24552b331fb32e8bf2c295a0b3bf7
Marek Knápek <knapek.mar@gmail.com>
2023-07-08 02:10:53 +0200

    Increase GCC constexpr limit.

--- mk_clib/projects/gcc/build_run_mkct.sh
+++ mk_clib/projects/gcc/build_run_mkct.sh
@@ -1,1 +1,1 @@
-gcc -std=c2x -DNDEBUG -O3 -Wall -Wextra -Wpedantic -o mkct.exe ../../app/mkct.c && ./mkct.exe
+gcc -std=c2x -DNDEBUG -O3 -fconstexpr-ops-limit=1000000000 -Wall -Wextra -Wpedantic -o mkct.exe ../../app/mkct.c && ./mkct.exe
