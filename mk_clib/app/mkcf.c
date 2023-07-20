348871acf756b5619b51233dd0d6de64d18660d1
Marek Knápek <knapek.mar@gmail.com>
2023-07-20 17:04:46 +0200

    Improve fuzzer.

--- mk_clib/app/mkcf.c
+++ mk_clib/app/mkcf.c
@@ -1,9 +1,29 @@
 #include "../src/mk_clib_app_fuzz.h"
 #include "../src/mk_lang_cpp.h"
-#include "../src/mk_lang_sizet.h"
+#include "../src/mk_lang_nodiscard.h"
+#include "../src/mk_lang_noexcept.h"
+#include "../src/mk_lang_types.h"
 
 
-mk_lang_extern_c int LLVMFuzzerTestOneInput(unsigned char const* data, mk_lang_size_t size)
+mk_lang_extern_c mk_lang_nodiscard mk_lang_types_usize_t LLVMFuzzerMutate(mk_lang_types_uchar_pt const data, mk_lang_types_usize_t const size, mk_lang_types_usize_t const size_max) mk_lang_noexcept;
+
+
+mk_lang_extern_c int LLVMFuzzerTestOneInput(mk_lang_types_uchar_pct const data, mk_lang_types_usize_t const size) mk_lang_noexcept
 {
 	return mk_clib_app_fuzz(data, size);
+}
+
+mk_lang_extern_c mk_lang_types_usize_t LLVMFuzzerCustomMutator(mk_lang_types_uchar_pt const data, mk_lang_types_usize_t const size, mk_lang_types_usize_t const size_max, mk_lang_types_uint_t const seed) mk_lang_noexcept
+{
+	mk_lang_types_usize_t s;
+
+	if(size != 0 && seed % 2 == 0)
+	{
+		return size - 1;
+	}
+	else
+	{
+		s = LLVMFuzzerMutate(data, size, size_max);
+		return s;
+	}
 }
