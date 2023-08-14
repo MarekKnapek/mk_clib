830de08f9cfe56a51bed47b6663bf8c693d95ca4
Marek Knápek <knapek.mar@gmail.com>
2023-08-14 15:27:21 +0200

    Fix bug in mk_lang_check_if_failed.

--- mk_clib/src/mk_lang_check.h
+++ mk_clib/src/mk_lang_check.h
@@ -11,7 +11,7 @@
 mk_lang_nodiscard mk_lang_jumbo mk_lang_bool_t mk_lang_check_if_failed_impl(mk_lang_bool_t const b) mk_lang_noexcept;
 
 
-#define mk_lang_check_if_failed(x) mk_lang_check_if_failed_impl(!!(x))
+#define mk_lang_check_if_failed(x) mk_lang_check_if_failed_impl(!!!(x))
 
 
 #if mk_lang_jumbo_want == 1
