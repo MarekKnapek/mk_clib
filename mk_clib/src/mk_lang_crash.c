b7c502224dd333a62eadb1119621cc15aea002c8
Marek Knápek <knapek.mar@gmail.com>
2023-09-22 03:08:26 +0200

    mk_lang_crash_detail_want

--- mk_clib/src/mk_lang_crash.c
+++ mk_clib/src/mk_lang_crash.c
@@ -7,10 +7,25 @@
 
 #include <stdlib.h> /* abort */
 
+#if mk_lang_crash_detail_have
+#include <stdio.h> /* puts fflush stdout */
+#endif
 
+
+#if mk_lang_crash_detail_have
+mk_lang_noreturn mk_lang_jumbo void mk_lang_crash_impl(char const* const msg) mk_lang_noexcept
+#else
 mk_lang_noreturn mk_lang_jumbo void mk_lang_crash(void) mk_lang_noexcept
+#endif
 {
+	int ta;
 	int volatile* volatile ptr;
+
+	#if mk_lang_crash_detail_have
+	ta = puts(msg); /* assert(ta != EOF && ta >= 0) */
+	ta = fflush(stdout); /* assert(ta != EOF && ta == 0) */
+	#else
+	#endif
 
 	ptr = mk_lang_null;
 	*ptr = 0;
--- mk_clib/src/mk_lang_crash.h
+++ mk_clib/src/mk_lang_crash.h
@@ -5,9 +5,22 @@
 #include "mk_lang_jumbo.h"
 #include "mk_lang_noexcept.h"
 #include "mk_lang_noreturn.h"
+#include "mk_lang_stringify.h"
 
 
+#if defined mk_lang_crash_detail_want && mk_lang_crash_detail_want == 1
+#define mk_lang_crash_detail_have 1
+#else
+#define mk_lang_crash_detail_have 0
+#endif
+
+
+#if mk_lang_crash_detail_have
+mk_lang_noreturn mk_lang_jumbo void mk_lang_crash_impl(char const* const msg) mk_lang_noexcept;
+#define mk_lang_crash() mk_lang_crash_impl("crash--->" __FILE__ ":" mk_lang_stringify(__LINE__) "<---crash")
+#else
 mk_lang_noreturn mk_lang_jumbo void mk_lang_crash(void) mk_lang_noexcept;
+#endif
 
 
 #if mk_lang_jumbo_want == 1
