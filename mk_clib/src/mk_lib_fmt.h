525e12fae38ef1335afd16e9453d990b414cf0f1
Marek Knápek <knapek.mar@gmail.com>
2023-10-06 17:42:16 +0200

    va_list

--- mk_clib/src/mk_lib_fmt.h
+++ mk_clib/src/mk_lib_fmt.h
@@ -10,7 +10,7 @@
 #include <stdarg.h> /* va_list */
 
 
-mk_lang_nodiscard mk_lang_jumbo mk_lang_types_usize_t mk_lib_fmt_v(mk_lang_types_pchar_pct const fmt, va_list const vp, mk_lang_types_pchar_pt const dst) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_usize_t mk_lib_fmt_v(mk_lang_types_pchar_pct const fmt, va_list* const vp, mk_lang_types_pchar_pt const dst) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_usize_t mk_lib_fmt(mk_lang_types_pchar_pct const fmt, mk_lang_types_pchar_pt const dst, ...) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_fmt_sprintf(mk_lang_types_pchar_pt const dst, mk_lang_types_pchar_pct const fmt, ...) mk_lang_noexcept;
 
