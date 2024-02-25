d9a89aa96f173407dc628cd3746022998949757c
Marek Knápek <knapek.mar@gmail.com>
2024-02-25 13:15:34 +0100

    skip empty arg

--- mk_clib/projects/vs2022/protect_clusters/protect_clusters.cpp
+++ mk_clib/projects/vs2022/protect_clusters/protect_clusters.cpp
@@ -103,17 +103,20 @@ mk_lang_types_sint_t wmain(mk_lang_types_sint_t const argc, mk_win_base_wchar_pc
 	mk_win_base_dword_t nwritten;
 	mk_win_base_dword_t waited;
 
-	err = enable_backup(); mk_lang_check_rereturn(err);
-	n = argc - 1;
-	for(i = 0; i != n; ++i)
+	if(argc >= 2)
 	{
-		path = argv[1 + i];
-		err = open_volume(path, &volume); mk_lang_check_rereturn(err);
-		mark.m_usn_source_info_or_copy_number = mk_win_kernel_communication_usn_source_id_e_data_management;
-		mark.m_volume = volume;
-		mark.m_handle_info = mk_win_kernel_communication_mark_handle_id_e_protect_clusters;
-		file = mk_win_kernel_files_w_create_file(path, mk_win_advapi_base_right_specific_file_e_read_attributes, mk_win_kernel_files_share_e_read | mk_win_kernel_files_share_e_write | mk_win_kernel_files_share_e_delete, mk_win_base_null, mk_win_kernel_files_create_e_open_existing, mk_win_kernel_files_flag_e_backup_semantics, mk_win_base_handle_null_g); mk_lang_check_return(file.m_data != mk_win_base_handle_invalid);
-		b = mk_win_kernel_communication_device_io_control(file, mk_win_kernel_communication_ioctl_id_e_fsctl_mark_handle, &mark, ((mk_lang_types_sint_t)(sizeof(mark))), mk_win_base_null, 0, &nwritten, mk_win_base_null); mk_lang_check_return(b != 0 && nwritten == 0);
+		err = enable_backup(); mk_lang_check_rereturn(err);
+		n = argc - 1;
+		for(i = 0; i != n; ++i)
+		{
+			path = argv[1 + i];
+			err = open_volume(path, &volume); mk_lang_check_rereturn(err);
+			mark.m_usn_source_info_or_copy_number = mk_win_kernel_communication_usn_source_id_e_data_management;
+			mark.m_volume = volume;
+			mark.m_handle_info = mk_win_kernel_communication_mark_handle_id_e_protect_clusters;
+			file = mk_win_kernel_files_w_create_file(path, mk_win_advapi_base_right_specific_file_e_read_attributes, mk_win_kernel_files_share_e_read | mk_win_kernel_files_share_e_write | mk_win_kernel_files_share_e_delete, mk_win_base_null, mk_win_kernel_files_create_e_open_existing, mk_win_kernel_files_flag_e_backup_semantics, mk_win_base_handle_null_g); mk_lang_check_return(file.m_data != mk_win_base_handle_invalid);
+			b = mk_win_kernel_communication_device_io_control(file, mk_win_kernel_communication_ioctl_id_e_fsctl_mark_handle, &mark, ((mk_lang_types_sint_t)(sizeof(mark))), mk_win_base_null, 0, &nwritten, mk_win_base_null); mk_lang_check_return(b != 0 && nwritten == 0);
+		}
+		waited = mk_win_kernel_synchronization_wait_for_single_object(mk_win_kernel_process_get_current(), mk_win_kernel_synchronization_infinite); mk_lang_check_return(0);
 	}
-	waited = mk_win_kernel_synchronization_wait_for_single_object(mk_win_kernel_process_get_current(), mk_win_kernel_synchronization_infinite); mk_lang_check_return(0);
 }
