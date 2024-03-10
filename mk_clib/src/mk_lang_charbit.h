fdf01400bad015c1cd6a483dca123479ff6e80f2
Marek Knápek <knapek.mar@gmail.com>
2024-03-10 13:41:49 +0100

    Disable warning 4668.

--- mk_clib/src/mk_lang_charbit.h
+++ mk_clib/src/mk_lang_charbit.h
@@ -3,10 +3,24 @@
 
 
 #if defined __cplusplus
+#if defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:4668)
+#endif
 #include <climits> /* CHAR_BIT */
+#if defined _MSC_FULL_VER
+#pragma warning(pop)
+#endif
 #define mk_lang_charbit CHAR_BIT
 #else
+#if defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:4668)
+#endif
 #include <limits.h> /* CHAR_BIT */
+#if defined _MSC_FULL_VER
+#pragma warning(pop)
+#endif
 #define mk_lang_charbit CHAR_BIT
 #endif
 
