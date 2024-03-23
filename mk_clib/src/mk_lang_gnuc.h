efaf12a1427f8558778677efb7cb23735a130111
Marek Knápek <knapek.mar@gmail.com>
2024-03-23 02:52:37 +0100

    FIx typo in version check.

--- mk_clib/src/mk_lang_gnuc.h
+++ mk_clib/src/mk_lang_gnuc.h
@@ -3,7 +3,7 @@
 
 
 #if defined __GNUC__ && defined __GNUC_MINOR__
-#define mk_lang_gnuc_is_at_least(major, minor) (((__GNUC__) > (major)) || (((__GNUC__) == (major)) && ((__GNUC_MINOR__) > (minor))))
+#define mk_lang_gnuc_is_at_least(major, minor) (((__GNUC__) > (major)) || (((__GNUC__) == (major)) && ((__GNUC_MINOR__) >= (minor))))
 #else
 #define mk_lang_gnuc_is_at_least(major, minor) 0
 #endif
