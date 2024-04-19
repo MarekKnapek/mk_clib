22f9a290768604569161d7cf008be969392a1ccb
Marek Knápek <knapek.mar@gmail.com>
2024-04-19 20:27:06 +0200

    Improve mk_lang_check.

--- mk_clib/src/mk_lang_check.h
+++ mk_clib/src/mk_lang_check.h
@@ -15,10 +15,10 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_bool_t mk_lang_check_to_bool_impl(
 
 #if defined NDEBUG
 #define mk_lang_check_line 1
-#define mk_lang_check_break()
+#define mk_lang_check_debug_break()
 #else
 #define mk_lang_check_line ((mk_lang_types_sint_t)(__LINE__))
-#define mk_lang_check_break() __debugbreak()
+#define mk_lang_check_debug_break() __debugbreak()
 #endif
 
 
@@ -29,10 +29,12 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_bool_t mk_lang_check_to_bool_impl(
 #endif
 
 
-#define mk_lang_check_return(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_break(); return mk_lang_check_line; } }while(0)
-#define mk_lang_check_goto_exret(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_break(); goto exret; } }while(0)
-#define mk_lang_check_crash(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_break(); mk_lang_crash(); } }while(0)
+#define mk_lang_check_return(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); return mk_lang_check_line; } }while(0)
+#define mk_lang_check_break(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); break; } } ((mk_lang_types_void_t)(0))
+#define mk_lang_check_goto_exret(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); goto exret; } }while(0)
+#define mk_lang_check_crash(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); mk_lang_crash(); } }while(0)
 #define mk_lang_check_rereturn(x) do{ mk_lang_types_sint_t err_; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely return err_; } }while(0)
+#define mk_lang_check_rebreak(x) { mk_lang_types_sint_t err_; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely break; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_regoto_exret(x) do{ mk_lang_types_sint_t err_; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely ret = err_; goto exret; } }while(0)
 
 
