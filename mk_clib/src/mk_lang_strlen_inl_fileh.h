0442d79bd4d6e640f10384d32f74d1a9df0d8270
Marek Knápek <knapek.mar@gmail.com>
2024-04-22 05:01:51 +0200

    refactor

---
+++ mk_clib/src/mk_lang_strlen_inl_fileh.h
@@ -0,0 +1,14 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_strlen_inl_defd.h"
+
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_usize_t mk_lang_strlen_inl_defd_fn(mk_lang_strlen_inl_defd_pct const str) mk_lang_noexcept;
+
+
+#include "mk_lang_strlen_inl_defu.h"
