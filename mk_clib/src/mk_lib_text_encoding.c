370ecc06574e958a8f4888d41b2e16d5f3d0d07a
Marek Knápek <knapek.mar@gmail.com>
2024-04-27 07:20:01 +0200

    Fix constexpr compilation error.

--- mk_clib/src/mk_lib_text_encoding.c
+++ mk_clib/src/mk_lib_text_encoding.c
@@ -22,9 +22,9 @@ mk_lang_nodiscard mk_lang_constexpr static mk_lang_inline mk_lang_types_sint_t m
 	mk_lang_types_sint_t i;
 	mk_lang_types_wchar_t twca;
 	mk_lang_types_ushort_t tusa;
+	mk_lang_types_ulong_t tul;
 	mk_lang_types_wchar_t twcb;
 	mk_lang_types_ushort_t tusb;
-	mk_lang_types_ulong_t tul;
 
 	mk_lang_assert(validate == mk_lang_false || validate == mk_lang_true);
 	mk_lang_assert(wide_str);
@@ -106,6 +106,16 @@ mk_lang_nodiscard mk_lang_constexpr static mk_lang_inline mk_lang_types_sint_t m
 
 mk_lang_nodiscard mk_lang_constexpr static mk_lang_inline mk_lang_types_sint_t mk_lib_text_encoding_wide_to_utf8_len(mk_lang_types_bool_t const validate, mk_lang_types_wchar_pct const wide_str, mk_lang_types_sint_t const wide_len, mk_lang_types_pchar_pt const utf8_str, mk_lang_types_sint_t const utf8_len, mk_lang_types_sint_pt const out_len) mk_lang_noexcept
 {
+#if mk_lang_sizeof_bi_wchar_t == 2 /* assuming UTF-16LE */
+	mk_lang_types_sint_t len;
+	mk_lang_types_sint_t n;
+	mk_lang_types_sint_t i;
+	mk_lang_types_wchar_t twca;
+	mk_lang_types_ushort_t tusa;
+	mk_lang_types_ulong_t tul;
+	mk_lang_types_wchar_t twcb;
+	mk_lang_types_ushort_t tusb;
+
 	mk_lang_assert(validate == mk_lang_false || validate == mk_lang_true);
 	mk_lang_assert(wide_str);
 	mk_lang_assert(wide_len >= 0);
@@ -113,9 +123,59 @@ mk_lang_nodiscard mk_lang_constexpr static mk_lang_inline mk_lang_types_sint_t m
 	mk_lang_assert(utf8_len == 0);
 	mk_lang_assert(out_len);
 
+	len = 0;
+	n = wide_len;
+	for(i = 0; i != n; ++i)
+	{
+		twca = wide_str[i];
+		tusa = ((mk_lang_types_ushort_t)(twca));
+		if(!((tusa & 0xfc00) == 0xd800))
+		{
+			tul = ((mk_lang_types_ulong_t)(tusa));
+		}
+		else
+		{
+			if(validate){ mk_lang_check_return(i + 1 != n); }else{ mk_lang_assert(i + 1 != n); }
+			++i;
+			twcb = wide_str[i];
+			tusb = ((mk_lang_types_ushort_t)(twcb));
+			if(validate){ mk_lang_check_return((tusb & 0xfc00) == 0xdc00); }else{ mk_lang_assert((tusb & 0xfc00) == 0xdc00); }
+			tul = ((mk_lang_types_ulong_t)(((mk_lang_types_ulong_t)(((mk_lang_types_ulong_t)(tusa & 0x03ff)) << 10)) | ((mk_lang_types_ulong_t)(tusb & 0x03ff))));
+		}
+		if(tul >= 0x00000000ul && tul <= 0x0000007ful)
+		{
+			++len;
+		}
+		else if(tul >= 0x00000080ul && tul <= 0x000007fful)
+		{
+			++len;
+			++len;
+		}
+		else if(tul >= 0x00000800ul && tul <= 0x0000fffful)
+		{
+			++len;
+			++len;
+			++len;
+		}
+		else if(tul >= 0x00010000ul && tul <= 0x10fffful)
+		{
+			++len;
+			++len;
+			++len;
+			++len;
+		}
+		else
+		{
+			if(validate){ mk_lang_check_return(mk_lang_false); }else{ mk_lang_assert(mk_lang_false); }
+		}
+	}
+	*out_len = len;
+	return 0;
+#else
 	mk_lang_assert(mk_lang_false); /* todo */
 	mk_lang_check_return(mk_lang_false);
 	return 0;
+#endif
 }
 
 
