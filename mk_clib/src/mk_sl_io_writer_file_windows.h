147bf0b6173df1a073e7ad74be19f4d07885e126
Marek Knápek <knapek.mar@gmail.com>
2024-04-28 11:35:41 +0200

    mk_sl_io_writer_file_windows

--- mk_clib/src/mk_sl_io_writer_file_windows.h
+++ mk_clib/src/mk_sl_io_writer_file_windows.h
@@ -7,6 +7,7 @@
 #include "mk_lang_noexcept.h"
 #include "mk_lang_restrict.h"
 #include "mk_lang_types.h"
+#include "mk_sl_io_transaction_windows.h"
 #include "mk_sl_uint8.h"
 #include "mk_win_kernel_files.h"
 
@@ -27,6 +28,8 @@ typedef mk_sl_io_writer_file_windows_t const* mk_lang_restrict mk_sl_io_writer_f
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_open_n(mk_sl_io_writer_file_windows_pt const writer, mk_lang_types_pchar_pct const name) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_open_w(mk_sl_io_writer_file_windows_pt const writer, mk_lang_types_wchar_pct const name) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_open_tx_n(mk_sl_io_writer_file_windows_pt const writer, mk_lang_types_pchar_pct const name, mk_sl_io_transaction_windows_pt const tx) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_open_tx_w(mk_sl_io_writer_file_windows_pt const writer, mk_lang_types_wchar_pct const name, mk_sl_io_transaction_windows_pt const tx) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_write(mk_sl_io_writer_file_windows_pt const writer, mk_sl_cui_uint8_pct const buf, mk_lang_types_usize_t const len, mk_lang_types_usize_pt const written) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_writer_file_windows_close(mk_sl_io_writer_file_windows_pt const writer) mk_lang_noexcept;
 
