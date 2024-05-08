753914d90f842829eb21ee819f9033c1ec125543
Marek Knápek <knapek.mar@gmail.com>
2024-05-08 18:14:57 +0200

    Better Linux / POSIX detection.

--- mk_clib/src/mk_lang_os.h
+++ mk_clib/src/mk_lang_os.h
@@ -9,7 +9,7 @@
 
 #if defined _MSC_VER && defined _MSC_FULL_VER
 #define mk_lang_os mk_lang_os_windows
-#elif (defined linux && linux == 1) || (defined __linux && __linux == 1) || (defined __linux__ && __linux__ == 1) || (defined __gnu_linux__ && __gnu_linux__ == 1)
+#elif ((defined linux && linux == 1) || (defined __linux && __linux == 1) || (defined __linux__ && __linux__ == 1) || (defined __gnu_linux__ && __gnu_linux__ == 1)) && ((defined _POSIX_C_SOURCE && _POSIX_C_SOURCE >= 200809l) && (defined _XOPEN_SOURCE && _XOPEN_SOURCE >= 700) && (defined _GNU_SOURCE))
 #define mk_lang_os mk_lang_os_linux
 #else
 #define mk_lang_os mk_lang_os_c
