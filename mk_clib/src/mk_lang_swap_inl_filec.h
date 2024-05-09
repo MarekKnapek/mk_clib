dfab08a53ae6567e5151f93decb445018c8cbbc0
Marek Knápek <knapek.mar@gmail.com>
2024-05-09 20:44:33 +0200

    mk_lang_swap

---
+++ mk_clib/src/mk_lang_swap_inl_filec.h
@@ -0,0 +1,28 @@
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+
+
+#include "mk_lang_swap_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lang_swap_inl_defd_fn(mk_lang_swap_inl_defd_prt const a, mk_lang_swap_inl_defd_prt const b) mk_lang_noexcept
+{
+	mk_lang_swap_inl_defd_t t mk_lang_constexpr_init;
+
+	mk_lang_assert(a);
+	mk_lang_assert(b);
+	mk_lang_assert(a != b);
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+
+#include "mk_lang_swap_inl_defu.h"
+
+
+#undef mk_lang_swap_t_name
+#undef mk_lang_swap_t_base
---
+++ mk_clib/src/mk_lang_swap_inl_fileh.h
@@ -0,0 +1,12 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+
+
+#include "mk_lang_swap_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lang_swap_inl_defd_fn(mk_lang_swap_inl_defd_prt const a, mk_lang_swap_inl_defd_prt const b) mk_lang_noexcept;
+
+
+#include "mk_lang_swap_inl_defu.h"
