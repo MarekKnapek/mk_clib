604ba9fe94547584fd8d9d89f8da8f6785a672da
Marek Knápek <knapek.mar@gmail.com>
2024-05-12 10:00:56 +0200

    Much better overlap.

--- mk_clib/src/mk_lang_overlap_inl_filec.h
+++ mk_clib/src/mk_lang_overlap_inl_filec.h
@@ -20,17 +20,26 @@ mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_o
 	mk_lang_assert(len2 >= 0);
 	mk_lang_assert(arr1 != arr2);
 
-	for(it1 = 0; it1 != len1; ++it1)
+	if(mk_lang_constexpr_is_constant_evaluated_test)
 	{
-		for(it2 = 0; it2 != len2; ++it2)
+		for(it1 = 0; it1 != len1; ++it1)
 		{
-			if(arr1 + it1 == arr2 + it2)
+			for(it2 = 0; it2 != len2; ++it2)
 			{
-				return mk_lang_true;
+				if(arr1 + it1 == arr2 + it2)
+				{
+					return mk_lang_true;
+				}
 			}
 		}
+		return mk_lang_false;
 	}
-	return mk_lang_false;
+	else
+	{
+		return
+			(arr1 >= arr2 && arr1 < arr2 + len2) ||
+			(arr2 >= arr1 && arr2 < arr1 + len1);
+	}
 }
 
 
