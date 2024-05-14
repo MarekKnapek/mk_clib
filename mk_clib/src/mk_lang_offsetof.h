cc8f56864bb06480b7d94c990458b8129cee85be
Marek Knápek <knapek.mar@gmail.com>
2024-05-14 03:45:40 +0200

    mk_lang_offsetof

---
+++ mk_clib/src/mk_lang_offsetof.h
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_mk_lang_offsetof
+#define mk_include_guard_mk_lang_offsetof
+
+
+#if defined __cplusplus
+#include <cstddef> /* offsetof */
+#else
+#include <stddef.h> /* offsetof */
+#endif
+
+
+#define mk_lang_offsetof offsetof
+
+
+#endif
