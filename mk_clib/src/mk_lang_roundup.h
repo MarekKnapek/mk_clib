f02ce0261eac5d680a56305699a9d3154840a895
Marek Knápek <knapek.mar@gmail.com>
2024-05-14 02:31:41 +0200

    mk_lang_roundup

---
+++ mk_clib/src/mk_lang_roundup.h
@@ -0,0 +1,12 @@
+#ifndef mk_include_guard_mk_lang_roundup
+#define mk_include_guard_mk_lang_roundup
+
+
+#include "mk_lang_types.h"
+
+
+#define mk_lang_roundup_roundup(a, b) ((((a) + ((b) - 1)) / (b)) * (b))
+#define mk_lang_roundup_align_ptr(a, b) ((mk_lang_types_void_pt)(mk_lang_roundup_roundup(((mk_lang_types_uintptr_t)((a))), (b))))
+
+
+#endif
