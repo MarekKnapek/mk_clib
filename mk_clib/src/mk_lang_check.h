3729201d5de4a92128ebc7803d0ceca76b711f36
Marek Knápek <knapek.mar@gmail.com>
2024-05-16 01:36:01 +0200

    Refactor mk_lang_check.

--- mk_clib/src/mk_lang_check.h
+++ mk_clib/src/mk_lang_check.h
@@ -37,14 +37,14 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_bool_t mk_lang_check_to_bool_impl(
 #endif
 
 
-#define mk_lang_check_return(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); return mk_lang_check_line; } }while(0)
+#define mk_lang_check_return(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); return mk_lang_check_line; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_break(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); break; } } ((mk_lang_types_void_t)(0))
-#define mk_lang_check_goto_exret(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); goto exret; } }while(0)
-#define mk_lang_check_crash(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); mk_lang_crash(); } }while(0)
+#define mk_lang_check_goto_exret(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); goto exret; } } ((mk_lang_types_void_t)(0))
+#define mk_lang_check_crash(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); mk_lang_crash(); } } ((mk_lang_types_void_t)(0))
 
-#define mk_lang_check_rereturn(x) do{ mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely return err_; } }while(0)
+#define mk_lang_check_rereturn(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely return err_; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_rebreak(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely break; } } ((mk_lang_types_void_t)(0))
-#define mk_lang_check_regoto_exret(x) do{ mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely ret = err_; goto exret; } }while(0)
+#define mk_lang_check_regoto_exret(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely ret = err_; goto exret; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_recrash(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely mk_lang_crash(); } } ((mk_lang_types_void_t)(0))
 
 
