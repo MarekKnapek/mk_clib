d2c49cca9ab04f1c90352ea5b4abde89463c0631
Marek Knápek <knapek.mar@gmail.com>
2024-05-16 01:34:00 +0200

    mk_lang_check_recrash

--- mk_clib/src/mk_lang_check.h
+++ mk_clib/src/mk_lang_check.h
@@ -41,9 +41,11 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_bool_t mk_lang_check_to_bool_impl(
 #define mk_lang_check_break(x) { if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); break; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_goto_exret(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); goto exret; } }while(0)
 #define mk_lang_check_crash(x) do{ if(!mk_lang_check_to_bool(x)){ mk_lang_unlikely mk_lang_check_debug_break(); mk_lang_crash(); } }while(0)
+
 #define mk_lang_check_rereturn(x) do{ mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely return err_; } }while(0)
 #define mk_lang_check_rebreak(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely break; } } ((mk_lang_types_void_t)(0))
 #define mk_lang_check_regoto_exret(x) do{ mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely ret = err_; goto exret; } }while(0)
+#define mk_lang_check_recrash(x) { mk_lang_types_sint_t err_ mk_lang_constexpr_init; err_ = ((mk_lang_types_sint_t)(x)); if(err_ != 0){ mk_lang_unlikely mk_lang_crash(); } } ((mk_lang_types_void_t)(0))
 
 
 #if mk_lang_jumbo_want == 1
