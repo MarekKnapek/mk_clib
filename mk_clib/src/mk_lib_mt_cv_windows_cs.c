a1b7515a76f33271870f6c72d58587d9676cea57
Marek Knápek <knapek.mar@gmail.com>
2024-05-16 10:53:42 +0200

    mk_lib_mt_cv_windows_cs

---
+++ mk_clib/src/mk_lib_mt_cv_windows_cs.c
@@ -0,0 +1,68 @@
+#include "mk_lib_mt_cv_windows_cs.h"
+
+#include "mk_lang_assert.h"
+#include "mk_lang_check.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+#include "mk_lib_mt_unique_lock_windows_cs.h"
+#include "mk_win_base.h"
+#include "mk_win_kernel_cv.h"
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_construct(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept
+{
+	mk_lang_assert(cv);
+
+	mk_win_kernel_cv_construct(&cv->m_cv);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_destruct(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept
+{
+	mk_lang_assert(cv);
+
+	mk_win_kernel_cv_destroy(&cv->m_cv);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_wait_exclusive(mk_lib_mt_cv_windows_cs_pt const cv, mk_lib_mt_unique_lock_exclusive_windows_cs_pt const lock) mk_lang_noexcept
+{
+	mk_win_base_bool_t ret;
+
+	mk_lang_assert(cv);
+	mk_lang_assert(lock);
+	mk_lang_assert(lock->m_mutex);
+
+	ret = mk_win_kernel_cv_wait_cs(&cv->m_cv, &lock->m_mutex->m_mutex, mk_win_base_infinite); mk_lang_check_return(ret != mk_win_base_false);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_wait_shared(mk_lib_mt_cv_windows_cs_pt const cv, mk_lib_mt_unique_lock_shared_windows_cs_pt const lock) mk_lang_noexcept
+{
+	mk_win_base_bool_t ret;
+
+	mk_lang_assert(cv);
+	mk_lang_assert(lock);
+	mk_lang_assert(lock->m_mutex);
+
+	ret = mk_win_kernel_cv_wait_cs(&cv->m_cv, &lock->m_mutex->m_mutex, mk_win_base_infinite); mk_lang_check_return(ret != mk_win_base_false);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_notify_one(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept
+{
+	mk_lang_assert(cv);
+
+	mk_win_kernel_cv_notify_one(&cv->m_cv);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_notify_all(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept
+{
+	mk_lang_assert(cv);
+
+	mk_win_kernel_cv_notify_all(&cv->m_cv);
+	return 0;
+}
---
+++ mk_clib/src/mk_lib_mt_cv_windows_cs.h
@@ -0,0 +1,34 @@
+#ifndef mk_include_guard_mk_lib_mt_cv_windows_cs
+#define mk_include_guard_mk_lib_mt_cv_windows_cs
+
+
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+#include "mk_lib_mt_unique_lock_windows_cs.h"
+#include "mk_win_kernel_cv.h"
+
+
+struct mk_lib_mt_cv_windows_cs_s
+{
+	mk_win_kernel_cv_t m_cv;
+};
+typedef struct mk_lib_mt_cv_windows_cs_s mk_lib_mt_cv_windows_cs_t;
+typedef mk_lib_mt_cv_windows_cs_t const mk_lib_mt_cv_windows_cs_ct;
+typedef mk_lib_mt_cv_windows_cs_t* mk_lib_mt_cv_windows_cs_pt;
+typedef mk_lib_mt_cv_windows_cs_t const* mk_lib_mt_cv_windows_cs_pct;
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_construct(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_destruct(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_wait_exclusive(mk_lib_mt_cv_windows_cs_pt const cv, mk_lib_mt_unique_lock_exclusive_windows_cs_pt const lock) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_wait_shared(mk_lib_mt_cv_windows_cs_pt const cv, mk_lib_mt_unique_lock_shared_windows_cs_pt const lock) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_notify_one(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_cv_windows_cs_notify_all(mk_lib_mt_cv_windows_cs_pt const cv) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_want == 1
+#include "mk_lib_mt_cv_windows_cs.c"
+#endif
+#endif
