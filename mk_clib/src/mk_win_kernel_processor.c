40c26597f2153b2623b6ecb1214d632ec22b13c9
Marek Knápek <knapek.mar@gmail.com>
2024-05-17 06:30:07 +0200

    SetThreadIdealProcessorEx

--- mk_clib/src/mk_win_kernel_processor.c
+++ mk_clib/src/mk_win_kernel_processor.c
@@ -11,6 +11,7 @@
 
 mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_word_t mk_win_base_stdcall GetActiveProcessorGroupCount(mk_lang_types_void_t) mk_lang_noexcept;
 mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_dword_t mk_win_base_stdcall GetActiveProcessorCount(mk_win_base_word_t const group_idx) mk_lang_noexcept;
+mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall SetThreadIdealProcessorEx(mk_win_base_handle_t const thread, mk_win_kernel_processor_number_pct const ideal_processor_new, mk_win_kernel_processor_number_pt const ideal_processor_old) mk_lang_noexcept;
 
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_word_t mk_win_kernel_processor_group_active_count(mk_lang_types_void_t) mk_lang_noexcept
@@ -26,5 +27,16 @@ mk_lang_nodiscard mk_lang_jumbo mk_win_base_dword_t mk_win_kernel_processor_acti
 	mk_win_base_dword_t ret;
 
 	ret = GetActiveProcessorCount(group_idx);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_kernel_processor_set_ideal(mk_win_base_handle_t const thread, mk_win_kernel_processor_number_pct const ideal_processor_new, mk_win_kernel_processor_number_pt const ideal_processor_old) mk_lang_noexcept
+{
+	mk_win_base_bool_t ret;
+
+	mk_lang_assert(thread.m_data);
+	mk_lang_assert(ideal_processor_new);
+
+	ret = SetThreadIdealProcessorEx(thread, ideal_processor_new, ideal_processor_old);
 	return ret;
 }
--- mk_clib/src/mk_win_kernel_processor.h
+++ mk_clib/src/mk_win_kernel_processor.h
@@ -9,8 +9,21 @@
 #include "mk_win_base.h"
 
 
+struct mk_win_kernel_processor_number_s
+{
+	mk_win_base_word_t m_group;
+	mk_win_base_uchar_t m_number;
+	mk_win_base_uchar_t m_reserved;
+};
+typedef struct mk_win_kernel_processor_number_s mk_win_kernel_processor_number_t;
+typedef mk_win_kernel_processor_number_t const mk_win_kernel_processor_number_ct;
+typedef mk_win_kernel_processor_number_t* mk_win_kernel_processor_number_pt;
+typedef mk_win_kernel_processor_number_t const* mk_win_kernel_processor_number_pct;
+
+
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_word_t mk_win_kernel_processor_group_active_count(mk_lang_types_void_t) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_dword_t mk_win_kernel_processor_active_count(mk_win_base_word_t const group_idx) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_kernel_processor_set_ideal(mk_win_base_handle_t const thread, mk_win_kernel_processor_number_pct const ideal_processor_new, mk_win_kernel_processor_number_pt const ideal_processor_old) mk_lang_noexcept;
 
 
 #if mk_lang_jumbo_want == 1
