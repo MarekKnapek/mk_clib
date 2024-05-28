04f4a06f60d17957cbe43ff74e0156596976c15f
Marek Knápek <knapek.mar@gmail.com>
2024-05-29 01:01:23 +0200

    __restrict

--- mk_clib/src/mk_lang_restrict.h
+++ mk_clib/src/mk_lang_restrict.h
@@ -2,11 +2,14 @@
 #define mk_include_guard_mk_lang_restrict
 
 
+#include "mk_lang_msvc.h"
 #include "mk_lang_version.h"
 
 
 #if mk_lang_version_at_least_c_99
 #define mk_lang_restrict restrict
+#elif mk_lang_msvc_ver >= mk_lang_msvc_ver_2005
+#define mk_lang_restrict __restrict
 #else
 #define mk_lang_restrict
 #endif
