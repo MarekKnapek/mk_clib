3b47e0639cbe595424a0f7a977b7b8469d67fa68
Marek Knápek <knapek.mar@gmail.com>
2024-05-30 17:27:05 +0200

    mk_lib_mt_ring::push_back_many

--- mk_clib/src/mk_lib_mt_ring_inl_defu.h
+++ mk_clib/src/mk_lib_mt_ring_inl_defu.h
@@ -55,6 +55,7 @@
 
 #undef mk_lib_mt_ring_inl_defd_ring_rw_construct
 #undef mk_lib_mt_ring_inl_defd_ring_rw_push_back_one
+#undef mk_lib_mt_ring_inl_defd_ring_rw_push_back_many
 #undef mk_lib_mt_ring_inl_defd_ring_rw_pop_back
 #undef mk_lib_mt_ring_inl_defd_ring_rw_push_front
 #undef mk_lib_mt_ring_inl_defd_ring_rw_pop_front_one
@@ -102,6 +103,7 @@
 #undef mk_lib_mt_ring_inl_defd_rw_construct
 #undef mk_lib_mt_ring_inl_defd_rw_destroy
 #undef mk_lib_mt_ring_inl_defd_rw_push_back_one
+#undef mk_lib_mt_ring_inl_defd_rw_push_back_many
 #undef mk_lib_mt_ring_inl_defd_rw_pop_back_void
 #undef mk_lib_mt_ring_inl_defd_rw_pop_back_copy
 #undef mk_lib_mt_ring_inl_defd_rw_push_front
