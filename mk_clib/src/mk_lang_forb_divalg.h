fe984ce1488fda2818a5851b2a0be09bd61b74e1
Marek Knápek <knapek.mar@gmail.com>
2024-05-30 15:10:33 +0200

    Remove obsolete files.

--- mk_clib/src/mk_lang_forb_divalg.h
+++
@@ -1,9 +0,0 @@
-#define mk_lang_forb_divalg_id 2
-#include mk_lang_forb_divalg_file
-#undef mk_lang_forb_divalg_id
-
-#define mk_lang_forb_divalg_id 3
-#include mk_lang_forb_divalg_file
-#undef mk_lang_forb_divalg_id
-
-#undef mk_lang_forb_divalg_file
