5069faf64f362d7aa423db9dcfbd2643e77f9090
Marek Knápek <knapek.mar@gmail.com>
2024-05-30 15:12:53 +0200

    Refactor roundup.

--- mk_clib/src/mk_lang_roundup.h
+++ mk_clib/src/mk_lang_roundup.h
@@ -5,7 +5,8 @@
 #include "mk_lang_types.h"
 
 
-#define mk_lang_roundup_roundup(a, b) ((((a) + ((b) - 1)) / (b)) * (b))
+#define mk_lang_roundup_size(a, b) ((a) + ((b) - 1))
+#define mk_lang_roundup_roundup(a, b) ((mk_lang_roundup_size((a), (b)) / (b)) * (b))
 #define mk_lang_roundup_align_ptr(a, b) ((mk_lang_types_void_pt)(mk_lang_roundup_roundup(((mk_lang_types_uintptr_t)((a))), (b))))
 
 
