6dc8381974260c00deb65b2866242f32d8baf5dd
Marek Knápek <knapek.mar@gmail.com>
2024-05-31 06:49:57 +0200

    Fix variable declaration in C89 style.

--- mk_clib/src/mk_win_kernel_synchronization.c
+++ mk_clib/src/mk_win_kernel_synchronization.c
@@ -13,9 +13,9 @@ mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_dword_t mk
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_dword_t mk_win_kernel_synchronization_wait_one(mk_win_base_handle_t const object, mk_win_base_dword_t const timeout) mk_lang_noexcept
 {
-	mk_lang_assert(object.m_data);
-
 	mk_win_base_dword_t ret;
+
+	mk_lang_assert(object.m_data);
 
 	ret = WaitForSingleObject(object, timeout);
 	return ret;
