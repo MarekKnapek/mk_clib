1bb8116a03180e4f1295afdf4b457212d46b3dba
Marek Knápek <knapek.mar@gmail.com>
2024-06-26 05:09:36 +0200

    LookupPrivilegeValueA

--- mk_clib/src/mk_win_advapi_security.c
+++ mk_clib/src/mk_win_advapi_security.c
@@ -10,6 +10,7 @@
 
 
 mk_lang_nodiscard mk_lang_extern_c mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall OpenProcessToken(mk_win_base_handle_t const process, mk_win_base_dword_t const desired_access, mk_win_base_handle_pt const token) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_extern_c mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall LookupPrivilegeValueA(mk_win_base_pchar_lpct const system_name, mk_win_base_pchar_lpct const privilege_name, mk_win_base_luid_pt const luid) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_extern_c mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall LookupPrivilegeValueW(mk_win_base_wchar_lpct const system_name, mk_win_base_wchar_lpct const privilege_name, mk_win_base_luid_pt const luid) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_extern_c mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall AdjustTokenPrivileges(mk_win_base_handle_t const token, mk_win_base_bool_t const disable_all, mk_win_advapi_security_token_privileges_pct const new_state, mk_win_base_dword_t const old_len, mk_win_advapi_security_token_privileges_pt const old_state, mk_win_base_dword_pt const nwritten) mk_lang_noexcept;
 
@@ -19,6 +20,14 @@ mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_advapi_security_open_p
 	mk_win_base_bool_t r;
 
 	r = OpenProcessToken(process, desired_access, token);
+	return r;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_advapi_security_lookup_privilege_value_a(mk_win_base_pchar_lpct const system_name, mk_win_base_pchar_lpct const privilege_name, mk_win_base_luid_pt const luid) mk_lang_noexcept
+{
+	mk_win_base_bool_t r;
+
+	r = LookupPrivilegeValueA(system_name, privilege_name, luid);
 	return r;
 }
 
