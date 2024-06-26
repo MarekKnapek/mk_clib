c48d6530a5630f4138db4bcc78d90327c6947c3f
Marek Knápek <knapek.mar@gmail.com>
2024-06-26 05:46:01 +0200

    Faster GetCurrentProcess.

--- mk_clib/src/mk_win_kernel_process.c
+++ mk_clib/src/mk_win_kernel_process.c
@@ -14,10 +14,17 @@ mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_wchar_lpt 
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_handle_t mk_win_kernel_process_get_current(mk_lang_types_void_t) mk_lang_noexcept
 {
+#if defined NDEBUG
+	mk_win_base_handle_t ret;
+
+	ret.m_data = ((mk_win_base_void_pct)(((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(((mk_lang_types_sint_t)(-1))))))));
+	return ret;
+#else
 	mk_win_base_handle_t ret;
 
 	ret = GetCurrentProcess();
 	return ret;
+#endif
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_wchar_lpt mk_win_kernel_process_get_command_line(mk_lang_types_void_t) mk_lang_noexcept
