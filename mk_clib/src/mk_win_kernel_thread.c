058bd64cfa2380c5f320e9f1f890366cc93de652
Marek Knápek <knapek.mar@gmail.com>
2024-06-28 07:21:13 +0200

    GetCurrentThread

--- mk_clib/src/mk_win_kernel_thread.c
+++ mk_clib/src/mk_win_kernel_thread.c
@@ -10,8 +10,24 @@
 #include "mk_win_base.h"
 
 
+mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_handle_t mk_win_base_stdcall GetCurrentThread(mk_lang_types_void_t) mk_lang_noexcept;
 mk_lang_extern_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_handle_t mk_win_base_stdcall CreateThread(mk_win_advapi_base_security_attributes_lpct const sa, mk_win_base_usize_t const stack_len, mk_win_kernel_thread_callback_t const callback, mk_win_base_void_lpt const parameter, mk_win_base_dword_t const flags, mk_win_base_dword_lpt const tid) mk_lang_noexcept;
 
+
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_handle_t mk_win_kernel_thread_get_current(mk_lang_types_void_t) mk_lang_noexcept
+{
+#if defined NDEBUG
+	mk_win_base_handle_t ret;
+
+	ret.m_data = ((mk_win_base_void_pct)(((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(((mk_lang_types_sint_t)(-2))))))));
+	return ret;
+#else
+	mk_win_base_handle_t ret;
+
+	ret = GetCurrentThread();
+	return ret;
+#endif
+}
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_handle_t mk_win_kernel_thread_create(mk_win_advapi_base_security_attributes_lpct const sa, mk_win_base_usize_t const stack_len, mk_win_kernel_thread_callback_t const callback, mk_win_base_void_lpt const parameter, mk_win_base_dword_t const flags, mk_win_base_dword_lpt const tid) mk_lang_noexcept
 {
