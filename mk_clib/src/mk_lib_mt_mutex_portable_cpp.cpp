8554c068ee563dc94f89c9e164873273e7ae26f2
Marek Knápek <knapek.mar@gmail.com>
2024-07-31 14:51:38 +0200

    Improve mt_portable_cpp.

--- mk_clib/src/mk_lib_mt_mutex_portable_cpp.cpp
+++ mk_clib/src/mk_lib_mt_mutex_portable_cpp.cpp
@@ -13,48 +13,84 @@
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_construct(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	::new(static_cast<mk_lang_types_void_pt>(&mutex->m_mutex))(std::mutex)();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	::new(static_cast<mk_lang_types_void_pt>(real))(mtx_t)();
 	return 0;
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_destruct(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	reinterpret_cast<std::mutex*>(&mutex->m_mutex)->~mutex();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	real->~mutex();
 	return 0;
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_exclusive_lock(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	reinterpret_cast<std::mutex*>(&mutex->m_mutex)->lock();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	real->lock();
 	return 0;
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_exclusive_unlock(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	reinterpret_cast<std::mutex*>(&mutex->m_mutex)->unlock();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	real->unlock();
 	return 0;
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_shared_lock(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	reinterpret_cast<std::mutex*>(&mutex->m_mutex)->lock();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	real->lock();
 	return 0;
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_mt_mutex_portable_cpp_shared_unlock(mk_lib_mt_mutex_portable_cpp_pt const mutex) mk_lang_noexcept
 {
+	typedef std::mutex mtx_t;
+	typedef mtx_t* mtx_pt;
+
+	mtx_pt real;
+
 	mk_lang_assert(mutex);
 
-	reinterpret_cast<std::mutex*>(&mutex->m_mutex)->unlock();
+	real = reinterpret_cast<mtx_pt>(&mutex->m_mutex);
+	real->unlock();
 	return 0;
 }
