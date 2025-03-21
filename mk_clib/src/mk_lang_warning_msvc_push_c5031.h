b743e56543c520a2b76840f4276cc18d56e9cc4a
Marek Knápek <knapek.mar@gmail.com>
2025-03-22 00:25:00 +0100

    mk_lang_warning

---
+++ mk_clib/src/mk_lang_warning_msvc_push_c5031.h
@@ -0,0 +1,4 @@
+#if defined _MSC_VER && defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:5031) /* warning C5031: #pragma warning(pop): likely mismatch, popping warning state pushed in different file */
+#endif
