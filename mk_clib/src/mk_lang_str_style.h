163cc06f4c685cf7784492ceeff0f5351a005bf3
Marek Knápek <knapek.mar@gmail.com>
2025-03-23 00:22:00 +0100

    mk_lang_str_style

---
+++ mk_clib/src/mk_lang_str_style.h
@@ -0,0 +1,10 @@
+#ifndef mk_include_guard_mk_lang_str_style_h
+#define mk_include_guard_mk_lang_str_style_h
+
+
+#define mk_lang_str_style_table                  1001
+#define mk_lang_str_style_arithmetic             1002
+#define mk_lang_str_style_arithmetic_if_possible 1003
+
+
+#endif
