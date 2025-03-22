09fcf08c61a4ad9666dd483d4fe957ea1d0bea65
Marek Knápek <knapek.mar@gmail.com>
2025-03-22 02:08:00 +0100

    mk_lib_crypto_hash_sha3_384

--- mk_clib/src/mk_clib_fuzz.c
+++ mk_clib/src/mk_clib_fuzz.c
@@ -26,6 +26,7 @@
 #include "mk_lib_crypto_hash_stream_sha2_512_fuzz.h"
 #include "mk_lib_crypto_hash_stream_sha3_224_fuzz.h"
 #include "mk_lib_crypto_hash_stream_sha3_256_fuzz.h"
+#include "mk_lib_crypto_hash_stream_sha3_384_fuzz.h"
 #include "mk_sl_cui_fuzz.h"
 
 
@@ -74,6 +75,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_clib_fuzz_fn(mk_
 	if(all || curr == ((mk_lang_types_uint_t)(__LINE__))){ err = mk_lib_crypto_hash_stream_sha2_512_fuzz(d, s); mk_lang_check_rereturn(err); }
 	if(all || curr == ((mk_lang_types_uint_t)(__LINE__))){ err = mk_lib_crypto_hash_stream_sha3_224_fuzz(d, s); mk_lang_check_rereturn(err); }
 	if(all || curr == ((mk_lang_types_uint_t)(__LINE__))){ err = mk_lib_crypto_hash_stream_sha3_256_fuzz(d, s); mk_lang_check_rereturn(err); }
+	if(all || curr == ((mk_lang_types_uint_t)(__LINE__))){ err = mk_lib_crypto_hash_stream_sha3_384_fuzz(d, s); mk_lang_check_rereturn(err); }
 	if(all || curr == ((mk_lang_types_uint_t)(__LINE__))){ mk_sl_cui_fuzz(d, s, allow_all); }
 	return 0;
 }
