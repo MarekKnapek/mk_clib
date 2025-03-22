f2f5d8dd033e9b5bc18b33ad63d2051233fe39c5
Marek Knápek <knapek.mar@gmail.com>
2025-03-22 01:47:00 +0100

    mk_lib_crypto_bitops_bulk

---
+++ mk_clib/src/mk_lib_crypto_bitops_bulk.c
@@ -0,0 +1,54 @@
+#ifndef mk_include_guard_mk_lib_crypto_bitops_bulk_c
+#define mk_include_guard_mk_lib_crypto_bitops_bulk_c
+#include "mk_lib_crypto_bitops_bulk.h"
+
+#include "mk_lang_jumbo.h"
+#include "mk_sl_cui_uint32.h"
+#include "mk_sl_cui_uint64.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_16
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 16
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_32
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 32
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_64
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 64
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_128
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 128
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint32_8
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint32
+#define mk_lib_crypto_bitops_bulk_t_count 8
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint64_8
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint64
+#define mk_lib_crypto_bitops_bulk_t_count 8
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint64_25
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint64
+#define mk_lib_crypto_bitops_bulk_t_count 25
+#include "mk_lib_crypto_bitops_bulk_inl_filec.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+
+#endif
---
+++ mk_clib/src/mk_lib_crypto_bitops_bulk.h
@@ -0,0 +1,56 @@
+#ifndef mk_include_guard_mk_lib_crypto_bitops_bulk_h
+#define mk_include_guard_mk_lib_crypto_bitops_bulk_h
+
+#include "mk_lang_jumbo.h"
+#include "mk_sl_cui_uint32.h"
+#include "mk_sl_cui_uint64.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_16
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 16
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_32
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 32
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_64
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 64
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint8_128
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint8
+#define mk_lib_crypto_bitops_bulk_t_count 128
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint32_8
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint32
+#define mk_lib_crypto_bitops_bulk_t_count 8
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint64_8
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint64
+#define mk_lib_crypto_bitops_bulk_t_count 8
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+#define mk_lib_crypto_bitops_bulk_t_name mk_lib_crypto_bitops_bulk_uint64_25
+#define mk_lib_crypto_bitops_bulk_t_tn mk_sl_cui_uint64
+#define mk_lib_crypto_bitops_bulk_t_count 25
+#include "mk_lib_crypto_bitops_bulk_inl_fileh.h"
+#include "mk_lib_crypto_bitops_bulk_inl_fileu.h"
+
+
+#if mk_lang_jumbo_have == 1
+#include "mk_lib_crypto_bitops_bulk.c"
+#endif
+#endif
