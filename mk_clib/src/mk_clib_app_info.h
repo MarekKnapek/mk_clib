668aa37aeda70262cc53004b1bcbb3cc4331fcb8
Marek Knápek <knapek.mar@gmail.com>
2025-03-22 01:08:00 +0100

    mk_clib_app_info

---
+++ mk_clib/src/mk_clib_app_info.h
@@ -0,0 +1,16 @@
+#ifndef mk_include_guard_mk_clib_app_info_h
+#define mk_include_guard_mk_clib_app_info_h
+
+
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+mk_lang_jumbo mk_lang_types_sint_t mk_clib_app_info_void(mk_lang_types_void_t) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_have == 1
+#include "mk_clib_app_info.c"
+#endif
+#endif
