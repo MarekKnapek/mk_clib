838340537e229c7c91c25a69e712184c86d2537b
Marek Knápek <knapek.mar@gmail.com>
2025-03-23 00:25:00 +0100

    mk_lang_warning

---
+++ mk_clib/src/mk_lang_warning_msvc_push_c4746.h
@@ -0,0 +1,5 @@
+#include "mk_lang_warning_msvc_push_c5031.h"
+#if defined _MSC_VER && defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:4746) /* warning C4746: volatile access of 'xxx' is subject to /volatile:<iso|ms> setting; consider using __iso_volatile_load/store intrinsic functions */
+#endif
---
+++ mk_clib/src/mk_lang_warning_msvc_push_c5045.h
@@ -0,0 +1,5 @@
+#include "mk_lang_warning_msvc_push_c5031.h"
+#if defined _MSC_VER && defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:5045) /* warning C5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified */
+#endif
