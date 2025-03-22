04e9506a38ea76e1d0312a84b1f3e9f32c462fb0
Marek Knápek <knapek.mar@gmail.com>
2025-03-22 01:53:00 +0100

    mk_lib_crypto_hash_md4

--- mk_clib/src/mk_clib_test.c
+++ mk_clib/src/mk_clib_test.c
@@ -8,12 +8,14 @@
 
 #include "mk_lang_sizeof_test.h"
 #include "mk_lib_crypto_hash_stream_md2_test.hpp"
+#include "mk_lib_crypto_hash_stream_md4_test.hpp"
 
 
 mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_noexcept
 {
 	mk_lang_sizeof_test();
 	mk_lib_crypto_hash_stream_md2_test();
+	mk_lib_crypto_hash_stream_md4_test();
 }
 
 
