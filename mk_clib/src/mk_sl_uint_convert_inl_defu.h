ac68f2d527e8f38e3338fcc60e8527429f09de99
Marek Knápek <knapek.mar@gmail.com>
2025-03-23 01:48:00 +0100

    mk_sl_uint_convert

---
+++ mk_clib/src/mk_sl_uint_convert_inl_defu.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_sl_uint_convert_inl_defd_name
+#undef mk_sl_uint_convert_inl_defd_tn
+
+
+#undef mk_sl_uint_convert_inl_defd_type_t
+#undef mk_sl_uint_convert_inl_defd_type_ct
+#undef mk_sl_uint_convert_inl_defd_type_pt
+#undef mk_sl_uint_convert_inl_defd_type_pct
+
+#undef mk_sl_uint_convert_inl_defd_type_assign
+
+#undef mk_sl_uint_convert_inl_defd_fn
---
+++ mk_clib/src/mk_sl_uint_convert_inl_fileh.h
@@ -0,0 +1,15 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_static_param.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_sl_uint_convert_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_sml(mk_sl_uint_convert_inl_defd_big_pct const big, mk_lang_static_param(mk_sl_uint_convert_inl_defd_sml_t, sml, mk_sl_uint_convert_inl_defd_big_size_bits_v / mk_sl_uint_convert_inl_defd_sml_size_bits_v)) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_big(mk_sl_uint_convert_inl_defd_big_pt const big, mk_lang_static_param(mk_sl_uint_convert_inl_defd_sml_ct, sml, mk_sl_uint_convert_inl_defd_big_size_bits_v / mk_sl_uint_convert_inl_defd_sml_size_bits_v)) mk_lang_noexcept;
+
+
+#include "mk_sl_uint_convert_inl_defu.h"
