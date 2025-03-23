f2d97ba472b263f19d173fd9c0ad715895f6c5c5
Marek Knápek <knapek.mar@gmail.com>
2025-03-23 01:44:00 +0100

    mk_sl_cui

---
+++ mk_clib/src/mk_sl_cui_fuzz_fromchars_inl_fileu.h
@@ -0,0 +1,5 @@
+#undef mk_sl_cui_fuzz_fromchars_t_name
+#undef mk_sl_cui_fuzz_fromchars_t_a_tn
+#undef mk_sl_cui_fuzz_fromchars_t_b_tn
+#undef mk_sl_cui_fuzz_fromchars_t_endian
+#undef mk_sl_cui_fuzz_fromchars_t_str_style
---
+++ mk_clib/src/mk_sl_cui_fuzz_printf_inl_fileu.h
@@ -0,0 +1,5 @@
+#undef mk_sl_cui_fuzz_printf_t_name
+#undef mk_sl_cui_fuzz_printf_t_a_tn
+#undef mk_sl_cui_fuzz_printf_t_b_tn
+#undef mk_sl_cui_fuzz_printf_t_endian
+#undef mk_sl_cui_fuzz_printf_t_str_style
