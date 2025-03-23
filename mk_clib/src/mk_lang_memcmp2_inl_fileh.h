7466de6388cb8cb59b7bad7cbc0548298dc8c744
Marek Knápek <knapek.mar@gmail.com>
2025-03-23 01:18:00 +0100

    mk_lang_memcmp2

---
+++ mk_clib/src/mk_lang_memcmp2_inl_fileh.h
@@ -0,0 +1,14 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_memcmp2_inl_defd.h"
+
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_sint_t mk_lang_memcmp2_inl_defd_fn(mk_lang_memcmp2_inl_defd_type_pct const bufa, mk_lang_memcmp2_inl_defd_type_pct const bufb, mk_lang_types_usize_t const count) mk_lang_noexcept;
+
+
+#include "mk_lang_memcmp2_inl_defu.h"
---
+++ mk_clib/src/mk_lang_memcmp2_inl_fileu.h
@@ -0,0 +1,2 @@
+#undef mk_lang_memcmp2_t_name
+#undef mk_lang_memcmp2_t_tn
