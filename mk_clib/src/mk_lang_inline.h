bd8e9af3e2ea5ac08ff8c063d8c078ddf998c6d0
Marek Knápek <knapek.mar@gmail.com>
2025-03-24 00:34:00 +0100

    mk_lang_inline

---
+++ mk_clib/src/mk_lang_inline.h
@@ -0,0 +1,20 @@
+#ifndef mk_include_guard_mk_lang_inline_h
+#define mk_include_guard_mk_lang_inline_h
+
+
+#include "mk_lang_compiler.h"
+#include "mk_lang_version.h"
+
+
+#if mk_lang_version_at_least_cpp_98
+#define mk_lang_inline inline
+#elif mk_lang_version_at_least_c_99
+#define mk_lang_inline inline
+#elif mk_lang_compiler_is_at_least_watcom(11, 0) /* todo maybe earlier */
+#define mk_lang_inline __inline
+#else
+#define mk_lang_inline
+#endif
+
+
+#endif
