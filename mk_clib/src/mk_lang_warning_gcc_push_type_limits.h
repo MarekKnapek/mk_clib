ad97b4c9aaa8375377aeaeb2c2972d477ba8ab68
Marek Knápek <knapek.mar@gmail.com>
2025-03-24 00:25:00 +0100

    mk_lang_warning

---
+++ mk_clib/src/mk_lang_warning_gcc_push_type_limits.h
@@ -0,0 +1,4 @@
+#if defined __GNUC__ && defined __GNUC_MINOR__ && !defined __clang__ && !defined __clang_major__ && !defined __clang_minor__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#endif
---
+++ mk_clib/src/mk_lang_warning_msvc_push_c4505.h
@@ -0,0 +1,5 @@
+#include "mk_lang_warning_msvc_push_c5031.h"
+#if defined _MSC_VER && defined _MSC_FULL_VER
+#pragma warning(push)
+#pragma warning(disable:4505) /* warning C4505: 'xxx': unreferenced function with internal linkage has been removed */
+#endif
