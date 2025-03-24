e20e575a90bd50a6995427f1e8ab794d4b9e52b3
Marek Knápek <knapek.mar@gmail.com>
2025-03-24 01:22:00 +0100

    mk_lang_overlap

---
+++ mk_clib/src/mk_lang_overlap_inl_fileh.h
@@ -0,0 +1,20 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_overlap_inl_defd.h"
+
+
+typedef mk_lang_overlap_inl_defd_type mk_lang_overlap_inl_defd_type_t;
+typedef mk_lang_overlap_inl_defd_type_t const mk_lang_overlap_inl_defd_type_ct;
+typedef mk_lang_overlap_inl_defd_type_t* mk_lang_overlap_inl_defd_type_pt;
+typedef mk_lang_overlap_inl_defd_type_t const* mk_lang_overlap_inl_defd_type_pct;
+
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_overlap_inl_defd_fn(mk_lang_overlap_inl_defd_type_pct const arr1, mk_lang_types_usize_t const len1, mk_lang_overlap_inl_defd_type_pct const arr2, mk_lang_types_usize_t const len2) mk_lang_noexcept;
+
+
+#include "mk_lang_overlap_inl_defu.h"
