8dd40cfa95c814ba0e9f5bc519faa3874090de95
Marek Knápek <knapek.mar@gmail.com>
2025-03-24 01:33:00 +0100

    mk_lang_memmov2

---
+++ mk_clib/src/mk_lang_memmov2_inl_fileh.h
@@ -0,0 +1,13 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_memmov2_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lang_memmov2_inl_defd_fn(mk_lang_memmov2_inl_defd_type_pt const dst, mk_lang_memmov2_inl_defd_type_pct const src, mk_lang_types_usize_t const count) mk_lang_noexcept;
+
+
+#include "mk_lang_memmov2_inl_defu.h"
