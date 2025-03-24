16b64096e7f16a35b26db3839abc37842ef76097
Marek Knápek <knapek.mar@gmail.com>
2025-03-24 01:55:00 +0100

    mk_lib_crypto_hash_md5

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_md5.c
@@ -0,0 +1,11 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_md5_c
+#define mk_include_guard_mk_lib_crypto_hash_stream_md5_c
+#include "mk_lib_crypto_hash_stream_md5.h"
+
+
+#define mk_lib_crypto_hash_stream_t_name md5
+#include "mk_lib_crypto_hash_stream_inl_filec.h"
+#include "mk_lib_crypto_hash_stream_inl_fileu.h"
+
+
+#endif
