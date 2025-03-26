9e126a8cdd996fc736f8d55ddb76f0ef2315b6be
Marek Knápek <knapek.mar@gmail.com>
2025-03-26 01:17:00 +0100

    mk_lang_memclr2

---
+++ mk_clib/src/mk_lang_memclr2_inl_defu.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lang_memclr2_inl_defd_name
+#undef mk_lang_memclr2_inl_defd_tn
+
+
+#undef mk_lang_memclr2_inl_defd_type_t
+#undef mk_lang_memclr2_inl_defd_type_ct
+#undef mk_lang_memclr2_inl_defd_type_pt
+#undef mk_lang_memclr2_inl_defd_type_pct
+
+#undef mk_lang_memclr2_inl_defd_type_set_zero
+
+#undef mk_lang_memclr2_inl_defd_fn
