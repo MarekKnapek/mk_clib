4128e6ce0589ec72d8fedde961ebcae7beb05c10
Marek Knápek <knapek.mar@gmail.com>
2025-03-26 02:08:00 +0100

    mk_lib_crypto_hash_sha3_256

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_sha3_256.c
@@ -0,0 +1,11 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_sha3_256_c
+#define mk_include_guard_mk_lib_crypto_hash_stream_sha3_256_c
+#include "mk_lib_crypto_hash_stream_sha3_256.h"
+
+
+#define mk_lib_crypto_hash_stream_t_name sha3_256
+#include "mk_lib_crypto_hash_stream_inl_filec.h"
+#include "mk_lib_crypto_hash_stream_inl_fileu.h"
+
+
+#endif
