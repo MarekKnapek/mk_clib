0ba11d99d56b9e6ad2956125298288db0e4c1e77
Marek Knápek <knapek.mar@gmail.com>
2025-03-26 01:07:00 +0100

    mk_win_base

---
+++ mk_clib/src/mk_win_base.c
@@ -0,0 +1,9 @@
+#ifndef mk_include_guard_mk_win_base_c
+#define mk_include_guard_mk_win_base_c
+#include "mk_win_base.h"
+
+#include "mk_lang_platform.h"
+#if mk_lang_platform_is_windows_at_least_any
+#endif
+
+#endif
