78a4c648c61cd07ab929cc6b16aad968c6952566
Marek Knápek <knapek.mar@gmail.com>
2025-03-29 00:53:00 +0100

    mk_lang_runtime_bool

---
+++ mk_clib/src/mk_lang_runtime_bool.c
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_mk_lang_runtime_bool_c
+#define mk_include_guard_mk_lang_runtime_bool_c
+#include "mk_lang_runtime_bool.h"
+
+
+#include "mk_lang_warning_msvc_push_c4505.h"
+#include "mk_lang_warning_gcc_push_unused_function.h"
+#include "mk_lang_warning_clang_push_unused_function.h"
+static void mk_lang_runtime_bool_dummy(void){}
+#include "mk_lang_warning_clang_pop.h"
+#include "mk_lang_warning_gcc_pop.h"
+#include "mk_lang_warning_msvc_pop.h"
+
+
+#endif
