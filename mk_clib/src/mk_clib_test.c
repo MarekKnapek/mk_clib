9776133a895a518851754d7528dc862b14d369d4
Marek Knápek <knapek.mar@gmail.com>
2025-03-29 02:02:00 +0100

    mk_lib_crypto_hash_sha2_256

--- mk_clib/src/mk_clib_test.c
+++ mk_clib/src/mk_clib_test.c
@@ -13,6 +13,7 @@
 #include "mk_lib_crypto_hash_stream_sha0_test.hpp"
 #include "mk_lib_crypto_hash_stream_sha1_test.hpp"
 #include "mk_lib_crypto_hash_stream_sha2_224_test.hpp"
+#include "mk_lib_crypto_hash_stream_sha2_256_test.hpp"
 
 
 mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_noexcept
@@ -24,6 +25,7 @@ mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_no
 	mk_lib_crypto_hash_stream_sha0_test();
 	mk_lib_crypto_hash_stream_sha1_test();
 	mk_lib_crypto_hash_stream_sha2_224_test();
+	mk_lib_crypto_hash_stream_sha2_256_test();
 }
 
 
