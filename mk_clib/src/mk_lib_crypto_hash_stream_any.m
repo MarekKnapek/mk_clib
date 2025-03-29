11579d843f8cd4b270e878396321f8de8b79f3a3
Marek Knápek <knapek.mar@gmail.com>
2025-03-29 02:18:00 +0100

    mk_lib_crypto_stream_any

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_any.m
@@ -0,0 +1,27 @@
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_c', `define(`mm_x', ``$1'')mm_x(`$2', `$4')undefine(`mm_x')')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_b', `mk_lib_crypto_hash_stream_any_mm_per_hash_c(`$1', `$4', `$2', eval($3 - $# + 1))ifelse(`$#', `5', `', ``$2'`'mk_lib_crypto_hash_stream_any_mm_per_hash_b(`$1', `$2', `$3', shift(shift(shift(shift($@)))))')')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_a', `mk_lib_crypto_hash_stream_any_mm_per_hash_b(`$1', `$2', `$#', shift(shift($@)))')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash', `mk_lib_crypto_hash_stream_any_mm_per_hash_a(`$1', `$2',
+`md2',
+`md4',
+`md5',
+`sha0',
+`sha1',
+`sha2_224',
+`sha2_256',
+`sha2_384',
+`sha2_512',
+`sha2_512_224',
+`sha2_512_256',
+`sha3_224',
+`sha3_256',
+`sha3_384',
+`sha3_512',
+`tiger2_128',
+`tiger2_160',
+`tiger2_192',
+`tiger_128',
+`tiger_160',
+`tiger_192',
+`whirlpool',
+`')')dnl
