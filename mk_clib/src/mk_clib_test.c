e354876a52a0fec0f121bf6fe2ff182898ad5b4a
Marek Knápek <knapek.mar@gmail.com>
2025-03-29 02:36:00 +0100

    mk_lib_crypto_hash_blake2b

--- mk_clib/src/mk_clib_test.c
+++ mk_clib/src/mk_clib_test.c
@@ -7,6 +7,9 @@
 #include "mk_lang_types.h"
 
 #include "mk_lang_sizeof_test.h"
+#include "mk_lib_crypto_hash_stream_blake2b_256_test.hpp"
+#include "mk_lib_crypto_hash_stream_blake2b_384_test.hpp"
+#include "mk_lib_crypto_hash_stream_blake2b_512_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_128_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_160_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_224_test.hpp"
@@ -40,6 +43,9 @@
 mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_noexcept
 {
 	mk_lang_sizeof_test();
+	mk_lib_crypto_hash_stream_blake2b_256_test();
+	mk_lib_crypto_hash_stream_blake2b_384_test();
+	mk_lib_crypto_hash_stream_blake2b_512_test();
 	mk_lib_crypto_hash_stream_blake2s_128_test();
 	mk_lib_crypto_hash_stream_blake2s_160_test();
 	mk_lib_crypto_hash_stream_blake2s_224_test();
---
+++ mk_clib/src/mk_lib_crypto_hash_stream_blake2b_512_test.cpp
@@ -0,0 +1,191 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_blake2b_512_test_cpp
+#define mk_include_guard_mk_lib_crypto_hash_stream_blake2b_512_test_cpp
+
+
+#if defined mk_lang_jumbo_want
+#undef mk_lang_jumbo_want
+#endif
+#define mk_lang_jumbo_want 2
+
+
+#include "mk_lang_warning_msvc_push_c4514.h"
+
+
+#include "mk_lib_crypto_hash_stream_blake2b_512_test.hpp"
+
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_countof.h"
+#include "mk_lang_extern.h"
+#include "mk_lang_inline.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_static_assert.h"
+#include "mk_lang_test.h"
+#include "mk_lang_types.h"
+#include "mk_lang_version.h"
+#include "mk_lib_cpp_constexpr_array_u8.hpp"
+#include "mk_lib_crypto_hash_stream_blake2b_512.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#if mk_lang_version_at_least_cpp_14 || mk_lang_version_at_least_msvc_cpp_14
+
+template<mk_lang_types_usize_t str_lit_len>
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo auto mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(mk_lang_types_pchar_t const(&str_lit)[str_lit_len]) mk_lang_noexcept
+{
+	mk_lib_cpp_constexpr_array_t<mk_sl_cui_uint8_t, str_lit_len - 1> msg mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_blake2b_512_t blake2b_512 mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_blake2b_512_digest_t digest mk_lang_constexpr_init;
+	mk_lib_cpp_constexpr_array_t<mk_sl_cui_uint8_t, mk_lib_crypto_hash_stream_blake2b_512_digest_len_v> ret mk_lang_constexpr_init;
+
+	mk_lang_static_assert(str_lit_len >= 1);
+
+	msg = mk_lib_cpp_constexpr_array_u8_from_pchar_lit(str_lit);
+	mk_lib_crypto_hash_stream_blake2b_512_init(&blake2b_512);
+	mk_lib_crypto_hash_stream_blake2b_512_append_u8s(&blake2b_512, msg.data(), msg.size());
+	mk_lib_crypto_hash_stream_blake2b_512_finish(&blake2b_512, &digest);
+	ret = mk_lib_cpp_constexpr_array_u8_from_u8s(digest.m_data.m_uint8s);
+	return ret;
+}
+
+#endif
+
+
+mk_lang_extern_force_c mk_lang_inline mk_lang_types_void_t mk_lib_crypto_hash_stream_blake2b_512_test(mk_lang_types_void_t) mk_lang_noexcept
+{
+	#define message_1 ""
+	#define message_2 "a"
+	#define message_3 "abc"
+	#define message_4 "message digest"
+	#define message_5 "abcdefghijklmnopqrstuvwxyz"
+	#define message_6 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	#define message_7 "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+	#define message_8 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+	#define digest_1 "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce"
+	#define digest_2 "333fcb4ee1aa7c115355ec66ceac917c8bfd815bf7587d325aec1864edd24e34d5abe2c6b1b5ee3face62fed78dbef802f2a85cb91d455a8f5249d330853cb3c"
+	#define digest_3 "ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923"
+	#define digest_4 "3c26ce487b1c0f062363afa3c675ebdbf5f4ef9bdc022cfbef91e3111cdc283840d8331fc30a8a0906cff4bcdbcd230c61aaec60fdfad457ed96b709a382359a"
+	#define digest_5 "c68ede143e416eb7b4aaae0d8e48e55dd529eafed10b1df1a61416953a2b0a5666c761e7d412e6709e31ffe221b7a7a73908cb95a4d120b8b090a87d1fbedb4c"
+	#define digest_6 "99964802e5c25e703722905d3fb80046b6bca698ca9e2cc7e49b4fe1fa087c2edf0312dfbb275cf250a1e542fd5dc2edd313f9c491127c2e8c0c9b24168e2d50"
+	#define digest_7 "686f41ec5afff6e87e1f076f542aa466466ff5fbde162c48481ba48a748d842799f5b30f5b67fc684771b33b994206d05cc310f31914edd7b97e41860d77d282"
+	#define digest_8 "a2ff3040eda405b929c2fc2fd93e8add6ac3bb5369b679bae170ac6956863ca006285f132a868000fc3fae5bc696e5d17fe3fddfb4a342876c40451184742986"
+
+	#if mk_lang_version_at_least_cpp_14 || mk_lang_version_at_least_msvc_cpp_14
+
+	mk_lang_constexpr_static auto const s_digest_computed_1 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_1);
+	mk_lang_constexpr_static auto const s_digest_computed_2 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_2);
+	mk_lang_constexpr_static auto const s_digest_computed_3 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_3);
+	mk_lang_constexpr_static auto const s_digest_computed_4 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_4);
+	mk_lang_constexpr_static auto const s_digest_computed_5 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_5);
+	mk_lang_constexpr_static auto const s_digest_computed_6 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_6);
+	mk_lang_constexpr_static auto const s_digest_computed_7 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_7);
+	mk_lang_constexpr_static auto const s_digest_computed_8 = mk_lib_crypto_hash_stream_blake2b_512_test_compute_from_str_lit(message_8);
+
+	mk_lang_constexpr_static auto const s_digest_precomputed_1 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_1);
+	mk_lang_constexpr_static auto const s_digest_precomputed_2 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_2);
+	mk_lang_constexpr_static auto const s_digest_precomputed_3 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_3);
+	mk_lang_constexpr_static auto const s_digest_precomputed_4 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_4);
+	mk_lang_constexpr_static auto const s_digest_precomputed_5 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_5);
+	mk_lang_constexpr_static auto const s_digest_precomputed_6 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_6);
+	mk_lang_constexpr_static auto const s_digest_precomputed_7 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_7);
+	mk_lang_constexpr_static auto const s_digest_precomputed_8 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_8);
+
+	mk_lang_static_assert(s_digest_computed_1 == s_digest_precomputed_1);
+	mk_lang_static_assert(s_digest_computed_2 == s_digest_precomputed_2);
+	mk_lang_static_assert(s_digest_computed_3 == s_digest_precomputed_3);
+	mk_lang_static_assert(s_digest_computed_4 == s_digest_precomputed_4);
+	mk_lang_static_assert(s_digest_computed_5 == s_digest_precomputed_5);
+	mk_lang_static_assert(s_digest_computed_6 == s_digest_precomputed_6);
+	mk_lang_static_assert(s_digest_computed_7 == s_digest_precomputed_7);
+	mk_lang_static_assert(s_digest_computed_8 == s_digest_precomputed_8);
+
+	#endif
+
+	mk_lang_types_pchar_pct const messages[] =
+	{
+		message_1,
+		message_2,
+		message_3,
+		message_4,
+		message_5,
+		message_6,
+		message_7,
+		message_8,
+	};
+
+	mk_lang_types_sint_t const message_lens[] =
+	{
+		mk_lang_countstr(message_1),
+		mk_lang_countstr(message_2),
+		mk_lang_countstr(message_3),
+		mk_lang_countstr(message_4),
+		mk_lang_countstr(message_5),
+		mk_lang_countstr(message_6),
+		mk_lang_countstr(message_7),
+		mk_lang_countstr(message_8),
+	};
+
+	mk_lang_types_pchar_pct const digests[] =
+	{
+		digest_1,
+		digest_2,
+		digest_3,
+		digest_4,
+		digest_5,
+		digest_6,
+		digest_7,
+		digest_8,
+	};
+
+	mk_lang_types_sint_t n mk_lang_constexpr_init;
+	mk_lang_types_sint_t i mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_blake2b_512_t blake2b_512 mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_blake2b_512_digest_t digest mk_lang_constexpr_init;
+	mk_lang_types_sint_t j mk_lang_constexpr_init;
+	mk_lang_types_sint_t len mk_lang_constexpr_init;
+	mk_sl_cui_uint8_t ta mk_lang_constexpr_init;
+
+	mk_lang_static_assert(mk_lang_countof(messages) == mk_lang_countof(digests));
+	mk_lang_static_assert(mk_lang_countof(messages) == mk_lang_countof(message_lens));
+
+	n = mk_lang_countof(messages);
+	for(i = 0; i != n; ++i)
+	{
+		mk_lang_assert(message_lens[i] >= 0);
+		mk_lib_crypto_hash_stream_blake2b_512_init(&blake2b_512);
+		mk_lib_crypto_hash_stream_blake2b_512_append_u8s(&blake2b_512, ((mk_sl_cui_uint8_pct)(messages[i])), ((mk_lang_types_usize_t)(message_lens[i])));
+		mk_lib_crypto_hash_stream_blake2b_512_finish(&blake2b_512, &digest);
+		for(j = 0; j != mk_lib_crypto_hash_stream_blake2b_512_digest_len_v; ++j)
+		{
+			len = mk_sl_cui_uint8_from_str_hex_n(&ta, &digests[i][j * 2], 2); mk_lang_assert(len == 2);
+			mk_lang_test(mk_sl_cui_uint8_eq(&digest.m_data.m_uint8s[j], &ta));
+		}
+	}
+
+	#undef message_1
+	#undef message_2
+	#undef message_3
+	#undef message_4
+	#undef message_5
+	#undef message_6
+	#undef message_7
+	#undef message_8
+
+	#undef digest_1
+	#undef digest_2
+	#undef digest_3
+	#undef digest_4
+	#undef digest_5
+	#undef digest_6
+	#undef digest_7
+	#undef digest_8
+}
+
+
+#include "mk_lang_warning_msvc_pop.h"
+
+
+#endif
