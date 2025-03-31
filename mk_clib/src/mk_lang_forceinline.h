17a5fd058012dd7f0d53bba82b630f8c1050e5df
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 00:36:00 +0100

    mk_lang_forceinline

---
+++ mk_clib/src/mk_lang_forceinline.h
@@ -0,0 +1,16 @@
+#ifndef mk_include_guard_mk_lang_forceinline_h
+#define mk_include_guard_mk_lang_forceinline_h
+
+
+#include "mk_lang_msvc.h"
+#include "mk_lang_version.h"
+
+
+#if (mk_lang_version_at_least_cpp_11 || mk_lang_version_at_least_msvc_cpp_11 || mk_lang_version_at_least_c_23) && mk_lang_msvc_ver && defined NDEBUG
+#define mk_lang_forceinline [[msvc::forceinline]]
+#else
+#define mk_lang_forceinline
+#endif
+
+
+#endif
