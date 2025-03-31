35eccf0d0b385589bab4de142e3d36c8140aaddd
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 00:40:00 +0100

    mk_lang_nodiscard

---
+++ mk_clib/src/mk_lang_nodiscard.h
@@ -0,0 +1,17 @@
+#ifndef mk_include_guard_mk_lang_nodiscard_h
+#define mk_include_guard_mk_lang_nodiscard_h
+
+
+#include "mk_lang_version.h"
+
+
+#if mk_lang_version_at_least_cpp_17 || mk_lang_version_at_least_msvc_cpp_17
+#define mk_lang_nodiscard [[nodiscard]]
+#elif mk_lang_version_at_least_c_23
+#define mk_lang_nodiscard [[nodiscard]]
+#else
+#define mk_lang_nodiscard
+#endif
+
+
+#endif
