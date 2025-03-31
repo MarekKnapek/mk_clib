84f9ab8d7d096e7ef97308748f712b5e1c687cb5
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 00:54:00 +0100

    mk_lang_external_ltc

---
+++ mk_clib/src/mk_lang_external_ltc.c
@@ -0,0 +1,10 @@
+#ifndef mk_include_guard_mk_lang_external_ltc_c
+#define mk_include_guard_mk_lang_external_ltc_c
+#include "mk_lang_external_ltc.h"
+
+
+#if mk_lang_external_ltc_have
+#endif
+
+
+#endif
