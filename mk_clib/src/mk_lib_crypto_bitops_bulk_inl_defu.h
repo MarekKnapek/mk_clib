f7bd3cd7a7b8352279ac7ee25ed0e8d910bdd4b2
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 01:53:00 +0100

    mk_lib_crypto_bitops_bulk

---
+++ mk_clib/src/mk_lib_crypto_bitops_bulk_inl_defu.h
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lib_crypto_bitops_bulk_inl_defd_name
+#undef mk_lib_crypto_bitops_bulk_inl_defd_tn
+#undef mk_lib_crypto_bitops_bulk_inl_defd_count
+
+
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_t
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_ct
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_pt
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_pct
+
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_xor2
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_xor3
+#undef mk_lib_crypto_bitops_bulk_inl_defd_base_add2_wrap_cid_cod
+
+
+#undef mk_lib_crypto_bitops_bulk_inl_defd_t
+#undef mk_lib_crypto_bitops_bulk_inl_defd_ct
+#undef mk_lib_crypto_bitops_bulk_inl_defd_pt
+#undef mk_lib_crypto_bitops_bulk_inl_defd_pct
+
+#undef mk_lib_crypto_bitops_bulk_inl_defd_memcpy
+#undef mk_lib_crypto_bitops_bulk_inl_defd_memclr
+#undef mk_lib_crypto_bitops_bulk_inl_defd_xor2
+#undef mk_lib_crypto_bitops_bulk_inl_defd_xor3
+#undef mk_lib_crypto_bitops_bulk_inl_defd_add2_wrap_cid_cod
