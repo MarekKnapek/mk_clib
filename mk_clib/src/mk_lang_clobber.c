9f910da2bb95503514fb8006b0ae12f9ec5a2c58
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 01:06:00 +0100

    mk_lang_clobber

---
+++ mk_clib/src/mk_lang_clobber.c
@@ -0,0 +1,17 @@
+#ifndef mk_include_guard_mk_lang_clobber_c
+#define mk_include_guard_mk_lang_clobber_c
+#include "mk_lang_clobber.h"
+
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lang_clobber(mk_lang_types_void_pct const arg) mk_lang_noexcept
+{
+	((mk_lang_types_void_t)(arg));
+}
+
+
+#endif
