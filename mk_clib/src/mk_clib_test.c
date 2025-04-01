d72c33b8cc745bc812dea1d2a47f71b5f2346510
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 02:30:00 +0100

    mk_lib_crypto_hash_blake2b_512

--- mk_clib/src/mk_clib_test.c
+++ mk_clib/src/mk_clib_test.c
@@ -9,6 +9,7 @@
 #include "mk_lang_sizeof_test.h"
 #include "mk_lib_crypto_hash_stream_blake2b_256_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2b_384_test.hpp"
+#include "mk_lib_crypto_hash_stream_blake2b_512_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_128_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_160_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_224_test.hpp"
@@ -37,6 +38,7 @@ mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_no
 	mk_lang_sizeof_test();
 	mk_lib_crypto_hash_stream_blake2b_256_test();
 	mk_lib_crypto_hash_stream_blake2b_384_test();
+	mk_lib_crypto_hash_stream_blake2b_512_test();
 	mk_lib_crypto_hash_stream_blake2s_128_test();
 	mk_lib_crypto_hash_stream_blake2s_160_test();
 	mk_lib_crypto_hash_stream_blake2s_224_test();
---
+++ mk_clib/src/mk_lib_crypto_hash_stream_blake2b_512_test.hpp
@@ -0,0 +1,18 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_blake2b_512_test_hpp
+#define mk_include_guard_mk_lib_crypto_hash_stream_blake2b_512_test_hpp
+
+
+#include "mk_lang_extern.h"
+#include "mk_lang_inline.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+mk_lang_extern_force_c mk_lang_inline mk_lang_types_void_t mk_lib_crypto_hash_stream_blake2b_512_test(mk_lang_types_void_t) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_have
+#include "mk_lib_crypto_hash_stream_blake2b_512_test.cpp"
+#endif
+#endif
