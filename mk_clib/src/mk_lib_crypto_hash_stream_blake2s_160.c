f2a41c70377a3b1035f073f7304b0a159ea8e811
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 02:25:00 +0100

    mk_lib_crypto_hash_blake2s_160

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_blake2s_160.c
@@ -0,0 +1,12 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_blake2s_160_c
+#define mk_include_guard_mk_lib_crypto_hash_stream_blake2s_160_c
+#include "mk_lib_crypto_hash_stream_blake2s_160.h"
+
+
+#define mk_lib_crypto_hash_stream_t_name blake2s_160
+#define mk_lib_crypto_hash_stream_t_padding 0
+#include "mk_lib_crypto_hash_stream_inl_filec.h"
+#include "mk_lib_crypto_hash_stream_inl_fileu.h"
+
+
+#endif
---
+++ mk_clib/src/mk_lib_crypto_hash_stream_blake2s_160.h
@@ -0,0 +1,18 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_blake2s_160_h
+#define mk_include_guard_mk_lib_crypto_hash_stream_blake2s_160_h
+
+
+#include "mk_lang_jumbo.h"
+#include "mk_lib_crypto_hash_block_blake2s_160.h"
+
+
+#define mk_lib_crypto_hash_stream_t_name blake2s_160
+#define mk_lib_crypto_hash_stream_t_padding 0
+#include "mk_lib_crypto_hash_stream_inl_fileh.h"
+#include "mk_lib_crypto_hash_stream_inl_fileu.h"
+
+
+#if mk_lang_jumbo_have
+#include "mk_lib_crypto_hash_stream_blake2s_160.c"
+#endif
+#endif
