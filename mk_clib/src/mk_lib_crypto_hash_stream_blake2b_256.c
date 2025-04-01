1c7740eb6c423b24bf3b387447bdf38820518641
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 02:28:00 +0100

    mk_lib_crypto_hash_blake2b_256

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_blake2b_256.c
@@ -0,0 +1,12 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_blake2b_256_c
+#define mk_include_guard_mk_lib_crypto_hash_stream_blake2b_256_c
+#include "mk_lib_crypto_hash_stream_blake2b_256.h"
+
+
+#define mk_lib_crypto_hash_stream_t_name blake2b_256
+#define mk_lib_crypto_hash_stream_t_padding 0
+#include "mk_lib_crypto_hash_stream_inl_filec.h"
+#include "mk_lib_crypto_hash_stream_inl_fileu.h"
+
+
+#endif
