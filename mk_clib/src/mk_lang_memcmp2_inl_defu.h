e86456a7a950b1d388b62909a2c162dc36cb0eec
Marek Knápek <knapek.mar@gmail.com>
2025-04-01 01:25:00 +0100

    mk_lang_memcmp2

---
+++ mk_clib/src/mk_lang_memcmp2_inl_defu.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lang_memcmp2_inl_defd_name
+#undef mk_lang_memcmp2_inl_defd_tn
+
+
+#undef mk_lang_memcmp2_inl_defd_type_t
+#undef mk_lang_memcmp2_inl_defd_type_ct
+#undef mk_lang_memcmp2_inl_defd_type_pt
+#undef mk_lang_memcmp2_inl_defd_type_pct
+
+#undef mk_lang_memcmp2_inl_defd_type_cmp
+
+#undef mk_lang_memcmp2_inl_defd_fn
