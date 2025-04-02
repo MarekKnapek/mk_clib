2ff2dfa6f1037ea9ddbbda9867a2fba28d4086a9
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 01:23:00 +0200

    mk_lang_memclr2

---
+++ mk_clib/src/mk_lang_memclr2_inl_filec.h
@@ -0,0 +1,31 @@
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_memclr2_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lang_memclr2_inl_defd_fn(mk_lang_memclr2_inl_defd_type_pt const dst, mk_lang_types_usize_t const count) mk_lang_noexcept
+{
+	mk_lang_types_usize_t n mk_lang_constexpr_init;
+	mk_lang_types_usize_t i mk_lang_constexpr_init;
+
+	#include "mk_lang_warning_msvc_push_c4296.h"
+	#include "mk_lang_warning_gcc_push_type_limits.h"
+	mk_lang_assert(dst);
+	mk_lang_assert(count >= 0);
+	#include "mk_lang_warning_gcc_pop.h"
+	#include "mk_lang_warning_msvc_pop.h"
+
+	n = count;
+	for(i = 0; i != n; ++i)
+	{
+		mk_lang_memclr2_inl_defd_type_set_zero(&dst[i]);
+	}
+}
+
+
+#include "mk_lang_memclr2_inl_defu.h"
