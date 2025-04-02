d735d0d1acfd2bb34f57add2405657155a97bd35
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 01:02:00 +0200

    mk_lang_sizet

---
+++ mk_clib/src/mk_lang_sizet.h
@@ -0,0 +1,18 @@
+#ifndef mk_include_guard_mk_lang_sizet_h
+#define mk_include_guard_mk_lang_sizet_h
+
+
+#if defined __cplusplus
+#include "mk_lang_intptrt.h"
+#include <cstddef> /* std::size_t */
+#define mk_lang_usize_t std::size_t
+#define mk_lang_ssize_t mk_lang_sintptr_t
+#else
+#include "mk_lang_intptrt.h"
+#include <stddef.h> /* size_t */
+#define mk_lang_usize_t size_t
+#define mk_lang_ssize_t mk_lang_sintptr_t
+#endif
+
+
+#endif
