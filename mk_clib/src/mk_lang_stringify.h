72aa88d7432cbff26cddaa1a20ad7f71c6370027
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 00:25:00 +0200

    mk_lang_stringify

---
+++ mk_clib/src/mk_lang_stringify.h
@@ -0,0 +1,9 @@
+#ifndef mk_include_guard_mk_lang_stringify_h
+#define mk_include_guard_mk_lang_stringify_h
+
+
+#define mk_lang_stringify_impl(x) #x
+#define mk_lang_stringify(x) mk_lang_stringify_impl(x)
+
+
+#endif
