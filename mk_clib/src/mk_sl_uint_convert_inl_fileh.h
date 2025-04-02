1cbfdd8b3b9d6f9ce7737f3bec0a0f2046445062
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 01:54:00 +0200

    mk_sl_uint_convert

---
+++ mk_clib/src/mk_sl_uint_convert_inl_fileh.h
@@ -0,0 +1,17 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_sl_uint_convert_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_sml(mk_sl_uint_convert_inl_defd_big_pct const big, mk_sl_uint_convert_inl_defd_sml_pt const sml) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_big(mk_sl_uint_convert_inl_defd_big_pt const big, mk_sl_uint_convert_inl_defd_sml_pct const sml) mk_lang_noexcept;
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_sml_multi(mk_sl_uint_convert_inl_defd_big_pct const big, mk_sl_uint_convert_inl_defd_sml_pt const sml, mk_lang_types_usize_t const count) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_uint_convert_inl_defd_to_big_multi(mk_sl_uint_convert_inl_defd_big_pt const big, mk_sl_uint_convert_inl_defd_sml_pct const sml, mk_lang_types_usize_t const count) mk_lang_noexcept;
+
+
+#include "mk_sl_uint_convert_inl_defu.h"
