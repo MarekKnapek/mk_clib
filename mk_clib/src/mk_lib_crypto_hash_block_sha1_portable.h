14549f0750fe712d84a2d475686e8dd82b3213f0
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 02:12:00 +0200

    mk_lib_crypto_hash_sha1

---
+++ mk_clib/src/mk_lib_crypto_hash_block_sha1_portable.h
@@ -0,0 +1,83 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_block_sha1_portable_h
+#define mk_include_guard_mk_lib_crypto_hash_block_sha1_portable_h
+
+/* NIST FIPS PUB 180-1 */
+/* NIST FIPS PUB 180-2 */
+/* NIST FIPS PUB 180-3 */
+/* NIST FIPS PUB 180-4 */
+
+#include "mk_lang_alignas.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_countof.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_pow2.h"
+#include "mk_lang_stringify.h"
+#include "mk_lang_types.h"
+#include "mk_sl_cui_uint32.h"
+#include "mk_sl_cui_uint64.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#define mk_lib_crypto_hash_block_sha1_portable_block_len_d 64
+#define mk_lib_crypto_hash_block_sha1_portable_digest_len_d 20
+#define mk_lib_crypto_hash_block_sha1_portable_name_def SHA-1
+enum mk_lib_crypto_hash_block_sha1_portable_block_len_e{ mk_lib_crypto_hash_block_sha1_portable_block_len_v = mk_lib_crypto_hash_block_sha1_portable_block_len_d }; typedef enum mk_lib_crypto_hash_block_sha1_portable_block_len_e mk_lib_crypto_hash_block_sha1_portable_block_len_t;
+enum mk_lib_crypto_hash_block_sha1_portable_digest_len_e{ mk_lib_crypto_hash_block_sha1_portable_digest_len_v = mk_lib_crypto_hash_block_sha1_portable_digest_len_d }; typedef enum mk_lib_crypto_hash_block_sha1_portable_digest_len_e mk_lib_crypto_hash_block_sha1_portable_digest_len_t;
+mk_lang_constexpr_static_inline mk_lang_types_pchar_t const mk_lib_crypto_hash_block_sha1_portable_name_str_buf[] = mk_lang_stringify(mk_lib_crypto_hash_block_sha1_portable_name_def);
+mk_lang_constexpr_static_inline mk_lang_types_sint_t const mk_lib_crypto_hash_block_sha1_portable_name_str_len = mk_lang_countstr(mk_lib_crypto_hash_block_sha1_portable_name_str_buf);
+
+
+union mk_lib_crypto_hash_block_sha1_portable_block_data_u
+{
+	mk_lang_alignas(mk_lib_crypto_hash_block_sha1_portable_block_len_v) mk_sl_cui_uint8_t m_uint8s[mk_lib_crypto_hash_block_sha1_portable_block_len_v];
+	mk_lang_types_ulllong_t m_align;
+};
+typedef union mk_lib_crypto_hash_block_sha1_portable_block_data_u mk_lib_crypto_hash_block_sha1_portable_block_data_t;
+struct mk_lib_crypto_hash_block_sha1_portable_block_s
+{
+	mk_lib_crypto_hash_block_sha1_portable_block_data_t m_data;
+};
+typedef struct mk_lib_crypto_hash_block_sha1_portable_block_s mk_lib_crypto_hash_block_sha1_portable_block_t;
+typedef mk_lib_crypto_hash_block_sha1_portable_block_t const mk_lib_crypto_hash_block_sha1_portable_block_ct;
+typedef mk_lib_crypto_hash_block_sha1_portable_block_t* mk_lib_crypto_hash_block_sha1_portable_block_pt;
+typedef mk_lib_crypto_hash_block_sha1_portable_block_t const* mk_lib_crypto_hash_block_sha1_portable_block_pct;
+
+union mk_lib_crypto_hash_block_sha1_portable_digest_data_u
+{
+	mk_lang_alignas(mk_lang_pow2_roundup(mk_lib_crypto_hash_block_sha1_portable_digest_len_v)) mk_sl_cui_uint8_t m_uint8s[mk_lib_crypto_hash_block_sha1_portable_digest_len_v];
+	mk_lang_types_ulllong_t m_align;
+};
+typedef union mk_lib_crypto_hash_block_sha1_portable_digest_data_u mk_lib_crypto_hash_block_sha1_portable_digest_data_t;
+struct mk_lib_crypto_hash_block_sha1_portable_digest_s
+{
+	mk_lib_crypto_hash_block_sha1_portable_digest_data_t m_data;
+};
+typedef struct mk_lib_crypto_hash_block_sha1_portable_digest_s mk_lib_crypto_hash_block_sha1_portable_digest_t;
+typedef mk_lib_crypto_hash_block_sha1_portable_digest_t const mk_lib_crypto_hash_block_sha1_portable_digest_ct;
+typedef mk_lib_crypto_hash_block_sha1_portable_digest_t* mk_lib_crypto_hash_block_sha1_portable_digest_pt;
+typedef mk_lib_crypto_hash_block_sha1_portable_digest_t const* mk_lib_crypto_hash_block_sha1_portable_digest_pct;
+
+
+#include "mk_lang_warning_msvc_push_c4820.h"
+struct mk_lib_crypto_hash_block_sha1_portable_s
+{
+	mk_sl_cui_uint32_t m_state[5];
+	mk_sl_cui_uint64_t m_len;
+};
+typedef struct mk_lib_crypto_hash_block_sha1_portable_s mk_lib_crypto_hash_block_sha1_portable_t;
+typedef mk_lib_crypto_hash_block_sha1_portable_t const mk_lib_crypto_hash_block_sha1_portable_ct;
+typedef mk_lib_crypto_hash_block_sha1_portable_t* mk_lib_crypto_hash_block_sha1_portable_pt;
+typedef mk_lib_crypto_hash_block_sha1_portable_t const* mk_lib_crypto_hash_block_sha1_portable_pct;
+#include "mk_lang_warning_msvc_pop.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha1_portable_init(mk_lib_crypto_hash_block_sha1_portable_pt const sha1_portable) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha1_portable_append_blocks(mk_lib_crypto_hash_block_sha1_portable_pt const sha1_portable, mk_lib_crypto_hash_block_sha1_portable_block_pct const pblocks, mk_lang_types_usize_t const nblocks) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha1_portable_finish(mk_lib_crypto_hash_block_sha1_portable_pt const sha1_portable, mk_lib_crypto_hash_block_sha1_portable_block_pt const block, mk_lang_types_sint_t const idx, mk_lib_crypto_hash_block_sha1_portable_digest_pt const digest) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_have == 1
+#include "mk_lib_crypto_hash_block_sha1_portable.c"
+#endif
+#endif
