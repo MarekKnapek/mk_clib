006f0d011c2a22a3524198640549791f7935ca55
Marek Knápek <knapek.mar@gmail.com>
2025-04-03 02:39:00 +0200

    mk_clib_hasher.wasm

---
+++ mk_clib/projects/emscripten/mk_clib_hasher.c
@@ -0,0 +1,72 @@
+#define mk_lang_jumbo_want 1
+
+
+#include "../../src/mk_lang_emscripten.h"
+#include "../../src/mk_lang_extern.h"
+#include "../../src/mk_lang_noexcept.h"
+#include "../../src/mk_lang_types.h"
+#include "../../src/mk_lib_wasm_hasher.h"
+
+
+static mk_lib_wasm_hasher_t mk_clib_hasher_g;
+
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_buffer_buf(mk_lang_types_void_t) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_buffer_buf(&mk_clib_hasher_g);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_buffer_len(mk_lang_types_void_t) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_buffer_len(&mk_clib_hasher_g);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_alg_count(mk_lang_types_void_t) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_alg_count(&mk_clib_hasher_g);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_default_alg(mk_lang_types_void_t) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_default_alg(&mk_clib_hasher_g);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_alg_str_buf(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_alg_str_buf(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_alg_str_len(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_alg_str_len(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_alg_key_buf(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_alg_key_buf(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_alg_key_len(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_alg_key_len(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_get_digest_len(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_get_digest_len(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_init(mk_lang_types_uintptr_t const idx) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_init(&mk_clib_hasher_g, idx);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_append(mk_lang_types_uintptr_t const count) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_append(&mk_clib_hasher_g, count);
+}
+
+mk_lang_extern_force_c mk_lang_emscripten_keepalive mk_lang_types_uintptr_t mk_clib_hasher_finish(mk_lang_types_uintptr_t const xof_len) mk_lang_noexcept
+{
+	return mk_lib_wasm_hasher_finish(&mk_clib_hasher_g, xof_len);
+}
