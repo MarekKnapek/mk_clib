1df3dbe9638d8154858aa18791f7877c108102cd
Marek Knápek <knapek.mar@gmail.com>
2025-04-05 01:55:00 +0200

    mk_sl_uint_more

---
+++ mk_clib/src/mk_sl_uint_more.h
@@ -0,0 +1,20 @@
+#ifndef mk_include_guard_mk_sl_uint_more_h
+#define mk_include_guard_mk_sl_uint_more_h
+
+
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+#include "mk_sl_cui_uint8.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_cui_uint8_xor2_many(mk_sl_cui_uint8_pt const a,mk_sl_cui_uint8_pct const b, mk_lang_types_usize_t const count) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_cui_uint8_xor3_many_1(mk_sl_cui_uint8_pct const a,mk_sl_cui_uint8_pct const b, mk_lang_types_usize_t const count, mk_sl_cui_uint8_pt const c) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_sl_cui_uint8_xor3_many_n(mk_sl_cui_uint8_pct const a,mk_sl_cui_uint8_pct const b, mk_lang_types_usize_t const count, mk_sl_cui_uint8_pt const c) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_have
+#include "mk_sl_uint_more.c"
+#endif
+#endif
