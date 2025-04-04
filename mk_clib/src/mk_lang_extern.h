c103965cbe63b854a4738274680e9a96e4a71ba6
Marek Knápek <knapek.mar@gmail.com>
2025-04-05 00:53:00 +0200

    mk_lang_extern

---
+++ mk_clib/src/mk_lang_extern.h
@@ -0,0 +1,25 @@
+#ifndef mk_include_guard_mk_lang_extern_h
+#define mk_include_guard_mk_lang_extern_h
+
+
+#include "mk_lang_jumbo.h"
+
+
+#if mk_lang_jumbo_variant != 1
+#if defined __cplusplus
+#define mk_lang_extern_c extern "C"
+#else
+#define mk_lang_extern_c extern
+#endif
+#else
+#define mk_lang_extern_c
+#endif
+
+#if defined __cplusplus
+#define mk_lang_extern_force_c extern "C"
+#else
+#define mk_lang_extern_force_c extern
+#endif
+
+
+#endif
