0fd5c633685c1548cee9d52d9da5b9f553818d65
Marek Knápek <knapek.mar@gmail.com>
2025-04-05 02:24:00 +0200

    mk_lib_crypto_xof_shake_128

--- mk_clib/src/mk_lib_crypto_xof_stream_any.m
+++ mk_clib/src/mk_lib_crypto_xof_stream_any.m
@@ -2,4 +2,5 @@ define(`mk_lib_crypto_xof_stream_any_mm_per_xof_c', `define(`mm_x', ``$1'')mm_x(
 define(`mk_lib_crypto_xof_stream_any_mm_per_xof_b', `mk_lib_crypto_xof_stream_any_mm_per_xof_c(`$1', `$4', `$2', eval($3 - $# + 1))ifelse(`$#', `5', `', ``$2'`'mk_lib_crypto_xof_stream_any_mm_per_xof_b(`$1', `$2', `$3', shift(shift(shift(shift($@)))))')')dnl
 define(`mk_lib_crypto_xof_stream_any_mm_per_xof_a', `mk_lib_crypto_xof_stream_any_mm_per_xof_b(`$1', `$2', `$#', shift(shift($@)))')dnl
 define(`mk_lib_crypto_xof_stream_any_mm_per_xof', `mk_lib_crypto_xof_stream_any_mm_per_xof_a(`$1', `$2',
+`shake_128',
 `')')dnl
