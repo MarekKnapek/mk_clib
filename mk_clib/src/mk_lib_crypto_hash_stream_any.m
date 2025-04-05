f88770f4d5c820a03be015c028f5c70c667103ae
Marek Knápek <knapek.mar@gmail.com>
2025-04-05 02:06:00 +0200

    mk_lib_crypto_stream_any

---
+++ mk_clib/src/mk_lib_crypto_hash_stream_any.m
@@ -0,0 +1,5 @@
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_c', `define(`mm_x', ``$1'')mm_x(`$2', `$4')undefine(`mm_x')')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_b', `mk_lib_crypto_hash_stream_any_mm_per_hash_c(`$1', `$4', `$2', eval($3 - $# + 1))ifelse(`$#', `5', `', ``$2'`'mk_lib_crypto_hash_stream_any_mm_per_hash_b(`$1', `$2', `$3', shift(shift(shift(shift($@)))))')')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash_a', `mk_lib_crypto_hash_stream_any_mm_per_hash_b(`$1', `$2', `$#', shift(shift($@)))')dnl
+define(`mk_lib_crypto_hash_stream_any_mm_per_hash', `mk_lib_crypto_hash_stream_any_mm_per_hash_a(`$1', `$2',
+`')')dnl
