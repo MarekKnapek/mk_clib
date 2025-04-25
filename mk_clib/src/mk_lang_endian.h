10a11b6d26189cb8b07463ed98bf81f1bfed7d1a
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 00:15:00 +0200

    mk_lang_endian

---
+++ mk_clib/src/mk_lang_endian.h
@@ -0,0 +1,10 @@
+#ifndef mk_include_guard_mk_lang_endian_h
+#define mk_include_guard_mk_lang_endian_h
+
+
+#define mk_lang_endian_little 1001
+#define mk_lang_endian_big    1002
+#define mk_lang_endian_native (mk_lang_endian_little) /* todo edit when adding new platform */
+
+
+#endif
