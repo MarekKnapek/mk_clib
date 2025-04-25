77359ce992ae9f1722b8322a73963a706cd24796
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 01:36:00 +0200

    mk_lang_memset2

---
+++ mk_clib/src/mk_lang_memset2_inl_defu.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lang_memset2_inl_defd_name
+#undef mk_lang_memset2_inl_defd_tn
+
+
+#undef mk_lang_memset2_inl_defd_type_t
+#undef mk_lang_memset2_inl_defd_type_ct
+#undef mk_lang_memset2_inl_defd_type_pt
+#undef mk_lang_memset2_inl_defd_type_pct
+
+#undef mk_lang_memset2_inl_defd_type_assign
+
+#undef mk_lang_memset2_inl_defd_fn
