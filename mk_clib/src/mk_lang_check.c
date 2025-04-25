73bcf10a8c7a9b8f8aca1aa69d374893276c3cba
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 01:18:00 +0200

    mk_lang_check

---
+++ mk_clib/src/mk_lang_check.c
@@ -0,0 +1,17 @@
+#ifndef mk_include_guard_mk_lang_check_c
+#define mk_include_guard_mk_lang_check_c
+#include "mk_lang_check.h"
+
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_bool_t mk_lang_check_to_bool_impl(mk_lang_types_bool_t const b) mk_lang_noexcept
+{
+	return b;
+}
+
+
+#endif
