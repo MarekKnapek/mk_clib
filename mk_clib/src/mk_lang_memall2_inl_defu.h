60a50233cecea1d5bae29403c1c6f28455745ae6
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 01:54:00 +0200

    mk_lang_memall2

---
+++ mk_clib/src/mk_lang_memall2_inl_defu.h
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lang_memall2_inl_defd_name
+#undef mk_lang_memall2_inl_defd_tn
+
+
+#undef mk_lang_memall2_inl_defd_memclr
+#undef mk_lang_memall2_inl_defd_memcmp
+#undef mk_lang_memall2_inl_defd_memcpy
+#undef mk_lang_memall2_inl_defd_memmov
+#undef mk_lang_memall2_inl_defd_memset
---
+++ mk_clib/src/mk_lang_memall2_inl_filec.h
@@ -0,0 +1,30 @@
+#include "mk_lang_memall2_inl_defd.h"
+
+
+#define mk_lang_memclr2_t_name mk_lang_memall2_inl_defd_memclr
+#define mk_lang_memclr2_t_tn mk_lang_memall2_inl_defd_tn
+#include "mk_lang_memclr2_inl_filec.h"
+#include "mk_lang_memclr2_inl_fileu.h"
+
+#define mk_lang_memcmp2_t_name mk_lang_memall2_inl_defd_memcmp
+#define mk_lang_memcmp2_t_tn mk_lang_memall2_inl_defd_tn
+#include "mk_lang_memcmp2_inl_filec.h"
+#include "mk_lang_memcmp2_inl_fileu.h"
+
+#define mk_lang_memcpy2_t_name mk_lang_memall2_inl_defd_memcpy
+#define mk_lang_memcpy2_t_tn mk_lang_memall2_inl_defd_tn
+#include "mk_lang_memcpy2_inl_filec.h"
+#include "mk_lang_memcpy2_inl_fileu.h"
+
+#define mk_lang_memmov2_t_name mk_lang_memall2_inl_defd_memmov
+#define mk_lang_memmov2_t_tn mk_lang_memall2_inl_defd_tn
+#include "mk_lang_memmov2_inl_filec.h"
+#include "mk_lang_memmov2_inl_fileu.h"
+
+#define mk_lang_memset2_t_name mk_lang_memall2_inl_defd_memset
+#define mk_lang_memset2_t_tn mk_lang_memall2_inl_defd_tn
+#include "mk_lang_memset2_inl_filec.h"
+#include "mk_lang_memset2_inl_fileu.h"
+
+
+#include "mk_lang_memall2_inl_defu.h"
