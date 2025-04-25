d1e0dc3cb6fdcd59c7bc978a9c2e173a450f6ee6
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 00:31:00 +0200

    mk_lang_attribute

---
+++ mk_clib/src/mk_lang_attribute.h
@@ -0,0 +1,15 @@
+#ifndef mk_include_guard_mk_lang_attribute_h
+#define mk_include_guard_mk_lang_attribute_h
+
+
+#include "mk_lang_compiler.h"
+
+
+#if mk_lang_compiler_is_at_least_gcc(11, 1)
+#define mk_lang_attribute_target(x) __attribute__((__target__(x)))
+#else
+#define mk_lang_attribute_target(x)
+#endif
+
+
+#endif
