fa7687e3262c538ccee6d746d9e22ede57834dae
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 01:37:00 +0200

    mk_lang_overlap

---
+++ mk_clib/src/mk_lang_overlap_inl_filec.h
@@ -0,0 +1,121 @@
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+
+
+#include "mk_lang_overlap_inl_defd.h"
+
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_overlap_inl_defd_fn(mk_lang_overlap_inl_defd_type_pct const arr1, mk_lang_types_usize_t const len1, mk_lang_overlap_inl_defd_type_pct const arr2, mk_lang_types_usize_t const len2) mk_lang_noexcept
+{
+	mk_lang_types_usize_t it1 mk_lang_constexpr_init;
+	mk_lang_types_usize_t it2 mk_lang_constexpr_init;
+
+	#include "mk_lang_warning_msvc_push_c4296.h"
+	#include "mk_lang_warning_gcc_push_type_limits.h"
+	mk_lang_assert(arr1 || len1 == 0);
+	mk_lang_assert(len1 >= 0);
+	mk_lang_assert(arr2 || len2 == 0);
+	mk_lang_assert(len2 >= 0);
+	#include "mk_lang_warning_gcc_pop.h"
+	#include "mk_lang_warning_msvc_pop.h"
+
+	#include "mk_lang_warning_msvc_push_c4127.h"
+	if(!mk_lang_constexpr_is_constant_evaluated_test)
+	#include "mk_lang_warning_msvc_pop.h"
+	{
+		#include "mk_lang_warning_msvc_push_c4826.h"
+		return
+			(((mk_lang_types_uintptr_t)(arr1)) >= ((mk_lang_types_uintptr_t)(arr2)) && ((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(arr1)))) < ((mk_lang_types_uintptr_t)(arr2 + len2))) ||
+			(((mk_lang_types_uintptr_t)(arr2)) >= ((mk_lang_types_uintptr_t)(arr1)) && ((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(arr2)))) < ((mk_lang_types_uintptr_t)(arr1 + len1)));
+		#include "mk_lang_warning_msvc_pop.h"
+	}
+	else
+	{
+		for(it1 = 0; it1 != len1; ++it1)
+		{
+			for(it2 = 0; it2 != len2; ++it2)
+			{
+				if(arr1 + it1 == arr2 + it2)
+				{
+					return mk_lang_true;
+				}
+			}
+		}
+		return mk_lang_false;
+	}
+}
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_sint_t mk_lang_overlap_inl_defd_fn2(mk_lang_overlap_inl_defd_type_pct const arr1, mk_lang_types_usize_t const len1, mk_lang_overlap_inl_defd_type_pct const arr2, mk_lang_types_usize_t const len2) mk_lang_noexcept
+{
+	mk_lang_types_usize_t it1 mk_lang_constexpr_init;
+	mk_lang_types_usize_t it2 mk_lang_constexpr_init;
+
+	#include "mk_lang_warning_msvc_push_c4296.h"
+	#include "mk_lang_warning_gcc_push_type_limits.h"
+	mk_lang_assert(arr1 || len1 == 0);
+	mk_lang_assert(len1 >= 0);
+	mk_lang_assert(arr2 || len2 == 0);
+	mk_lang_assert(len2 >= 0);
+	#include "mk_lang_warning_gcc_pop.h"
+	#include "mk_lang_warning_msvc_pop.h"
+
+	#include "mk_lang_warning_msvc_push_c4127.h"
+	if(!mk_lang_constexpr_is_constant_evaluated_test)
+	#include "mk_lang_warning_msvc_pop.h"
+	{
+		#include "mk_lang_warning_msvc_push_c4826.h"
+		if(arr1 == arr2)
+		{
+			return 3; /* exact match */
+		}
+		else if(((mk_lang_types_uintptr_t)(arr1)) >= ((mk_lang_types_uintptr_t)(arr2)) && ((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(arr1)))) < ((mk_lang_types_uintptr_t)(arr2 + len2)))
+		{
+			return 2; /* arr2 is on lower address */
+		}
+		else if(((mk_lang_types_uintptr_t)(arr2)) >= ((mk_lang_types_uintptr_t)(arr1)) && ((mk_lang_types_uintptr_t)(((mk_lang_types_sintptr_t)(arr2)))) < ((mk_lang_types_uintptr_t)(arr1 + len1)))
+		{
+			return 1; /* arr1 is on lower address */
+		}
+		else
+		{
+			return 0; /* no overlap */
+		}
+		#include "mk_lang_warning_msvc_pop.h"
+	}
+	else
+	{
+		for(it1 = 0; it1 != len1; ++it1)
+		{
+			for(it2 = 0; it2 != len2; ++it2)
+			{
+				if(arr1 + it1 == arr2 + it2)
+				{
+					if(it1 != 0 && it2 == 0)
+					{
+						return 1; /* arr1 is on lower address */
+					}
+					else if(it1 == 0 && it2 != 0)
+					{
+						return 2; /* arr2 is on lower address */
+					}
+					else if(it1 == 0 && it2 == 0)
+					{
+						return 3; /* exact match */
+					}
+					else
+					{
+						mk_lang_assert_false();
+					}
+				}
+			}
+		}
+		return 0; /* no overlap */
+	}
+}
+
+
+#include "mk_lang_overlap_inl_defu.h"
