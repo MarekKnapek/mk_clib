0fe915282a8ed9c9cf4134454ea756bddaa492f0
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 01:44:00 +0200

    mk_lang_memcpy2

---
+++ mk_clib/src/mk_lang_memcpy2_inl_defu.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+#undef mk_lang_memcpy2_inl_defd_name
+#undef mk_lang_memcpy2_inl_defd_tn
+
+
+#undef mk_lang_memcpy2_inl_defd_type_t
+#undef mk_lang_memcpy2_inl_defd_type_ct
+#undef mk_lang_memcpy2_inl_defd_type_pt
+#undef mk_lang_memcpy2_inl_defd_type_prt
+#undef mk_lang_memcpy2_inl_defd_type_pct
+#undef mk_lang_memcpy2_inl_defd_type_prct
+
+#undef mk_lang_memcpy2_inl_defd_fn
