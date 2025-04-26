2f8fcccd6503551d4a8c9eb55badcba50e7d7570
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 03:00:00 +0200

    mk_lib_crypto_kdf_pbkdf2

---
+++ mk_clib/src/mk_lib_crypto_kdf_pbkdf2_inl_fileh.h
@@ -0,0 +1,18 @@
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_types.h"
+#include "mk_sl_cui_uint8.h"
+
+
+/* RFC 2898 */
+/* PKCS #5 */
+
+
+#include "mk_lib_crypto_kdf_pbkdf2_inl_defd.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_kdf_pbkdf2_inl_defd_fn(mk_sl_cui_uint8_pct const password_buf, mk_lang_types_sint_t const password_len, mk_sl_cui_uint8_pct const salt_buf, mk_lang_types_sint_t const salt_len, mk_lang_types_ulong_t const cost, mk_lang_types_sint_t const key_len, mk_sl_cui_uint8_pt const key_buf) mk_lang_noexcept;
+
+
+#include "mk_lib_crypto_kdf_pbkdf2_inl_defu.h"
