d71c401e51aad9ab09c9616a5604fea7bde32f6f
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:45:00 +0200

    mk_lib_crypto_hash_blake2b_384

--- mk_clib/src/mk_lib_crypto_hash_stream_any.m
+++ mk_clib/src/mk_lib_crypto_hash_stream_any.m
@@ -3,6 +3,7 @@ define(`mk_lib_crypto_hash_stream_any_mm_per_hash_b', `mk_lib_crypto_hash_stream
 define(`mk_lib_crypto_hash_stream_any_mm_per_hash_a', `mk_lib_crypto_hash_stream_any_mm_per_hash_b(`$1', `$2', `$#', shift(shift($@)))')dnl
 define(`mk_lib_crypto_hash_stream_any_mm_per_hash', `mk_lib_crypto_hash_stream_any_mm_per_hash_a(`$1', `$2',
 `blake2b_256',
+`blake2b_384',
 `blake2s_128',
 `blake2s_160',
 `blake2s_224',
