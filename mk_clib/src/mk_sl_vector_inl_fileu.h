ff0523840511d86bdd283289873b08361deff6dc
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 03:06:00 +0200

    mk_sl_vector

---
+++ mk_clib/src/mk_sl_vector_inl_fileu.h
@@ -0,0 +1,8 @@
+#undef mk_sl_vector_t_name
+#undef mk_sl_vector_t_element_type
+#undef mk_sl_vector_t_mallocatorg
+#undef mk_sl_vector_t_copy
+#undef mk_sl_vector_t_element_construct_void
+#undef mk_sl_vector_t_element_destruct
+#undef mk_sl_vector_t_element_copy
+#undef mk_sl_vector_t_element_move
