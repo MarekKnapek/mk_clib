393d26495fc552b4827a1b4342516d20afd4a2d9
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:47:00 +0200

    mk_lib_crypto_hash_blake3

--- mk_clib/src/mk_clib_test.c
+++ mk_clib/src/mk_clib_test.c
@@ -14,6 +14,7 @@
 #include "mk_lib_crypto_hash_stream_blake2s_160_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_224_test.hpp"
 #include "mk_lib_crypto_hash_stream_blake2s_256_test.hpp"
+#include "mk_lib_crypto_hash_stream_blake3_test.hpp"
 #include "mk_lib_crypto_hash_stream_md2_test.hpp"
 #include "mk_lib_crypto_hash_stream_md4_test.hpp"
 #include "mk_lib_crypto_hash_stream_md5_test.hpp"
@@ -43,6 +44,7 @@ mk_lang_jumbo mk_lang_types_void_t mk_clib_test(mk_lang_types_void_t) mk_lang_no
 	mk_lib_crypto_hash_stream_blake2s_160_test();
 	mk_lib_crypto_hash_stream_blake2s_224_test();
 	mk_lib_crypto_hash_stream_blake2s_256_test();
+	mk_lib_crypto_hash_stream_blake3_test();
 	mk_lib_crypto_hash_stream_md2_test();
 	mk_lib_crypto_hash_stream_md4_test();
 	mk_lib_crypto_hash_stream_md5_test();
