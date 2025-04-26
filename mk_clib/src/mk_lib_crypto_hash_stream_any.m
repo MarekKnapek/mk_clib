d8fd4be023f535b40ab28bef2a433a65f49ccbea
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:52:00 +0200

    mk_lib_crypto_hash_streebog_512

--- mk_clib/src/mk_lib_crypto_hash_stream_any.m
+++ mk_clib/src/mk_lib_crypto_hash_stream_any.m
@@ -26,6 +26,7 @@ define(`mk_lib_crypto_hash_stream_any_mm_per_hash', `mk_lib_crypto_hash_stream_a
 `sha3_384',
 `sha3_512',
 `streebog_256',
+`streebog_512',
 `tiger2_128',
 `tiger2_160',
 `tiger2_192',
