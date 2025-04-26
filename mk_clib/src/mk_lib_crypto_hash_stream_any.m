f321b1429620385a94198ee32fb5a2b1a41f8caa
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:25:00 +0200

    mk_lib_crypto_hash_md5

--- mk_clib/src/mk_lib_crypto_hash_stream_any.m
+++ mk_clib/src/mk_lib_crypto_hash_stream_any.m
@@ -4,4 +4,5 @@ define(`mk_lib_crypto_hash_stream_any_mm_per_hash_a', `mk_lib_crypto_hash_stream
 define(`mk_lib_crypto_hash_stream_any_mm_per_hash', `mk_lib_crypto_hash_stream_any_mm_per_hash_a(`$1', `$2',
 `md2',
 `md4',
+`md5',
 `')')dnl
