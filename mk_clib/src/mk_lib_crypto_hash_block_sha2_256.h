d5be8d5e58cf6a4c2c8cee3f4d310ce94861d07c
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:29:00 +0200

    mk_lib_crypto_hash_sha2_256

---
+++ mk_clib/src/mk_lib_crypto_hash_block_sha2_256.h
@@ -0,0 +1,77 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_block_sha2_256_h
+#define mk_include_guard_mk_lib_crypto_hash_block_sha2_256_h
+
+/* NIST FIPS PUB 180-2 */
+/* NIST FIPS PUB 180-3 */
+/* NIST FIPS PUB 180-4 */
+
+#include "mk_lang_alignas.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_countof.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_stringify.h"
+#include "mk_lang_types.h"
+#include "mk_lib_crypto_hash_block_sha2_base_32bit.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#define mk_lib_crypto_hash_block_sha2_256_block_len_d 64
+#define mk_lib_crypto_hash_block_sha2_256_digest_len_d 32
+#define mk_lib_crypto_hash_block_sha2_256_name_def SHA-256
+enum mk_lib_crypto_hash_block_sha2_256_block_len_e{ mk_lib_crypto_hash_block_sha2_256_block_len_v = mk_lib_crypto_hash_block_sha2_256_block_len_d }; typedef enum mk_lib_crypto_hash_block_sha2_256_block_len_e mk_lib_crypto_hash_block_sha2_256_block_len_t;
+enum mk_lib_crypto_hash_block_sha2_256_digest_len_e{ mk_lib_crypto_hash_block_sha2_256_digest_len_v = mk_lib_crypto_hash_block_sha2_256_digest_len_d }; typedef enum mk_lib_crypto_hash_block_sha2_256_digest_len_e mk_lib_crypto_hash_block_sha2_256_digest_len_t;
+mk_lang_constexpr_static_inline mk_lang_types_pchar_t const mk_lib_crypto_hash_block_sha2_256_name_str_buf[] = mk_lang_stringify(mk_lib_crypto_hash_block_sha2_256_name_def);
+mk_lang_constexpr_static_inline mk_lang_types_sint_t const mk_lib_crypto_hash_block_sha2_256_name_str_len = mk_lang_countstr(mk_lib_crypto_hash_block_sha2_256_name_str_buf);
+
+
+union mk_lib_crypto_hash_block_sha2_256_block_data_u
+{
+	mk_lang_alignas(mk_lib_crypto_hash_block_sha2_256_block_len_v) mk_sl_cui_uint8_t m_uint8s[mk_lib_crypto_hash_block_sha2_256_block_len_v];
+	mk_lang_types_ulllong_t m_align;
+};
+typedef union mk_lib_crypto_hash_block_sha2_256_block_data_u mk_lib_crypto_hash_block_sha2_256_block_data_t;
+struct mk_lib_crypto_hash_block_sha2_256_block_s
+{
+	mk_lib_crypto_hash_block_sha2_256_block_data_t m_data;
+};
+typedef struct mk_lib_crypto_hash_block_sha2_256_block_s mk_lib_crypto_hash_block_sha2_256_block_t;
+typedef mk_lib_crypto_hash_block_sha2_256_block_t const mk_lib_crypto_hash_block_sha2_256_block_ct;
+typedef mk_lib_crypto_hash_block_sha2_256_block_t* mk_lib_crypto_hash_block_sha2_256_block_pt;
+typedef mk_lib_crypto_hash_block_sha2_256_block_t const* mk_lib_crypto_hash_block_sha2_256_block_pct;
+
+union mk_lib_crypto_hash_block_sha2_256_digest_data_u
+{
+	mk_lang_alignas(mk_lib_crypto_hash_block_sha2_256_digest_len_v) mk_sl_cui_uint8_t m_uint8s[mk_lib_crypto_hash_block_sha2_256_digest_len_v];
+	mk_lang_types_ulllong_t m_align;
+};
+typedef union mk_lib_crypto_hash_block_sha2_256_digest_data_u mk_lib_crypto_hash_block_sha2_256_digest_data_t;
+struct mk_lib_crypto_hash_block_sha2_256_digest_s
+{
+	mk_lib_crypto_hash_block_sha2_256_digest_data_t m_data;
+};
+typedef struct mk_lib_crypto_hash_block_sha2_256_digest_s mk_lib_crypto_hash_block_sha2_256_digest_t;
+typedef mk_lib_crypto_hash_block_sha2_256_digest_t const mk_lib_crypto_hash_block_sha2_256_digest_ct;
+typedef mk_lib_crypto_hash_block_sha2_256_digest_t* mk_lib_crypto_hash_block_sha2_256_digest_pt;
+typedef mk_lib_crypto_hash_block_sha2_256_digest_t const* mk_lib_crypto_hash_block_sha2_256_digest_pct;
+
+
+struct mk_lib_crypto_hash_block_sha2_256_s
+{
+	mk_lib_crypto_hash_block_sha2_base_32bit_t m_sha2_base_32bit;
+};
+typedef struct mk_lib_crypto_hash_block_sha2_256_s mk_lib_crypto_hash_block_sha2_256_t;
+typedef mk_lib_crypto_hash_block_sha2_256_t const mk_lib_crypto_hash_block_sha2_256_ct;
+typedef mk_lib_crypto_hash_block_sha2_256_t* mk_lib_crypto_hash_block_sha2_256_pt;
+typedef mk_lib_crypto_hash_block_sha2_256_t const* mk_lib_crypto_hash_block_sha2_256_pct;
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha2_256_init(mk_lib_crypto_hash_block_sha2_256_pt const sha2_256) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha2_256_append_blocks(mk_lib_crypto_hash_block_sha2_256_pt const sha2_256, mk_lib_crypto_hash_block_sha2_256_block_pct const pblocks, mk_lang_types_usize_t const nblocks) mk_lang_noexcept;
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_sha2_256_finish(mk_lib_crypto_hash_block_sha2_256_pt const sha2_256, mk_lib_crypto_hash_block_sha2_256_block_pt const block, mk_lang_types_sint_t const idx, mk_lib_crypto_hash_block_sha2_256_digest_pt const digest) mk_lang_noexcept;
+
+
+#if mk_lang_jumbo_have == 1
+#include "mk_lib_crypto_hash_block_sha2_256.c"
+#endif
+#endif
