a3eb1df7dbffab8797d75e231b16882c2b59f7ae
Marek Knápek <knapek.mar@gmail.com>
2025-04-26 02:52:00 +0200

    mk_lib_crypto_hash_streebog_512

---
+++ mk_clib/src/mk_lib_crypto_hash_block_streebog_512.c
@@ -0,0 +1,103 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_block_streebog_512_c
+#define mk_include_guard_mk_lib_crypto_hash_block_streebog_512_c
+#include "mk_lib_crypto_hash_block_streebog_512.h"
+
+/* http://tc26.ru/research/polozhenie/GOST_R_34_11-2012_eng.pdf */
+
+#include "mk_lang_alignof.h"
+#include "mk_lang_assert.h"
+#include "mk_lang_charbit.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_limits.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_static_assert.h"
+#include "mk_lang_types.h"
+#include "mk_lib_crypto_bitops_bulk.h"
+#include "mk_sl_cui_uint64.h"
+#include "mk_sl_cui_uint8.h"
+#include "mk_sl_uint_convert.h"
+
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_streebog_512_init(mk_lib_crypto_hash_block_streebog_512_pt const streebog_512) mk_lang_noexcept
+{
+	mk_lang_static_assert(mk_lang_charbit == 8);
+	mk_lang_static_assert(sizeof(mk_sl_cui_uint8_t) == 1);
+	mk_lang_static_assert(sizeof(mk_sl_cui_uint64_t) == 8);
+	mk_lang_static_assert(sizeof(mk_lib_crypto_hash_block_streebog_512_block_t) == 64);
+	mk_lang_static_assert(sizeof(mk_lib_crypto_hash_block_streebog_512_digest_t) == 64);
+
+	mk_lang_assert(streebog_512);
+
+	mk_lib_crypto_hash_block_streebog_base_erase_block(&streebog_512->m_streebog_base.m_state.m_data.m_h);
+	mk_lib_crypto_hash_block_streebog_base_erase_block(&streebog_512->m_streebog_base.m_state.m_data.m_n);
+	mk_lib_crypto_hash_block_streebog_base_erase_block(&streebog_512->m_streebog_base.m_state.m_data.m_s);
+}
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_streebog_512_append_blocks(mk_lib_crypto_hash_block_streebog_512_pt const streebog_512, mk_lib_crypto_hash_block_streebog_512_block_pct const pblocks, mk_lang_types_usize_t const nblocks) mk_lang_noexcept
+{
+	mk_lang_static_assert(sizeof(mk_lib_crypto_hash_block_streebog_512_block_t) == sizeof(mk_lib_crypto_hash_block_streebog_base_block_t));
+	mk_lang_static_assert(mk_lang_alignof(mk_lib_crypto_hash_block_streebog_512_block_t) == mk_lang_alignof(mk_lib_crypto_hash_block_streebog_base_block_t));
+
+	#include "mk_lang_warning_msvc_push_c4296.h"
+	#include "mk_lang_warning_gcc_push_type_limits.h"
+	mk_lang_assert(streebog_512);
+	mk_lang_assert(pblocks || nblocks == 0);
+	mk_lang_assert(nblocks >= 0);
+	mk_lang_assert(nblocks <= mk_lang_limits_usize_max / mk_lib_crypto_hash_block_streebog_512_block_len_v);
+	#include "mk_lang_warning_gcc_pop.h"
+	#include "mk_lang_warning_msvc_pop.h"
+
+	#include "mk_lang_warning_msvc_push_c4127.h"
+	if(!mk_lang_constexpr_is_constant_evaluated_test)
+	#include "mk_lang_warning_msvc_pop.h"
+	{
+		mk_lib_crypto_hash_block_streebog_base_append_blocks(&streebog_512->m_streebog_base, ((mk_lib_crypto_hash_block_streebog_base_block_pct)(pblocks)), nblocks);
+	}
+	else
+	{
+		#include "mk_lang_warning_clang_push_unreachable_code.h"
+		mk_lang_types_usize_t n mk_lang_constexpr_init;
+		mk_lang_types_usize_t i mk_lang_constexpr_init;
+		mk_lib_crypto_hash_block_streebog_base_block_t streebog_base_block mk_lang_constexpr_init;
+		n = nblocks;
+		for(i = 0; i != n; ++i)
+		{
+			mk_lib_crypto_bitops_bulk_uint8_64_memcpy(&streebog_base_block.m_data.m_uint8s[0], &pblocks[i].m_data.m_uint8s[0]);
+			mk_lib_crypto_hash_block_streebog_base_append_blocks(&streebog_512->m_streebog_base, &streebog_base_block, 1);
+		}
+		#include "mk_lang_warning_clang_pop.h"
+	}
+}
+
+mk_lang_constexpr mk_lang_jumbo mk_lang_types_void_t mk_lib_crypto_hash_block_streebog_512_finish(mk_lib_crypto_hash_block_streebog_512_pt const streebog_512, mk_lib_crypto_hash_block_streebog_512_block_pt const block, mk_lang_types_sint_t const idx, mk_lib_crypto_hash_block_streebog_512_digest_pt const digest) mk_lang_noexcept
+{
+	mk_lang_types_sint_t i mk_lang_constexpr_init;
+
+	mk_lang_assert(streebog_512);
+	mk_lang_assert(block);
+	mk_lang_assert(idx >= 0 && idx < mk_lib_crypto_hash_block_streebog_512_block_len_v);
+	mk_lang_assert(digest);
+
+	#include "mk_lang_warning_msvc_push_c4127.h"
+	if(!mk_lang_constexpr_is_constant_evaluated_test)
+	#include "mk_lang_warning_msvc_pop.h"
+	{
+		mk_lib_crypto_hash_block_streebog_base_stage_3(&streebog_512->m_streebog_base, ((mk_lib_crypto_hash_block_streebog_base_block_pt)(block)), idx);
+	}
+	else
+	{
+		#include "mk_lang_warning_clang_push_unreachable_code.h"
+		mk_lib_crypto_hash_block_streebog_base_block_t streebog_base_block mk_lang_constexpr_init;
+		mk_sl_cui_uint8_memcpy_fn(&streebog_base_block.m_data.m_uint8s[0], &block->m_data.m_uint8s[0], ((mk_lang_types_usize_t)(idx)));
+		mk_lib_crypto_hash_block_streebog_base_stage_3(&streebog_512->m_streebog_base, &streebog_base_block, idx);
+		#include "mk_lang_warning_clang_pop.h"
+	}
+	for(i = 0; i != 8; ++i)
+	{
+		mk_sl_uint_convert_64_8_le_to_sml(&streebog_512->m_streebog_base.m_state.m_data.m_h.m_data.m_uint64s[i], &digest->m_data.m_uint8s[i * mk_sl_cui_uint64_size_bytes_v]);
+	}
+}
+
+
+#endif
---
+++ mk_clib/src/mk_lib_crypto_hash_stream_streebog_512_test.cpp
@@ -0,0 +1,201 @@
+#ifndef mk_include_guard_mk_lib_crypto_hash_stream_streebog_512_test_cpp
+#define mk_include_guard_mk_lib_crypto_hash_stream_streebog_512_test_cpp
+
+
+#if defined mk_lang_jumbo_want
+#undef mk_lang_jumbo_want
+#endif
+#define mk_lang_jumbo_want 2
+
+
+#include "mk_lang_warning_msvc_push_c4514.h"
+
+
+#include "mk_lib_crypto_hash_stream_streebog_512_test.hpp"
+
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_countof.h"
+#include "mk_lang_extern.h"
+#include "mk_lang_inline.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_static_assert.h"
+#include "mk_lang_test.h"
+#include "mk_lang_types.h"
+#include "mk_lang_version.h"
+#include "mk_lib_cpp_constexpr_array_u8.hpp"
+#include "mk_lib_crypto_hash_stream_streebog_512.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#if mk_lang_version_at_least_cpp_14 || mk_lang_version_at_least_msvc_cpp_14
+
+template<mk_lang_types_usize_t str_lit_len>
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo auto mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(mk_lang_types_pchar_t const(&str_lit)[str_lit_len]) mk_lang_noexcept
+{
+	mk_lib_cpp_constexpr_array_t<mk_sl_cui_uint8_t, str_lit_len - 1> msg mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_streebog_512_t streebog_512 mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_streebog_512_digest_t digest mk_lang_constexpr_init;
+	mk_lib_cpp_constexpr_array_t<mk_sl_cui_uint8_t, mk_lib_crypto_hash_stream_streebog_512_digest_len_v> ret mk_lang_constexpr_init;
+
+	mk_lang_static_assert(str_lit_len >= 1);
+
+	msg = mk_lib_cpp_constexpr_array_u8_from_pchar_lit(str_lit);
+	mk_lib_crypto_hash_stream_streebog_512_init(&streebog_512);
+	mk_lib_crypto_hash_stream_streebog_512_append_u8s(&streebog_512, msg.data(), msg.size());
+	mk_lib_crypto_hash_stream_streebog_512_finish(&streebog_512, &digest);
+	ret = mk_lib_cpp_constexpr_array_u8_from_u8s(digest.m_data.m_uint8s);
+	return ret;
+}
+
+#endif
+
+
+mk_lang_extern_force_c mk_lang_types_void_t mk_lib_crypto_hash_stream_streebog_512_test(mk_lang_types_void_t) mk_lang_noexcept
+{
+	#define message_1 ""
+	#define message_2 "a"
+	#define message_3 "abc"
+	#define message_4 "message digest"
+	#define message_5 "abcdefghijklmnopqrstuvwxyz"
+	#define message_6 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	#define message_7 "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+	#define message_8 "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x30\x31\x32"
+	#define message_9 "\xd1\xe5\x20\xe2\xe5\xf2\xf0\xe8\x2c\x20\xd1\xf2\xf0\xe8\xe1\xee\xe6\xe8\x20\xe2\xed\xf3\xf6\xe8\x2c\x20\xe2\xe5\xfe\xf2\xfa\x20\xf1\x20\xec\xee\xf0\xff\x20\xf1\xf2\xf0\xe5\xeb\xe0\xec\xe8\x20\xed\xe0\x20\xf5\xf0\xe0\xe1\xf0\xfb\xff\x20\xef\xeb\xfa\xea\xfb\x20\xc8\xe3\xee\xf0\xe5\xe2\xfb"
+
+	#define digest_1 "8e945da209aa869f0455928529bcae4679e9873ab707b55315f56ceb98bef0a7362f715528356ee83cda5f2aac4c6ad2ba3a715c1bcd81cb8e9f90bf4c1c1a8a"
+	#define digest_2 "8b2a40ecab7b7496bc4cc0f773595452baf658849b495acc3ba017206810efb00420ccd73fb3297e0f7890941b84ac4a8bc27e3c95e1f97c094609e2136abb7e"
+	#define digest_3 "28156e28317da7c98f4fe2bed6b542d0dab85bb224445fcedaf75d46e26d7eb8d5997f3e0915dd6b7f0aab08d9c8beb0d8c64bae2ab8b3c8c6bc53b3bf0db728"
+	#define digest_4 "96b52f322e3ecf6348d177608e2ddb084309c1642a94923c0bc50e41e4cc50e851d1dd94e4b7a35c30503caf87e3e2ac334e2c805adb99b5adb5443dd4ac23c8"
+	#define digest_5 "ec7b127dcca6b0d741b10ed42062cc4487b4a93f96cfc7faf2e7f79778b1f44159089c91fb0910bec0eee7cdca524fcf291cf933fff406f4f3a03872f2341ff8"
+	#define digest_6 "cc49b68c195d18d3fef26f3d4a6554db62298b96d19fbefa52a139e8558d0528535569ebdea172692857ede3351c02fe9d749ef7273dcecaca5b3e295511650b"
+	#define digest_7 "116201023e88d93a4d076ba77207e8702c6cfa6fcc69b82bb22ae6be9b63f16b19baaf8771e01e6dc25c2b4486fa3bbf8601905762cbbad5ba25a1e034879192"
+	#define digest_8 "1b54d01a4af5b9d5cc3d86d68d285462b19abc2475222f35c085122be4ba1ffa00ad30f8767b3a82384c6574f024c311e2a481332b08ef7f41797891c1646f48"
+	#define digest_9 "1e88e62226bfca6f9994f1f2d51569e0daf8475a3b0fe61a5300eee46d961376035fe83549ada2b8620fcd7c496ce5b33f0cb9dddc2b6460143b03dabac9fb28"
+
+	#if mk_lang_version_at_least_cpp_14 || mk_lang_version_at_least_msvc_cpp_14
+
+	mk_lang_constexpr_static auto const s_digest_computed_1 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_1);
+	mk_lang_constexpr_static auto const s_digest_computed_2 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_2);
+	mk_lang_constexpr_static auto const s_digest_computed_3 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_3);
+	mk_lang_constexpr_static auto const s_digest_computed_4 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_4);
+	mk_lang_constexpr_static auto const s_digest_computed_5 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_5);
+	mk_lang_constexpr_static auto const s_digest_computed_6 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_6);
+	mk_lang_constexpr_static auto const s_digest_computed_7 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_7);
+	mk_lang_constexpr_static auto const s_digest_computed_8 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_8);
+	mk_lang_constexpr_static auto const s_digest_computed_9 = mk_lib_crypto_hash_stream_streebog_512_test_compute_from_str_lit(message_9);
+
+	mk_lang_constexpr_static auto const s_digest_precomputed_1 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_1);
+	mk_lang_constexpr_static auto const s_digest_precomputed_2 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_2);
+	mk_lang_constexpr_static auto const s_digest_precomputed_3 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_3);
+	mk_lang_constexpr_static auto const s_digest_precomputed_4 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_4);
+	mk_lang_constexpr_static auto const s_digest_precomputed_5 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_5);
+	mk_lang_constexpr_static auto const s_digest_precomputed_6 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_6);
+	mk_lang_constexpr_static auto const s_digest_precomputed_7 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_7);
+	mk_lang_constexpr_static auto const s_digest_precomputed_8 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_8);
+	mk_lang_constexpr_static auto const s_digest_precomputed_9 = mk_lib_cpp_constexpr_array_u8_from_hex_lit(digest_9);
+
+	mk_lang_static_assert(s_digest_computed_1 == s_digest_precomputed_1);
+	mk_lang_static_assert(s_digest_computed_2 == s_digest_precomputed_2);
+	mk_lang_static_assert(s_digest_computed_3 == s_digest_precomputed_3);
+	mk_lang_static_assert(s_digest_computed_4 == s_digest_precomputed_4);
+	mk_lang_static_assert(s_digest_computed_5 == s_digest_precomputed_5);
+	mk_lang_static_assert(s_digest_computed_6 == s_digest_precomputed_6);
+	mk_lang_static_assert(s_digest_computed_7 == s_digest_precomputed_7);
+	mk_lang_static_assert(s_digest_computed_8 == s_digest_precomputed_8);
+	mk_lang_static_assert(s_digest_computed_9 == s_digest_precomputed_9);
+
+	#endif
+
+	mk_lang_types_pchar_pct const messages[] =
+	{
+		message_1,
+		message_2,
+		message_3,
+		message_4,
+		message_5,
+		message_6,
+		message_7,
+		message_8,
+		message_9,
+	};
+
+	mk_lang_types_sint_t const message_lens[] =
+	{
+		mk_lang_countstr(message_1),
+		mk_lang_countstr(message_2),
+		mk_lang_countstr(message_3),
+		mk_lang_countstr(message_4),
+		mk_lang_countstr(message_5),
+		mk_lang_countstr(message_6),
+		mk_lang_countstr(message_7),
+		mk_lang_countstr(message_8),
+		mk_lang_countstr(message_9),
+	};
+
+	mk_lang_types_pchar_pct const digests[] =
+	{
+		digest_1,
+		digest_2,
+		digest_3,
+		digest_4,
+		digest_5,
+		digest_6,
+		digest_7,
+		digest_8,
+		digest_9,
+	};
+
+	mk_lang_types_sint_t n mk_lang_constexpr_init;
+	mk_lang_types_sint_t i mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_streebog_512_t streebog_512 mk_lang_constexpr_init;
+	mk_lib_crypto_hash_stream_streebog_512_digest_t digest mk_lang_constexpr_init;
+	mk_lang_types_sint_t j mk_lang_constexpr_init;
+	mk_lang_types_sint_t len mk_lang_constexpr_init;
+	mk_sl_cui_uint8_t ta mk_lang_constexpr_init;
+
+	mk_lang_static_assert(mk_lang_countof(messages) == mk_lang_countof(digests));
+	mk_lang_static_assert(mk_lang_countof(messages) == mk_lang_countof(message_lens));
+
+	n = mk_lang_countof(messages);
+	for(i = 0; i != n; ++i)
+	{
+		mk_lang_assert(message_lens[i] >= 0);
+		mk_lib_crypto_hash_stream_streebog_512_init(&streebog_512);
+		mk_lib_crypto_hash_stream_streebog_512_append_u8s(&streebog_512, ((mk_sl_cui_uint8_pct)(messages[i])), ((mk_lang_types_usize_t)(message_lens[i])));
+		mk_lib_crypto_hash_stream_streebog_512_finish(&streebog_512, &digest);
+		for(j = 0; j != mk_lib_crypto_hash_stream_streebog_512_digest_len_v; ++j)
+		{
+			len = mk_sl_cui_uint8_from_str_hex_n(&ta, &digests[i][j * 2], 2); mk_lang_assert(len == 2);
+			mk_lang_test(mk_sl_cui_uint8_eq(&digest.m_data.m_uint8s[j], &ta));
+		}
+	}
+
+	#undef message_1
+	#undef message_2
+	#undef message_3
+	#undef message_4
+	#undef message_5
+	#undef message_6
+	#undef message_7
+	#undef message_8
+	#undef message_9
+
+	#undef digest_1
+	#undef digest_2
+	#undef digest_3
+	#undef digest_4
+	#undef digest_5
+	#undef digest_6
+	#undef digest_7
+	#undef digest_8
+	#undef digest_9
+}
+
+
+#include "mk_lang_warning_msvc_pop.h"
+
+
+#endif
