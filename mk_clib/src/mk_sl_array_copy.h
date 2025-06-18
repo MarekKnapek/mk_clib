cead1edfb282050968855b3842d81fcc5afac5c0
Marek Knápek <knapek.mar@gmail.com>
2025-06-18 22:56:29 +0200

    mk_sl_array

---
+++ mk_clib/src/mk_sl_array_copy.h
@@ -0,0 +1,9 @@
+#ifndef mk_include_guard_mk_sl_array_copy_h
+#define mk_include_guard_mk_sl_array_copy_h
+
+
+#define mk_sl_array_copy_use_bitblt 1001
+#define mk_sl_array_copy_use_custom 1002
+
+
+#endif
---
+++ mk_clib/src/mk_sl_array_inl_fileu.h
@@ -0,0 +1,11 @@
+#undef mk_sl_array_t_name
+#undef mk_sl_array_t_element_type
+#undef mk_sl_array_t_capacity
+#undef mk_sl_array_t_copy_style
+#undef mk_sl_array_t_element_construct_void
+#undef mk_sl_array_t_element_destroy
+#undef mk_sl_array_t_element_construct_copy
+#undef mk_sl_array_t_element_construct_move
+#undef mk_sl_array_t_element_assign_copy
+#undef mk_sl_array_t_element_assign_move
+#undef mk_sl_array_t_element_eq
