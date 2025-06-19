47dc0a986822e4cec0e9efbd0d6dbbac8272ec71
Marek Knápek <knapek.mar@gmail.com>
2025-06-19 06:45:35 +0200

    fff lllong

--- mk_clib/src/mk_lang_lllong.h
+++ mk_clib/src/mk_lang_lllong.h
@@ -2,11 +2,30 @@
 #define mk_include_guard_mk_lang_lllong_h
 
 
-#if defined __SIZEOF_INT128__ && __SIZEOF_INT128__ != 0
+#if defined mk_lang_disable_lllong_want
+#if (mk_lang_disable_lllong_want) == 0
+#define mk_lang_disable_lllong_have 0
+#elif (mk_lang_disable_lllong_want) == 1
+#define mk_lang_disable_lllong_have 1
+#else
+#error xxxxxxxxxx
+#endif
+#else
+#define mk_lang_disable_lllong_have 0
+#endif
+
+
+#if !mk_lang_disable_lllong_have
+#if defined __SIZEOF_INT128__
+#if (__SIZEOF_INT128__) != 0
 #define mk_lang_lllong_has 1
 #define mk_lang_ulllong_t unsigned __int128
 #define mk_lang_slllong_t signed __int128
-#else
+#endif
+#endif
+#endif
+
+#if !defined mk_lang_lllong_has
 #include "mk_lang_llong.h"
 #define mk_lang_lllong_has 0
 #define mk_lang_ulllong_t mk_lang_ullong_t
