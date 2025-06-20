884d7d7b569dfad9b8fed74c93d04505fca4e15d
Marek Knápek <knapek.mar@gmail.com>
2025-06-20 17:30:21 +0200

    fff types - might be problematic

---
+++ mk_clib/src/mk_lang_types.c
@@ -0,0 +1,24 @@
+#ifndef mk_include_guard_mk_lang_types_c
+#define mk_include_guard_mk_lang_types_c
+#include "mk_lang_types.h"
+
+#include "mk_lang_assert.h"
+#include "mk_lang_constexpr.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+
+
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_types_pc_eq(mk_lang_types_pchar_pct const a, mk_lang_types_pchar_pct const b) mk_lang_noexcept
+{
+	mk_lang_types_bool_t ret mk_lang_constexpr_init;
+
+	mk_lang_assert(a);
+	mk_lang_assert(b);
+
+	ret = *a == *b;
+	return ret;
+}
+
+
+#endif
