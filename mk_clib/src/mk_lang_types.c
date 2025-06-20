23123acc0dd15151e9abc33279fb8a8b91faab05
Marek Knápek <knapek.mar@gmail.com>
2025-06-20 17:50:20 +0200

    fff types - might be problematic

--- mk_clib/src/mk_lang_types.c
+++ mk_clib/src/mk_lang_types.c
@@ -9,7 +9,7 @@
 #include "mk_lang_noexcept.h"
 
 
-mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_types_pc_eq(mk_lang_types_pchar_pct const a, mk_lang_types_pchar_pct const b) mk_lang_noexcept
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_types_pchar_eq(mk_lang_types_pchar_pct const a, mk_lang_types_pchar_pct const b) mk_lang_noexcept
 {
 	mk_lang_types_bool_t ret mk_lang_constexpr_init;
 
--- mk_clib/src/mk_lang_types.h
+++ mk_clib/src/mk_lang_types.h
@@ -70,7 +70,7 @@ mk_lang_typedef(mk_lang_types_double);
 mk_lang_typedef(mk_lang_types_ldouble);
 
 
-mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_types_pc_eq(mk_lang_types_pchar_pct const a, mk_lang_types_pchar_pct const b) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_bool_t mk_lang_types_pchar_eq(mk_lang_types_pchar_pct const a, mk_lang_types_pchar_pct const b) mk_lang_noexcept;
 
 
 #if mk_lang_jumbo_have
