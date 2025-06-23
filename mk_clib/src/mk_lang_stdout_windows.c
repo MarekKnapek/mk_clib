8b68dbd633e724a8a05f56a3c022f14be7411eb8
Marek Knápek <knapek.mar@gmail.com>
2025-06-23 19:11:09 +0200

    color

--- mk_clib/src/mk_lang_stdout_windows.c
+++ mk_clib/src/mk_lang_stdout_windows.c
@@ -8,6 +8,7 @@
 #include "mk_lang_assert.h"
 #include "mk_lang_bool.h"
 #include "mk_lang_check.h"
+#include "mk_lang_clobber.h"
 #include "mk_lang_jumbo.h"
 #include "mk_lang_nodiscard.h"
 #include "mk_lang_noexcept.h"
@@ -43,6 +44,78 @@ mk_lang_typedef(mk_lang_stdout_windows);
 
 
 static mk_lang_stdout_windows_t mk_lang_stdout_windows_g;
+
+
+mk_lang_nodiscard static mk_lang_inline mk_win_dll_kernel_console_attribute_t mk_lang_stdout_windows_pr_color_to_attribute(mk_lang_stdout_windows_color_text_t const color) mk_lang_noexcept
+{
+	mk_win_dll_kernel_console_attribute_t attribute;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+
+	switch(color)
+	{
+		case mk_lang_stdout_windows_color_text_e_dark_black   : attribute = ((mk_win_dll_kernel_console_attribute_t)(0)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_red     : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_red)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_green   : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_green)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_blue    : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_yellow  : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_green)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_magenta : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_cyan    : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_green | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_dark_white   : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_green | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_light_black  : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity)); break;
+		case mk_lang_stdout_windows_color_text_e_light_red    : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_red)); break;
+		case mk_lang_stdout_windows_color_text_e_light_green  : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_green)); break;
+		case mk_lang_stdout_windows_color_text_e_light_blue   : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_light_yellow : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_green)); break;
+		case mk_lang_stdout_windows_color_text_e_light_magenta: attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_light_cyan   : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_green | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_light_white  : attribute = ((mk_win_dll_kernel_console_attribute_t)(mk_win_dll_kernel_console_attribute_e_foreground_intensity | mk_win_dll_kernel_console_attribute_e_foreground_red | mk_win_dll_kernel_console_attribute_e_foreground_green | mk_win_dll_kernel_console_attribute_e_foreground_blue)); break;
+		case mk_lang_stdout_windows_color_text_e_dummy_end: mk_lang_assert_false(); break;
+		default: mk_lang_assert_false(); break;
+	}
+	mk_lang_clobber(&attribute);
+	return attribute;
+}
+
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lang_stdout_windows_pr_color_change(mk_lang_stdout_windows_color_text_t const color, mk_win_base_word_pt const restore) mk_lang_noexcept
+{
+	mk_win_dll_kernel_console_attribute_t attribute;
+	mk_win_base_bool_t b;
+	mk_win_dll_kernel_console_console_screen_buffer_info_t info;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(restore);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type >= 0);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type < mk_lang_stdout_windows_type_e_dummy_end);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type != mk_lang_stdout_windows_type_e_not_initialized);
+
+	if(mk_lang_stdout_windows_g.m_type == mk_lang_stdout_windows_type_e_normal)
+	{
+		attribute = mk_lang_stdout_windows_pr_color_to_attribute(color);
+		b = mk_win_dll_kernel_console_get_console_screen_buffer_info(mk_lang_stdout_windows_g.m_handle, &info); mk_lang_check_return(b != 0);
+		b = mk_win_dll_kernel_console_set_console_text_attribute(mk_lang_stdout_windows_g.m_handle, ((mk_win_base_word_t)(attribute))); mk_lang_check_return(b != 0);
+		*restore = info.m_attributes;
+	}
+	return 0;
+}
+
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lang_stdout_windows_pr_color_restore(mk_win_base_word_pct const restore) mk_lang_noexcept
+{
+	mk_win_base_bool_t b;
+
+	mk_lang_assert(restore);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type >= 0);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type < mk_lang_stdout_windows_type_e_dummy_end);
+	mk_lang_assert(mk_lang_stdout_windows_g.m_type != mk_lang_stdout_windows_type_e_not_initialized);
+
+	if(mk_lang_stdout_windows_g.m_type == mk_lang_stdout_windows_type_e_normal)
+	{
+		b = mk_win_dll_kernel_console_set_console_text_attribute(mk_lang_stdout_windows_g.m_handle, *restore); mk_lang_check_return(b != 0);
+	}
+	return 0;
+}
 
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_init(mk_lang_types_void_t) mk_lang_noexcept
@@ -81,12 +154,12 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_prin
 
 	switch(mk_lang_stdout_windows_g.m_type)
 	{
-		case mk_lang_stdout_windows_type_e_not_initialized: ((mk_lang_types_void_t)(str_buf)); ((mk_lang_types_void_t)(str_len)); break;
+		case mk_lang_stdout_windows_type_e_not_initialized: mk_lang_assert_false(); break;
 		case mk_lang_stdout_windows_type_e_normal: b = mk_win_dll_kernel_console_a_write(mk_lang_stdout_windows_g.m_handle, str_buf, ((mk_win_base_dword_t)(str_len)), &written, mk_win_base_null); mk_lang_check_return(b != 0); mk_lang_check_return(written == ((mk_win_base_dword_t)(str_len))); break;
 		case mk_lang_stdout_windows_type_e_redirected: b = mk_win_dll_kernel_files_write_file(mk_lang_stdout_windows_g.m_handle, str_buf, ((mk_win_base_dword_t)(str_len)), &written, mk_win_base_null); mk_lang_check_return(b != 0); mk_lang_check_return(written == ((mk_win_base_dword_t)(str_len))); break;
 		case mk_lang_stdout_windows_type_e_missing: ((mk_lang_types_void_t)(str_buf)); ((mk_lang_types_void_t)(str_len)); break;
-		case mk_lang_stdout_windows_type_e_dummy_end: mk_lang_assert(mk_lang_false); break;
-		default: mk_lang_assert(mk_lang_false); break;
+		case mk_lang_stdout_windows_type_e_dummy_end: mk_lang_assert_false(); break;
+		default: mk_lang_assert_false(); break;
 	}
 	return 0;
 }
@@ -102,12 +175,12 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_prin
 
 	switch(mk_lang_stdout_windows_g.m_type)
 	{
-		case mk_lang_stdout_windows_type_e_not_initialized: ((mk_lang_types_void_t)(str_buf)); ((mk_lang_types_void_t)(str_len)); break;
+		case mk_lang_stdout_windows_type_e_not_initialized: mk_lang_assert_false(); break;
 		case mk_lang_stdout_windows_type_e_normal: b = mk_win_dll_kernel_console_w_write(mk_lang_stdout_windows_g.m_handle, str_buf, ((mk_win_base_dword_t)(str_len)), &written, mk_win_base_null); mk_lang_check_return(b != 0); mk_lang_check_return(written == ((mk_win_base_dword_t)(str_len))); break;
 		case mk_lang_stdout_windows_type_e_redirected: b = mk_win_dll_kernel_files_write_file(mk_lang_stdout_windows_g.m_handle, str_buf, ((mk_win_base_dword_t)(str_len)) * mk_lang_sizeof_bi_wchar_t, &written, mk_win_base_null); mk_lang_check_return(b != 0); mk_lang_check_return(written == ((mk_win_base_dword_t)(str_len)) * mk_lang_sizeof_bi_wchar_t); break;
 		case mk_lang_stdout_windows_type_e_missing: ((mk_lang_types_void_t)(str_buf)); ((mk_lang_types_void_t)(str_len)); break;
-		case mk_lang_stdout_windows_type_e_dummy_end: mk_lang_assert(mk_lang_false); break;
-		default: mk_lang_assert(mk_lang_false); break;
+		case mk_lang_stdout_windows_type_e_dummy_end: mk_lang_assert_false(); break;
+		default: mk_lang_assert_false(); break;
 	}
 	return 0;
 }
@@ -160,6 +233,102 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_prin
 	mk_lang_assert(str_len >= 0);
 
 	err = mk_lang_tchar_dispatch(mk_lang_stdout_windows_println_)(str_buf, str_len); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_print_color_n(mk_lang_stdout_windows_color_text_t const color, mk_lang_types_pchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_print_n(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_print_color_w(mk_lang_stdout_windows_color_text_t const color, mk_lang_types_wchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_print_w(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_print_color_t(mk_lang_stdout_windows_color_text_t const color, mk_lang_tchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_print_t(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_println_color_n(mk_lang_stdout_windows_color_text_t const color, mk_lang_types_pchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_println_n(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_println_color_w(mk_lang_stdout_windows_color_text_t const color, mk_lang_types_wchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_println_w(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_stdout_windows_println_color_t(mk_lang_stdout_windows_color_text_t const color, mk_lang_tchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+	mk_win_base_word_t restore;
+
+	mk_lang_assert(color >= 0);
+	mk_lang_assert(color < mk_lang_stdout_windows_color_text_e_dummy_end);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+
+	err = mk_lang_stdout_windows_pr_color_change(color, &restore); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_println_t(str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lang_stdout_windows_pr_color_restore(&restore); mk_lang_check_rereturn(err);
 	return 0;
 }
 
--- mk_clib/src/mk_win_dll_kernel_console.h
+++ mk_clib/src/mk_win_dll_kernel_console.h
@@ -34,8 +34,42 @@ enum mk_win_dll_kernel_console_std_handle_id_e
 typedef enum mk_win_dll_kernel_console_std_handle_id_e mk_win_dll_kernel_console_std_handle_id_t;
 #include "mk_lang_warning_gcc_pop.h"
 
+enum mk_win_dll_kernel_console_attribute_e
+{
+	mk_win_dll_kernel_console_attribute_e_foreground_blue            = 0x0001,
+	mk_win_dll_kernel_console_attribute_e_foreground_green           = 0x0002,
+	mk_win_dll_kernel_console_attribute_e_foreground_red             = 0x0004,
+	mk_win_dll_kernel_console_attribute_e_foreground_intensity       = 0x0008,
+	mk_win_dll_kernel_console_attribute_e_background_blue            = 0x0010,
+	mk_win_dll_kernel_console_attribute_e_background_green           = 0x0020,
+	mk_win_dll_kernel_console_attribute_e_background_red             = 0x0040,
+	mk_win_dll_kernel_console_attribute_e_background_intensity       = 0x0080,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_leading_byte    = 0x0100,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_trailing_byte   = 0x0200,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_sbcsdbcs        = 0x0300,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_grid_horizontal = 0x0400,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_grid_lvertical  = 0x0800,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_grid_rvertical  = 0x1000,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_reverse_video   = 0x4000,
+	mk_win_dll_kernel_console_attribute_e_common_lvb_underscore      = 0x8000,
+	mk_win_dll_kernel_console_attribute_e_dummy_end = 0xffff
+};
+typedef enum mk_win_dll_kernel_console_attribute_e mk_win_dll_kernel_console_attribute_t;
+
 
 typedef mk_win_base_bool_t(mk_win_base_far mk_win_base_stdcall*mk_win_dll_kernel_console_handler_t)(mk_win_base_dword_t const type) mk_lang_noexcept;
+
+
+struct mk_win_dll_kernel_console_console_screen_buffer_info_s
+{
+	mk_win_base_coord_t m_size;
+	mk_win_base_coord_t m_cursor_position;
+	mk_win_base_word_t m_attributes;
+	mk_win_base_smallrect_t m_window;
+	mk_win_base_coord_t m_maximum_window_size;
+};
+typedef struct mk_win_dll_kernel_console_console_screen_buffer_info_s mk_win_dll_kernel_console_console_screen_buffer_info_t;
+mk_lang_typedef(mk_win_dll_kernel_console_console_screen_buffer_info);
 
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_set_ctrl_handler(mk_win_dll_kernel_console_handler_t const handler, mk_win_base_bool_t const add) mk_lang_noexcept;
@@ -43,6 +77,8 @@ mk_lang_nodiscard mk_lang_jumbo mk_win_base_handle_t mk_win_dll_kernel_console_g
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_get_mode(mk_win_base_handle_t const handle, mk_win_base_dword_lpt const mode) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_a_write(mk_win_base_handle_t const handle, mk_win_base_pchar_lpct const buff, mk_win_base_dword_t const len, mk_win_base_dword_lpt const nwritten, mk_win_base_void_lpt const reserved) mk_lang_noexcept;
 mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_w_write(mk_win_base_handle_t const handle, mk_win_base_wchar_lpct const buff, mk_win_base_dword_t const len, mk_win_base_dword_lpt const nwritten, mk_win_base_void_lpt const reserved) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_get_console_screen_buffer_info(mk_win_base_handle_t const handle, mk_win_dll_kernel_console_console_screen_buffer_info_pt const console_screen_buffer_info) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_bool_t mk_win_dll_kernel_console_set_console_text_attribute(mk_win_base_handle_t const handle, mk_win_base_word_t const attributes) mk_lang_noexcept;
 
 
 #endif
