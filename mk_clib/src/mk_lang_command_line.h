574356f1bb4abae4b9094af8b2423016cf394e3b
Marek Knápek <knapek.mar@gmail.com>
2025-06-25 04:50:44 +0200

    better main

--- mk_clib/src/mk_lang_command_line.h
+++ mk_clib/src/mk_lang_command_line.h
@@ -2,13 +2,15 @@
 #define mk_include_guard_mk_lang_command_line_h
 
 
-#include "mk_lang_types.h"
-#include "mk_lang_noexcept.h"
-#include "mk_lang_nodiscard.h"
 #include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_tchar.h"
+#include "mk_lang_types.h"
 
 
-mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_command_line_parse(mk_lang_types_wchar_pct const cmdline, mk_lang_types_wchar_ppct const argv, mk_lang_types_sint_pt const lens, mk_lang_types_sint_t const count, mk_lang_types_sint_pt const argc) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_command_line_parse_std(mk_lang_types_sint_t const argc_in, mk_lang_types_pchar_pcpct const argv_in, mk_lang_tchar_ppct const argv_out, mk_lang_types_sint_pt const lens_out, mk_lang_types_sint_t const count, mk_lang_types_sint_pt const argc_out) mk_lang_noexcept;
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lang_command_line_parse_win(mk_lang_types_wchar_pct const cmdline, mk_lang_types_wchar_ppct const argv, mk_lang_types_sint_pt const lens, mk_lang_types_sint_t const count, mk_lang_types_sint_pt const argc) mk_lang_noexcept;
 
 
 #if mk_lang_jumbo_have
