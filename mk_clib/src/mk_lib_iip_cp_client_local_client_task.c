e0a60e56933a9b9223f1a4391c6e301e46939b88
Marek Knápek <knapek.mar@gmail.com>
2025-07-08 02:46:36 +0200

    wip local client

--- mk_clib/src/mk_lib_iip_cp_client_local_client_task.c
+++ mk_clib/src/mk_lib_iip_cp_client_local_client_task.c
@@ -627,7 +627,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	mk_lang_assert(connection);
 
 	self = mk_lib_iip_cp_client_local_client_task_prrw_type_to_self(connection); mk_lang_assert(self);
-	err = mk_lib_iip_cp_client_local_client_task_prrw_construct(self, settings); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_cp_client_local_client_task_prrw_construct(self, ((mk_lib_iip_cp_client_local_client_task_settings_pct)(settings))); mk_lang_check_rereturn(err);
 	return 0;
 }
 
@@ -699,7 +699,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	mk_lang_assert(connection);
 
 	self = mk_lib_iip_cp_client_local_client_task_prrw_type_to_self(connection); mk_lang_assert(self);
-	err = mk_lib_iip_cp_client_local_client_task_prrw_step(self, allow_to_block, tm, step_result); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_cp_client_local_client_task_prrw_step(self, allow_to_block, tm, ((mk_lib_iip_cp_client_local_client_task_result_pt)(step_result))); mk_lang_check_rereturn(err);
 	return 0;
 }
 
