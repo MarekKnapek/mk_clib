a5a7d2ee7708a865d409005633918ec68110eb05
Marek Knápek <knapek.mar@gmail.com>
2025-07-23 14:46:05 +0200

    multiple messages in single read

--- mk_clib/src/mk_lib_iip_cp_client_connection_task.c
+++ mk_clib/src/mk_lib_iip_cp_client_connection_task.c
@@ -447,8 +447,24 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	err = mk_lib_iip_cp_dynamic_ring_u8_rw_consolidate(&task->m_connection.m_state.m_buf_rcv); mk_lang_check_rereturn(err);
 	data = mk_lib_iip_cp_dynamic_ring_u8_rw_get_data_a(&task->m_connection.m_state.m_buf_rcv);
 	size = mk_lib_iip_cp_dynamic_ring_u8_rw_get_size_a(&task->m_connection.m_state.m_buf_rcv);
-	err = mk_lib_iip_cp_message_parse_message(data, ((mk_lang_types_sint_t)(size)), &parse_error_code, &consumed, &task->m_connection.m_state.m_msg); mk_lang_check_rereturn(err); mk_lang_check_return(parse_error_code == mk_lib_iip_cp_message_parse_error_code_e_ok); mk_lang_check_return(consumed >= 1); mk_lang_assert(consumed <= ((mk_lang_types_sint_t)(size))); /* todo handle not enough data */
-	err = mk_lib_iip_cp_client_connection_debug_print_msg(task, &task->m_connection.m_state.m_msg, mk_lib_iip_cp_client_connection_debug_print_direction_e_incomming); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_cp_message_parse_message(data, ((mk_lang_types_sint_t)(size)), &parse_error_code, &consumed, &task->m_connection.m_state.m_msg); mk_lang_check_rereturn(err); mk_lang_check_return(parse_error_code == mk_lib_iip_cp_message_parse_error_code_e_ok); mk_lang_check_return(consumed >= 1); mk_lang_assert(consumed <= ((mk_lang_types_sint_t)(size)));
+	switch(parse_error_code)
+	{
+		case mk_lib_iip_cp_message_parse_error_code_e_ok                            : /* nothing */ break;
+		case mk_lib_iip_cp_message_parse_error_code_e_not_enough_data               : err = mk_lib_iip_cp_message_reconstruct(&task->m_connection.m_state.m_msg, mk_lib_iip_cp_message_message_type_id_e_dummy_end); mk_lang_check_rereturn(err); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_too_much_data                 : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_unknown_message_type          : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_unknown_session_status_status : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_unknown_message_status_status : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_unknown_host_reply_result_code: mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_too_many_tunnels              : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_too_many_leases               : mk_lang_check_todo(); break;
+		case mk_lib_iip_cp_message_parse_error_code_e_dummy_end                     : mk_lang_assert_false(); break;
+	}
+	if(parse_error_code == mk_lib_iip_cp_message_parse_error_code_e_ok)
+	{
+		err = mk_lib_iip_cp_client_connection_debug_print_msg(task, &task->m_connection.m_state.m_msg, mk_lib_iip_cp_client_connection_debug_print_direction_e_incomming); mk_lang_check_rereturn(err);
+	}
 	err = mk_lib_iip_cp_dynamic_ring_u8_rw_pop_front_many(&task->m_connection.m_state.m_buf_rcv, consumed); mk_lang_check_rereturn(err);
 	return 0;
 }
@@ -1047,8 +1063,25 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 						}
 						else
 						{
-							err = mk_lib_iip_cp_client_connection_task_prrw_issue_read(task); mk_lang_check_rereturn(err);
-							*step_result = mk_lib_iip_cp_client_connection_task_result_e_did_something;
+							if(!mk_lib_iip_cp_dynamic_ring_u8_rw_is_empty(&task->m_connection.m_state.m_buf_rcv))
+							{
+								err = mk_lib_iip_cp_client_connection_task_prrw_parse_message(task); mk_lang_check_rereturn(err);
+								if(task->m_connection.m_state.m_msg.m_header.m_type != mk_lib_iip_cp_message_message_type_id_e_dummy_end)
+								{
+									task->m_step = mk_lib_iip_cp_client_connection_task_step_e_dispatch_msg;
+									*step_result = mk_lib_iip_cp_client_connection_task_result_e_did_something;
+								}
+								else
+								{
+									err = mk_lib_iip_cp_client_connection_task_prrw_issue_read(task); mk_lang_check_rereturn(err);
+									*step_result = mk_lib_iip_cp_client_connection_task_result_e_did_something;
+								}
+							}
+							else
+							{
+								err = mk_lib_iip_cp_client_connection_task_prrw_issue_read(task); mk_lang_check_rereturn(err);
+								*step_result = mk_lib_iip_cp_client_connection_task_result_e_did_something;
+							}
 						}
 					}
 					else
