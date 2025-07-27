964e4625b0c5412b98e6e4bb15a1e4b010731383
Marek Knápek <knapek.mar@gmail.com>
2025-07-27 14:42:41 +0200

    crc32 fuzz

--- mk_clib/src/mk_win_dll_ntdll.c
+++ mk_clib/src/mk_win_dll_ntdll.c
@@ -18,6 +18,7 @@
 
 mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_dll_ntdll_status_t mk_win_base_stdcall RtlIpv4StringToAddressA(mk_lang_types_pchar_pct const str, mk_win_base_boolean_t const strict, mk_lang_types_pchar_ppct const terminator, mk_win_dll_ws2_ipv4_addr_pt const ipv4addr) mk_lang_noexcept;
 mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_dll_ntdll_status_t mk_win_base_stdcall RtlIpv6StringToAddressA(mk_lang_types_pchar_pct const str, mk_lang_types_pchar_ppct const terminator, mk_win_dll_ws2_ipv6_addr_pt const ipv6addr) mk_lang_noexcept;
+mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_dword_t mk_win_base_stdcall RtlComputeCrc32(mk_win_base_dword_t const initial, mk_win_base_void_pct const data_buf, mk_win_base_sint_t const data_len) mk_lang_noexcept;
 
 
 mk_lang_nodiscard mk_lang_jumbo mk_win_dll_ntdll_status_t mk_win_dll_ntdll_rtl_ipv4_string_to_address_a(mk_lang_types_pchar_pct const str, mk_win_base_boolean_t const strict, mk_lang_types_pchar_ppct const terminator, mk_win_dll_ws2_ipv4_addr_pt const ipv4addr) mk_lang_noexcept
@@ -33,6 +34,14 @@ mk_lang_nodiscard mk_lang_jumbo mk_win_dll_ntdll_status_t mk_win_dll_ntdll_rtl_i
 	mk_win_dll_ntdll_status_t ret;
 
 	ret = RtlIpv6StringToAddressA(str, terminator, ipv6addr);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_dword_t mk_win_dll_ntdll_rtl_compute_crc32(mk_win_base_dword_t const initial, mk_win_base_void_pct const data_buf, mk_win_base_sint_t const data_len) mk_lang_noexcept
+{
+	mk_win_base_dword_t ret;
+
+	ret = RtlComputeCrc32(initial, data_buf, data_len);
 	return ret;
 }
 
