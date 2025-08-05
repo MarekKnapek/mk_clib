00efae02ac61388ecbc0a320c7359ab2c6fc45ef
Marek Knápek <knapek.mar@gmail.com>
2025-08-05 05:33:24 +0200

    wip seq num

--- mk_clib/src/mk_lib_iip_cp_client_socket.h
+++ mk_clib/src/mk_lib_iip_cp_client_socket.h
@@ -91,7 +91,8 @@ struct mk_lib_iip_cp_client_socket_state_s
 	mk_sl_cui_uint16_t m_remote_port;
 	mk_sl_cui_uint32_t m_local_stream_id;
 	mk_sl_cui_uint32_t m_remote_stream_id;
-	mk_sl_cui_uint32_t m_sequence_number;
+	mk_sl_cui_uint32_t m_local_sequence_number;
+	mk_sl_cui_uint32_t m_remote_sequence_number;
 	mk_lib_iip_cp_types_remote_destination_t m_remote_destination;
 	mk_lang_types_pchar_t m_remote_b32[mk_lang_roundup_div(mk_lib_crypto_hash_block_sha2_256_digest_len_v * 8, 5)];
 	mk_lib_iip_cp_dynamic_ring_u8_t m_data_to_sent;
