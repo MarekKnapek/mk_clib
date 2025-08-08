e160ddfec0058094c552158be178b11cb5ea0667
Marek Knápek <knapek.mar@gmail.com>
2025-08-08 06:18:37 +0200

    fixes

--- mk_clib/src/mk_sl_dynamic_ring.c
+++ mk_clib/src/mk_sl_dynamic_ring.c
@@ -36,7 +36,7 @@ mk_lang_nodiscard mk_lang_constexpr mk_lang_jumbo mk_lang_types_sint_t mk_sl_dyn
 	mk_lang_assert(dst);
 	mk_lang_assert(src);
 
-	mk_lang_debug_break();
+	mk_sl_cui_uint8_assign(dst, src);
 	return 0;
 }
 
