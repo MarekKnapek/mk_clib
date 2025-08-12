11f91d09e28389bc79db43528724eb086c4ae198
Marek Knápek <knapek.mar@gmail.com>
2025-08-12 22:45:59 +0200

    mk_lib_iip_cp_bytes

--- mk_clib/src/mk_lib_iip_cp_bytes.h
+++ mk_clib/src/mk_lib_iip_cp_bytes.h
@@ -2,31 +2,24 @@
 #define mk_include_guard_mk_lib_iip_cp_bytes_h
 
 
+#include "mk_lang_concat.h"
 #include "mk_lang_jumbo.h"
 #include "mk_lang_typedef.h"
 #include "mk_sl_cui_uint8.h"
 
 
-struct mk_lib_iip_cp_bytes_20_s
-{
-	mk_sl_cui_uint8_t m_bytes[20];
-};
-typedef struct mk_lib_iip_cp_bytes_20_s mk_lib_iip_cp_bytes_20_t;
-mk_lang_typedef(mk_lib_iip_cp_bytes_20);
+#define mk_lib_iip_cp_bytes_make(x) \
+struct mk_lang_concat(mk_lang_concat(mk_lib_iip_cp_bytes_, x), _s) \
+{ \
+	mk_sl_cui_uint8_t m_bytes[x]; \
+}; \
+typedef struct mk_lang_concat(mk_lang_concat(mk_lib_iip_cp_bytes_, x), _s) mk_lang_concat(mk_lang_concat(mk_lib_iip_cp_bytes_, x), _t); \
+mk_lang_typedef(mk_lang_concat(mk_lib_iip_cp_bytes_, x))
 
-struct mk_lib_iip_cp_bytes_128_s
-{
-	mk_sl_cui_uint8_t m_bytes[128];
-};
-typedef struct mk_lib_iip_cp_bytes_128_s mk_lib_iip_cp_bytes_128_t;
-mk_lang_typedef(mk_lib_iip_cp_bytes_128);
 
-struct mk_lib_iip_cp_bytes_256_s
-{
-	mk_sl_cui_uint8_t m_bytes[256];
-};
-typedef struct mk_lib_iip_cp_bytes_256_s mk_lib_iip_cp_bytes_256_t;
-mk_lang_typedef(mk_lib_iip_cp_bytes_256);
+mk_lib_iip_cp_bytes_make(20);
+mk_lib_iip_cp_bytes_make(128);
+mk_lib_iip_cp_bytes_make(256);
 
 
 #if mk_lang_jumbo_have
