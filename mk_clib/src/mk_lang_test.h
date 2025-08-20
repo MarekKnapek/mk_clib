dcbe4c772cc8bce198cba2c4af052e605df466d7
Marek Knápek <knapek.mar@gmail.com>
2025-08-21 00:57:48 +0200

    macros

--- mk_clib/src/mk_lang_test.h
+++ mk_clib/src/mk_lang_test.h
@@ -2,14 +2,10 @@
 #define mk_include_guard_mk_lang_test_h
 
 
-#include "mk_lang_types.h"
-#include "mk_lang_stringify.h"
-#include "mk_lang_runtime_bool.h"
-#include "mk_lang_noexcept.h"
-#include "mk_lang_jumbo.h"
-#include "mk_lang_debug_break.h"
+#include "mk_lang_configuration.h"
 #include "mk_lang_crash.h"
-#include "mk_lang_countof.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_types.h"
 
 
 #if defined mk_lang_test_detail_want && (mk_lang_test_detail_want) == 1
@@ -17,14 +13,12 @@
 #elif defined mk_lang_test_detail_want && (mk_lang_test_detail_want) == 0
 #define mk_lang_test_detail_have 0
 #elif !defined mk_lang_test_detail_want
-#if defined DEBUG || defined _DEBUG
+#if mk_lang_configuration_is_debug
+#define mk_lang_test_detail_have 1
+#elif mk_lang_configuration_is_release
 #define mk_lang_test_detail_have 1
 #else
-#if defined NDEBUG || defined _NDEBUG
-#define mk_lang_test_detail_have 1
-#else
-#define mk_lang_test_detail_have 1
-#endif
+#error xxxxxxxxxx
 #endif
 #else
 #error xxxxxxxxxx
@@ -32,15 +26,20 @@
 
 
 #if mk_lang_test_detail_have
-mk_lang_jumbo mk_lang_types_void_t mk_lang_test_detail_impl(mk_lang_types_pchar_pct const msg_buf, mk_lang_types_sint_t const msg_len) mk_lang_noexcept;
+#include "mk_lang_countof.h"
+#include "mk_lang_debug_break.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_stringify.h"
+mk_lang_jumbo mk_lang_types_void_t mk_lang_test_detail_print_impl(mk_lang_types_pchar_pct const msg_buf, mk_lang_types_sint_t const msg_len) mk_lang_noexcept;
 #define mk_lang_test_detail_lit(x) x, mk_lang_countstr(x)
-#define mk_lang_test_detail_print() mk_lang_test_detail_impl(mk_lang_test_detail_lit("fail--->" __FILE__ ":" mk_lang_stringify(__LINE__) "<---fail"))
+#define mk_lang_test_detail_print(x) mk_lang_test_detail_print_impl(mk_lang_test_detail_lit("test_failed--->" __FILE__ ":" mk_lang_stringify(__LINE__) "`" mk_lang_stringify(x) "`<---test_failed"))
+#define mk_lang_test_failed(x) ((mk_lang_types_void_t)(mk_lang_test_detail_print(x), mk_lang_debug_break(), mk_lang_crash()))
 #else
-#define mk_lang_test_detail_print()
+#define mk_lang_test_failed(x) ((mk_lang_types_void_t)(mk_lang_crash()))
 #endif
 
 
-#define mk_lang_test(x) do{ if(!(x)){ mk_lang_test_detail_print(); mk_lang_debug_break(); mk_lang_crash(); } }while(mk_lang_runtime_bool_fn_false)
+#define mk_lang_test(x) ((mk_lang_types_void_t)((x) ? ((mk_lang_types_void_t)(0)) : ((mk_lang_types_void_t)(mk_lang_test_failed(x)))))
 
 
 #if mk_lang_jumbo_have
