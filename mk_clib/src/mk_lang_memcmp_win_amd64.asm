34d0c3fe23bfa0862fd91be5d8123d21807ce271
Marek Knápek <knapek.mar@gmail.com>
2025-08-22 09:46:19 +0200

    RtlCompareMemory

--- mk_clib/src/mk_lang_memcmp_win_amd64.asm
+++ mk_clib/src/mk_lang_memcmp_win_amd64.asm
@@ -3,7 +3,32 @@ RtlCompareMemory proto
 .code
 
 memcmp proc
-	jmp RtlCompareMemory
+	mov qword ptr [rsp +  8], rbx
+	mov qword ptr [rsp + 16], rsi
+	push rdi
+	sub rsp, 32
+	mov rbx, r8
+	mov rdi, rdx
+	mov rsi, rcx
+	call RtlCompareMemory
+	mov r8, rax
+	cmp rax, rbx
+	jne nonequal
+	xor eax, eax
+	mov rbx, qword ptr [ rsp + 48 ]
+	mov rsi, qword ptr [ rsp + 56 ]
+	add rsp, 32
+	pop rdi
+	ret 0
+nonequal:
+	movzx eax, byte ptr [ rsi + rax ]
+	movzx ecx, byte ptr [ rdi + r8 ]
+	mov rbx, qword ptr [ rsp + 48 ]
+	sub eax, ecx
+	mov rsi, qword ptr [ rsp + 56 ]
+	add rsp, 32
+	pop rdi
+	ret 0
 memcmp endp
 
 end
