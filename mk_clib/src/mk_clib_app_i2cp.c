a32b5032fa79079f0e1c8c7d3afd33c9aa78bf04
Marek Knápek <knapek.mar@gmail.com>
2025-08-25 23:54:46 +0200

    mk_clib_app_i2cp_checker

--- mk_clib/src/mk_clib_app_i2cp.c
+++ mk_clib/src/mk_clib_app_i2cp.c
@@ -157,7 +157,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_clib_app_i2cp_ch
 	{
 		mk_lang_assert(consumed_in >= 1);
 		mk_lang_assert(consumed_in <= data_len);
-		err = mk_lib_iip_cp_message_serialize_message(&out_buf[0], mk_lang_countof(out_buf), &error_code_out, &consumed_out, &msg); mk_lang_check_rereturn(err);
+		error_code_out = mk_lib_iip_cp_message_serialize_error_code_e_ok; err = mk_lib_iip_cp_message_serialize_message(&out_buf[0], mk_lang_countof(out_buf), &error_code_out, &consumed_out, &msg); mk_lang_check_rereturn(err);
 		mk_lang_check_return(error_code_out == mk_lib_iip_cp_message_serialize_error_code_e_ok);
 		mk_lang_check_return(consumed_out == consumed_in);
 		mk_lang_check_return(mk_sl_cui_uint8_memcmp_fn(&out_buf[0], data_buf, consumed_out) == 0);
