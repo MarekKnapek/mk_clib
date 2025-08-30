f4e8baf81d176c97e6cf100d419cee44fbc3a1ff
Marek Knápek <knapek.mar@gmail.com>
2025-08-30 21:47:07 +0200

    Fix natvis.

--- mk_clib/src/mk_lib_iip_cp_client_local_client_task.c
+++ mk_clib/src/mk_lib_iip_cp_client_local_client_task.c
@@ -600,6 +600,12 @@ mk_lang_nodiscard static mk_lang_inline mk_lib_iip_cp_client_local_client_task_p
 	mk_lib_iip_cp_client_local_client_pt local_listener;
 	mk_lib_iip_cp_client_local_client_task_pt self;
 
+	/* for natvis purposes */
+	mk_lang_assert(mk_lang_offsetof(mk_lib_iip_cp_client_local_client_task_settings_t, m_type) == 0);
+	mk_lang_assert(mk_lang_offsetof(mk_lib_iip_cp_client_local_client_t, m_settings) == 0);
+	mk_lang_assert(mk_lang_offsetof(mk_lib_iip_cp_client_local_client_task_t, m_local) == sizeof(mk_lang_types_void_pt));
+	/* for natvis purposes */
+
 	mk_lang_assert(connection);
 
 	#include "mk_lang_warning_clang_push_cast_align.h"
