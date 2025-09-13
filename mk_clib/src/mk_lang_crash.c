f0896879f41694bc3996efdad88e4a56b9e00a4c
Marek Knápek <knapek.mar@gmail.com>
2025-09-13 21:27:27 +0200

    mk_lang_crash

--- mk_clib/src/mk_lang_crash.c
+++ mk_clib/src/mk_lang_crash.c
@@ -2,7 +2,9 @@
 #define mk_include_guard_mk_lang_crash_c
 #include "mk_lang_crash.h"
 
+#include "mk_lang_arch.h"
 #include "mk_lang_debug_break.h"
+#include "mk_lang_inline.h"
 #include "mk_lang_jumbo.h"
 #include "mk_lang_noexcept.h"
 #include "mk_lang_noreturn.h"
@@ -13,11 +15,15 @@
 
 
 #include "mk_lang_warning_msvc_push_c4746.h"
-mk_lang_noreturn mk_lang_jumbo mk_lang_types_void_t mk_lang_crash_impl(mk_lang_types_void_t) mk_lang_noexcept
+mk_lang_noreturn static mk_lang_inline mk_lang_types_void_t mk_lang_crash_impl(mk_lang_types_void_t) mk_lang_noexcept
 {
 	mk_lang_types_sint_pvt volatile ptr;
 
+	#if !mk_lang_arch_is_emscripten
 	ptr = mk_lang_null;
+	#else
+	ptr = ((mk_lang_types_sint_pvt)(((mk_lang_types_uintptr_t)(((mk_lang_types_ulong_t)(3ul * 1024ul * 1024ul * 1024ul)))))); /* Sure we have nothing at 3GB mark. Right? Hope this triggers crash or something. */
+	#endif
 	*ptr = 0;
 
 	abort();
