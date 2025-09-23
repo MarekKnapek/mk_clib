03cae1aa9978d4bc8c0d070354f2980f24fc3d5d
Marek Knápek <knapek.mar@gmail.com>
2025-09-24 00:52:14 +0200

    msvc

--- mk_clib/src/mk_lang_msvc.h
+++ mk_clib/src/mk_lang_msvc.h
@@ -11,6 +11,7 @@
 #endif
 
 
+#define mk_lang_msvc_ver_2022_17_14_16 1944l
 #define mk_lang_msvc_ver_2022_17_14_15 1944l
 #define mk_lang_msvc_ver_2022_17_14_14 1944l
 #define mk_lang_msvc_ver_2022_17_14_13 1944l
@@ -119,6 +120,7 @@
 #define mk_lang_msvc_ver_6         1200l
 
 
+#define mk_lang_msvc_full_ver_2022_17_14_16 194435217l
 #define mk_lang_msvc_full_ver_2022_17_14_15 194435216l
 #define mk_lang_msvc_full_ver_2022_17_14_14 194435216l
 #define mk_lang_msvc_full_ver_2022_17_14_13 194435215l
