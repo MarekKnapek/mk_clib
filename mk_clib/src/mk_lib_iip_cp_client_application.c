1b4d8d54b2eee26af25b3dcc3a7559379b1cb192
Marek Knápek <knapek.mar@gmail.com>
2025-09-26 16:38:58 +0200

    mk_lib_iip_cp_client_application_task_rw_log

--- mk_clib/src/mk_lib_iip_cp_client_application.c
+++ mk_clib/src/mk_lib_iip_cp_client_application.c
@@ -155,6 +155,21 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	return 0;
 }
 
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_client_application_task_prrw_log(mk_lib_iip_cp_client_application_task_pt const task, mk_lang_types_pchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	mk_lib_iip_logger_pt logger;
+	mk_lang_types_sint_t err;
+
+	mk_lang_assert(task);
+
+	logger = &task->m_application.m_state.m_shared.m_logger;
+	err = mk_lib_iip_logger_rw_begin_line(logger); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_logger_rw_append_current_time(logger); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_logger_rw_print(logger, str_buf, str_len); mk_lang_check_rereturn(err);
+	err = mk_lib_iip_logger_rw_end_line(logger); mk_lang_check_rereturn(err);
+	return 0;
+}
+
 mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_client_application_task_prrw_request_close(mk_lib_iip_cp_client_application_task_pt const task) mk_lang_noexcept
 {
 	mk_lang_types_sint_t err;
@@ -254,6 +269,11 @@ mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_iip_cp_client_applic
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_iip_cp_client_application_task_rw_destroy(mk_lib_iip_cp_client_application_task_pt const task) mk_lang_noexcept
 {
 	return mk_lib_iip_cp_client_application_task_prrw_destroy(task);
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_iip_cp_client_application_task_rw_log(mk_lib_iip_cp_client_application_task_pt const task, mk_lang_types_pchar_pct const str_buf, mk_lang_types_sint_t const str_len) mk_lang_noexcept
+{
+	return mk_lib_iip_cp_client_application_task_prrw_log(task, str_buf, str_len);
 }
 
 mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_lib_iip_cp_client_application_task_rw_request_close(mk_lib_iip_cp_client_application_task_pt const task) mk_lang_noexcept
