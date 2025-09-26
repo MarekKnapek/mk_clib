e56fbaa8906258b677f1d3ba511f1889c9c46f2a
Marek Knápek <knapek.mar@gmail.com>
2025-09-26 20:19:38 +0200

    Better stop handling.

--- mk_clib/src/mk_lib_iip_cp_client_session.c
+++ mk_clib/src/mk_lib_iip_cp_client_session.c
@@ -421,6 +421,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	task->m_session.m_state.m_has_id = mk_lang_false;
 	task->m_session.m_state.m_child_sessions = 0;
 	task->m_session.m_state.m_stop_requested = mk_lang_false;
+	task->m_session.m_state.m_stop_acknowledged = mk_lang_false;
 	err = mk_lib_iip_cp_message_construct(&task->m_session.m_state.m_msg, mk_lib_iip_cp_message_message_type_id_e_dummy_end); mk_lang_check_rereturn(err);
 	task->m_session.m_state.m_has_msg_pending = mk_lang_false;
 	err = mk_lib_iip_cp_types_leasez_rw_construct(&task->m_session.m_state.m_leases); mk_lang_check_rereturn(err);
@@ -1763,6 +1764,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 
 mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_client_session_task_prrw_step_idle_construct_destroy_session_msg(mk_lib_iip_cp_client_session_task_pt const task, mk_lib_iip_cp_client_session_task_result_pt const step_result) mk_lang_noexcept
 {
+	mk_lib_iip_cp_client_session_task_result_t stp_res;
 	mk_lang_types_sint_t err;
 	mk_lib_iip_cp_message_destroy_session_pt msg_destroy_session;
 
@@ -1771,24 +1773,26 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_lib_iip_cp_clien
 	mk_lang_assert(*step_result == mk_lib_iip_cp_client_session_task_result_e_dummy_end);
 	mk_lang_assert(task->m_step == mk_lib_iip_cp_client_session_task_step_e_idle);
 
-	if
-	(
-		(task->m_session.m_state.m_stop_requested && !task->m_session.m_state.m_has_msg_pending && !mk_lib_iip_cp_client_session_handle_is_zero(&task->m_session.m_settings.m_master_session)) ||
-		(task->m_session.m_state.m_stop_requested && !task->m_session.m_state.m_has_msg_pending && mk_lib_iip_cp_client_session_handle_is_zero(&task->m_session.m_settings.m_master_session) && task->m_session.m_state.m_child_sessions == 0) ||
-		(mk_lang_false)
-	)
+	stp_res = mk_lib_iip_cp_client_session_task_result_e_idling;
+	if(task->m_session.m_state.m_stop_requested && !task->m_session.m_state.m_stop_acknowledged)
 	{
-		err = mk_lib_iip_cp_message_reconstruct(&task->m_session.m_state.m_msg, mk_lib_iip_cp_message_message_type_id_e_destroy_session); mk_lang_check_rereturn(err);
-		msg_destroy_session = &task->m_session.m_state.m_msg.m_mix.m_data.m_destroy_session;
-		msg_destroy_session->m_session_id = task->m_session.m_state.m_id;
-		task->m_session.m_state.m_has_msg_pending = mk_lang_true;
-		task->m_step = mk_lib_iip_cp_client_session_task_step_e_pickup_msg_destroy_session;
-		*step_result = mk_lib_iip_cp_client_session_task_result_e_did_something;
+		if
+		(
+			(!task->m_session.m_state.m_has_msg_pending && !mk_lib_iip_cp_client_session_handle_is_zero(&task->m_session.m_settings.m_master_session)) ||
+			(!task->m_session.m_state.m_has_msg_pending && mk_lib_iip_cp_client_session_handle_is_zero(&task->m_session.m_settings.m_master_session) && task->m_session.m_state.m_child_sessions == 0) ||
+			(mk_lang_false)
+		)
+		{
+			task->m_session.m_state.m_stop_acknowledged = mk_lang_true;
+			err = mk_lib_iip_cp_message_reconstruct(&task->m_session.m_state.m_msg, mk_lib_iip_cp_message_message_type_id_e_destroy_session); mk_lang_check_rereturn(err);
+			msg_destroy_session = &task->m_session.m_state.m_msg.m_mix.m_data.m_destroy_session;
+			msg_destroy_session->m_session_id = task->m_session.m_state.m_id;
+			task->m_session.m_state.m_has_msg_pending = mk_lang_true;
+			task->m_step = mk_lib_iip_cp_client_session_task_step_e_pickup_msg_destroy_session;
+			stp_res = mk_lib_iip_cp_client_session_task_result_e_did_something;
+		}
 	}
-	else
-	{
-		*step_result = mk_lib_iip_cp_client_session_task_result_e_idling;
-	}
+	*step_result = stp_res;
 	return 0;
 }
 
