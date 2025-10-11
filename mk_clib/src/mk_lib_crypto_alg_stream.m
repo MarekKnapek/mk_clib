866f30d87f5e2b4378cac239adf1dae0ee225200
Marek Knápek <knapek.mar@gmail.com>
2025-10-11 17:29:50 +0200

    fff stream

--- mk_clib/src/mk_lib_crypto_alg_stream.m
+++ mk_clib/src/mk_lib_crypto_alg_stream.m
@@ -6,4 +6,4 @@ define(`mk_lib_crypto_alg_stream_mm_list', `dnl
 `'')dnl
 define(`mk_lib_crypto_alg_stream_mm_hlf', `ifelse(`$#', `1', ``$1'', ``'`$1', mk_lib_crypto_alg_stream_mm_hlf(shift(shift($@)))')')`'dnl
 define(`mk_lib_crypto_alg_stream_mm_half', `mk_lib_crypto_alg_stream_mm_hlf(mk_lib_crypto_alg_stream_mm_list())')`'dnl
-define(`mk_lib_crypto_alg_stream_mm_recurse', `ifelse(`$#', `2', `', `$1($2)')')`'dnl
+define(`mk_lib_crypto_alg_stream_mm_recurse', `ifelse(`$#', `2', `', `$1(`$2')mk_lib_crypto_alg_stream_mm_recurse(`$1', shift(shift($@)))')')`'dnl
