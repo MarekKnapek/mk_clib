efb346ea2f8e51f765c1a174de98d1f59113d117
Marek Knápek <knapek.mar@gmail.com>
2025-10-13 20:49:35 +0200

    dir

---
+++ mk_clib/src/mk_sl_io_directory_iterator.c
@@ -0,0 +1,110 @@
+#ifndef mk_include_guard_mk_sl_io_directory_iterator_c
+#define mk_include_guard_mk_sl_io_directory_iterator_c
+#include "mk_sl_io_directory_iterator.h"
+
+#include "mk_lang_assert.h"
+#include "mk_lang_concat.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_platform.h"
+#include "mk_lang_tchar.h"
+#include "mk_lang_types.h"
+#include "mk_sl_cui_uint8.h"
+
+
+#if mk_lang_platform_is_windows_at_least_any
+#include "mk_sl_io_directory_iterator_windows.h"
+#define mk_sl_io_directory_iterator_impl_open_n   mk_sl_io_directory_iterator_windows_open_n
+#define mk_sl_io_directory_iterator_impl_open_w   mk_sl_io_directory_iterator_windows_open_w
+#define mk_sl_io_directory_iterator_impl_read     mk_sl_io_directory_iterator_windows_read
+#define mk_sl_io_directory_iterator_impl_seek_rel mk_sl_io_directory_iterator_windows_seek_rel
+#define mk_sl_io_directory_iterator_impl_close    mk_sl_io_directory_iterator_windows_close
+#elif mk_lang_platform_is_linux
+#include "mk_sl_io_directory_iterator_linux.h"
+#define mk_sl_io_directory_iterator_impl_open_n   mk_sl_io_directory_iterator_linux_open_n
+#define mk_sl_io_directory_iterator_impl_open_w   mk_sl_io_directory_iterator_linux_open_w
+#define mk_sl_io_directory_iterator_impl_read     mk_sl_io_directory_iterator_linux_read
+#define mk_sl_io_directory_iterator_impl_seek_rel mk_sl_io_directory_iterator_linux_seek_rel
+#define mk_sl_io_directory_iterator_impl_close    mk_sl_io_directory_iterator_linux_close
+#elif mk_lang_platform_is_posix_at_least_any
+#include "mk_sl_io_directory_iterator_posix.h"
+#define mk_sl_io_directory_iterator_impl_open_n   mk_sl_io_directory_iterator_posix_open_n
+#define mk_sl_io_directory_iterator_impl_open_w   mk_sl_io_directory_iterator_posix_open_w
+#define mk_sl_io_directory_iterator_impl_read     mk_sl_io_directory_iterator_posix_read
+#define mk_sl_io_directory_iterator_impl_seek_rel mk_sl_io_directory_iterator_posix_seek_rel
+#define mk_sl_io_directory_iterator_impl_close    mk_sl_io_directory_iterator_posix_close
+#elif mk_lang_platform_is_portable
+#include "mk_sl_io_directory_iterator_portable.h"
+#define mk_sl_io_directory_iterator_impl_open_n   mk_sl_io_directory_iterator_portable_open_n
+#define mk_sl_io_directory_iterator_impl_open_w   mk_sl_io_directory_iterator_portable_open_w
+#define mk_sl_io_directory_iterator_impl_read     mk_sl_io_directory_iterator_portable_read
+#define mk_sl_io_directory_iterator_impl_seek_rel mk_sl_io_directory_iterator_portable_seek_rel
+#define mk_sl_io_directory_iterator_impl_close    mk_sl_io_directory_iterator_portable_close
+#else
+#error xxxxxxxxxx
+#endif
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_open_n(mk_sl_io_directory_iterator_pt const reader, mk_lang_types_pchar_pct const name) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_sl_io_directory_iterator_impl_open_n(&reader->m_file_handle, name);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_open_w(mk_sl_io_directory_iterator_pt const reader, mk_lang_types_wchar_pct const name) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_sl_io_directory_iterator_impl_open_w(&reader->m_file_handle, name);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_open_t(mk_sl_io_directory_iterator_pt const reader, mk_lang_tchar_pct const name) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_lang_concat(mk_sl_io_directory_iterator_impl_open_, mk_lang_tchar_suffix)(&reader->m_file_handle, name);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_read(mk_sl_io_directory_iterator_pt const reader, mk_sl_cui_uint8_pt const buf, mk_lang_types_sint_t const len, mk_lang_types_sint_pt const read) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_sl_io_directory_iterator_impl_read(&reader->m_file_handle, buf, len, read);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_seek_rel(mk_sl_io_directory_iterator_pt const reader, mk_lang_types_slong_t const offset) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_sl_io_directory_iterator_impl_seek_rel(&reader->m_file_handle, offset);
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_close(mk_sl_io_directory_iterator_pt const reader) mk_lang_noexcept
+{
+	mk_lang_types_sint_t ret;
+
+	mk_lang_assert(reader);
+
+	ret = mk_sl_io_directory_iterator_impl_close(&reader->m_file_handle);
+	return ret;
+}
+
+
+#endif
---
+++ mk_clib/src/mk_sl_io_directory_iterator_windows.c
@@ -0,0 +1,103 @@
+#ifndef mk_include_guard_mk_sl_io_directory_iterator_windows_c
+#define mk_include_guard_mk_sl_io_directory_iterator_windows_c
+#include "mk_sl_io_directory_iterator_windows.h"
+
+#include "mk_lang_platform.h"
+#if mk_lang_platform_is_windows_at_least_any
+
+#include "mk_lang_assert.h"
+#include "mk_lang_bool.h"
+#include "mk_lang_check.h"
+#include "mk_lang_jumbo.h"
+#include "mk_lang_nodiscard.h"
+#include "mk_lang_noexcept.h"
+#include "mk_lang_null.h"
+#include "mk_lang_restrict.h"
+#include "mk_lang_types.h"
+#include "mk_sl_cui_uint8.h"
+#include "mk_win_dll_advapi.h"
+#include "mk_win_dll_kernel_errors.h"
+#include "mk_win_dll_kernel_files.h"
+#include "mk_win_dll_kernel_handle.h"
+
+
+#define mk_sl_io_directory_iterator_windows_is_valid(x) (!mk_win_base_handle_is_invalid(x))
+
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_rw_construct_void(mk_sl_io_directory_iterator_windows_pt const writer) mk_lang_noexcept
+{
+	mk_lang_assert(writer);
+
+	writer->m_file_handle = mk_win_base_handle_get_invalid();
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_open_n(mk_sl_io_directory_iterator_windows_pt const writer, mk_lang_types_pchar_pct const name) mk_lang_noexcept
+{
+	mk_sl_io_directory_iterator_windows_handle_t handle;
+
+	mk_lang_assert(writer);
+	mk_lang_assert(name && name[0] != '\0');
+
+	handle = mk_win_dll_kernel_files_a_create_file(name, ((mk_win_base_dword_t)(mk_win_dll_advapi_right_generic_e_write)), mk_win_dll_kernel_files_share_e_delete, mk_win_base_null, mk_win_dll_kernel_files_create_e_create_always, mk_win_dll_kernel_files_flag_e_none, mk_win_base_handle_get_null()); mk_lang_check_return(mk_sl_io_directory_iterator_windows_is_valid(handle));
+	writer->m_file_handle = handle;
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_open_w(mk_sl_io_directory_iterator_windows_pt const writer, mk_lang_types_wchar_pct const name) mk_lang_noexcept
+{
+	mk_sl_io_directory_iterator_windows_handle_t handle;
+
+	mk_lang_assert(writer);
+	mk_lang_assert(name && name[0] != L'\0');
+
+	handle = mk_win_dll_kernel_files_w_create_file(name, ((mk_win_base_dword_t)(mk_win_dll_advapi_right_generic_e_write)), mk_win_dll_kernel_files_share_e_delete, mk_win_base_null, mk_win_dll_kernel_files_create_e_create_always, mk_win_dll_kernel_files_flag_e_none, mk_win_base_handle_get_null()); mk_lang_check_return(mk_sl_io_directory_iterator_windows_is_valid(handle));
+	writer->m_file_handle = handle;
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_write(mk_sl_io_directory_iterator_windows_pt const writer, mk_sl_cui_uint8_pct const buf, mk_lang_types_sint_t const len, mk_lang_types_sint_pt const written) mk_lang_noexcept
+{
+	mk_win_base_bool_t ret;
+	mk_win_base_dword_t w;
+
+	mk_lang_assert(writer);
+	mk_lang_assert(buf);
+	mk_lang_assert(len >= 0);
+	mk_lang_assert(written);
+	mk_lang_assert(mk_sl_io_directory_iterator_windows_is_valid(writer->m_file_handle));
+
+	ret = mk_win_dll_kernel_files_write_file(writer->m_file_handle, buf, ((mk_win_base_dword_t)(len)), &w, mk_win_base_null); mk_lang_check_return(ret != 0);
+	*written = ((mk_lang_types_sint_t)(w));
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_close(mk_sl_io_directory_iterator_windows_pt const writer) mk_lang_noexcept
+{
+	mk_win_base_bool_t ret;
+
+	mk_lang_assert(writer);
+
+	if(mk_sl_io_directory_iterator_windows_is_valid(writer->m_file_handle))
+	{
+		ret = mk_win_dll_kernel_handle_close(writer->m_file_handle); mk_lang_check_return(ret != 0);
+	}
+	return 0;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_lang_types_sint_t mk_sl_io_directory_iterator_windows_rw_reset(mk_sl_io_directory_iterator_windows_pt const writer) mk_lang_noexcept
+{
+	mk_lang_types_sint_t err;
+
+	mk_lang_assert(writer);
+
+	err = mk_sl_io_directory_iterator_windows_close(writer); mk_lang_check_rereturn(err);
+	err = mk_sl_io_directory_iterator_windows_rw_construct_void(writer); mk_lang_check_rereturn(err);
+	return 0;
+}
+
+
+#else
+static void mk_sl_io_directory_iterator_windows_dummy(void){}
+#endif
+#endif
