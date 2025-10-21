672b478d322301d0a7364f2a71344a44c4c237d9
Marek Knápek <knapek.mar@gmail.com>
2025-10-21 15:52:33 +0200

    mk_lib_crypto_alg_serpent_base

--- mk_clib/src/mk_lib_crypto_alg_serpent_base.c
+++ mk_clib/src/mk_lib_crypto_alg_serpent_base.c
@@ -2,13 +2,14 @@
 #define mk_include_guard_mk_lib_crypto_alg_serpent_base_c
 #include "mk_lib_crypto_alg_serpent_base.h"
 
+#include "mk_lang_inline.h"
 #include "mk_lang_types.h"
 
 
 #include "mk_lang_warning_gcc_push_unused_function.h"
 #include "mk_lang_warning_clang_push_unused_function.h"
-mk_lang_types_void_t mk_lib_crypto_alg_serpent_base_dummy(mk_lang_types_void_t);
-mk_lang_types_void_t mk_lib_crypto_alg_serpent_base_dummy(mk_lang_types_void_t){}
+mk_lang_inline mk_lang_types_void_t mk_lib_crypto_alg_serpent_base_dummy(mk_lang_types_void_t);
+mk_lang_inline mk_lang_types_void_t mk_lib_crypto_alg_serpent_base_dummy(mk_lang_types_void_t){}
 #include "mk_lang_warning_clang_pop.h"
 #include "mk_lang_warning_gcc_pop.h"
 #endif
