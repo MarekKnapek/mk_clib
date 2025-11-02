02c5c66e042c3eb4a75549cea43d3890d13f8db2
Marek Knápek <knapek.mar@gmail.com>
2025-11-02 19:07:37 +0100

    GetTickCount64

--- mk_clib/src/mk_win_dll_kernel_time.c
+++ mk_clib/src/mk_win_dll_kernel_time.c
@@ -14,6 +14,7 @@
 
 
 mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_dword_t mk_win_base_stdcall GetTickCount(mk_lang_types_void_t) mk_lang_noexcept;
+mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_ullong_t mk_win_base_stdcall GetTickCount64(mk_lang_types_void_t) mk_lang_noexcept;
 mk_lang_extern_force_c mk_win_base_dll_import mk_lang_types_void_t mk_win_base_stdcall GetSystemTime(mk_win_dll_kernel_time_system_time_lpt const system_time) mk_lang_noexcept;
 mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall SystemTimeToFileTime(mk_win_dll_kernel_time_system_time_lpct const system_time, mk_win_dll_kernel_time_file_time_lpt const file_time) mk_lang_noexcept;
 mk_lang_extern_force_c mk_lang_nodiscard mk_win_base_dll_import mk_win_base_bool_t mk_win_base_stdcall FileTimeToSystemTime(mk_win_dll_kernel_time_file_time_lpct const file_time, mk_win_dll_kernel_time_system_time_lpt const system_time) mk_lang_noexcept;
@@ -27,6 +28,14 @@ mk_lang_nodiscard mk_lang_jumbo mk_win_base_dword_t mk_win_dll_kernel_time_get_t
 	mk_win_base_dword_t ret;
 
 	ret = GetTickCount();
+	return ret;
+}
+
+mk_lang_nodiscard mk_lang_jumbo mk_win_base_ullong_t mk_win_dll_kernel_time_get_tick_count64(mk_lang_types_void_t) mk_lang_noexcept
+{
+	mk_win_base_ullong_t ret;
+
+	ret = GetTickCount64();
 	return ret;
 }
 
