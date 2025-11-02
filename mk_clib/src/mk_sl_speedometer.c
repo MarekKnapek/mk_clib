90e1430dd7c15271f359014c3996d467457315b0
Marek Knápek <knapek.mar@gmail.com>
2025-11-02 21:21:39 +0100

    mk_sl_speedometer

--- mk_clib/src/mk_sl_speedometer.c
+++ mk_clib/src/mk_sl_speedometer.c
@@ -129,14 +129,10 @@ mk_lang_constexpr static mk_lang_inline mk_lang_types_void_t mk_sl_speedometer_p
 	mk_sl_fxp_128_64_64_from_uint64(fxp, &cui);
 }
 
-mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_sl_speedometer_pr_report(mk_sl_speedometer_pt const speedometer, mk_lang_tchar_pt const str_buf, mk_lang_types_sint_t const str_len, mk_lang_types_bool_pt const reported, mk_lang_types_sint_pt const out_len) mk_lang_noexcept
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_sl_speedometer_pr_report_impl(mk_sl_speedometer_pt const speedometer, mk_lang_tchar_pt const str_buf, mk_lang_types_sint_t const str_len, mk_lang_types_bool_pt const reported, mk_lang_types_sint_pt const out_len) mk_lang_noexcept
 {
 	mk_lang_tchar_pt ptr;
 	mk_lang_types_sint_t rem;
-	mk_sl_stopwatch_duration_t time_diff_dur;
-	mk_lang_types_ulong_t time_treshold_ul;
-	mk_sl_stopwatch_duration_t time_treshold_dur;
-	mk_lang_types_bool_t want;
 	mk_lang_types_uint_t kilobyte_ui;
 	mk_sl_fxp_128_64_64_t kilobyte_fxp;
 	mk_lang_types_uint_t speed_treshold_ui;
@@ -145,6 +141,7 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_sl_speedometer_p
 	mk_lang_types_sint_t n;
 	mk_lang_types_sint_t i;
 	mk_sl_fxp_128_64_64_t amount_fxp;
+	mk_sl_stopwatch_duration_t time_diff_dur;
 	mk_sl_fxp_128_64_64_t time_diff_fxp_nanos;
 	mk_lang_types_ulong_t second_ul;
 	mk_sl_fxp_128_64_64_t second_fxp;
@@ -164,56 +161,78 @@ mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_sl_speedometer_p
 	mk_lang_assert(reported);
 	mk_lang_assert(out_len);
 
-	*reported = mk_lang_false;
 	ptr = str_buf;
 	rem = str_len;
+	speedometer->m_lat_reported = speedometer->m_lat_append;
+	++speedometer->m_tick;
+	kilobyte_ui = 1ul * 1024ul; mk_sl_fxp_128_64_64_from_bi_uint(&kilobyte_fxp, &kilobyte_ui);
+	speed_treshold_ui = 999u; mk_sl_fxp_128_64_64_from_bi_uint(&speed_treshold_fxp, &speed_treshold_ui);
+	amount_cui = speedometer->m_amounts[(speedometer->m_idx + 0) % mk_lang_countof(speedometer->m_amounts)];
+	n = mk_lang_countof(speedometer->m_amounts);
+	for(i = 1; i != n; ++i)
+	{
+		mk_sl_cui_uint64_add2_wrap_cid_cod(&amount_cui, &speedometer->m_amounts[(speedometer->m_idx + i) % mk_lang_countof(speedometer->m_amounts)]);
+	}
+	mk_sl_fxp_128_64_64_from_uint64(&amount_fxp, &amount_cui);
+	mk_sl_stopwatch_timestamp_get_duration(&speedometer->m_timestamps[(speedometer->m_idx + 1) % mk_lang_countof(speedometer->m_timestamps)], &speedometer->m_lat_append, &time_diff_dur);
+	mk_sl_speedometer_pr_fxp_128_64_64_from_duration(&time_diff_fxp_nanos, &time_diff_dur);
+	second_ul = 1ul * 1000ul * 1000ul * 1000ul; mk_sl_fxp_128_64_64_from_bi_ulong(&second_fxp, &second_ul);
+	mk_sl_fxp_128_64_64_div3_wrap(&time_diff_fxp_nanos, &second_fxp, &time_diff_fxp_seconds);
+	mk_sl_fxp_128_64_64_div3_wrap(&amount_fxp, &time_diff_fxp_seconds, &speed_fxp);
+	speedometer->m_scale = ((mk_lang_types_uchar_t)(mk_sl_speedometer_scale_e_b));
+	while(mk_sl_fxp_128_64_64_gt(&speed_fxp, &speed_treshold_fxp) && ((mk_sl_speedometer_scale_t)(speedometer->m_scale)) != mk_sl_speedometer_scale_e_tb)
+	{
+		mk_sl_fxp_128_64_64_div2_wrap(&speed_fxp, &kilobyte_fxp);
+		speedometer->m_scale = ((mk_lang_types_uchar_t)(((mk_sl_speedometer_scale_t)(((mk_lang_types_sint_t)(speedometer->m_scale)) + 1))));
+	}
+	len = mk_sl_fxp_128_64_64_to_string_t(&speed_fxp, ptr, mk_lang_min(5, rem)); if(!(len >= 1)){ return 0; } mk_lang_assert(len >= 1); mk_lang_assert(len <= mk_lang_min(5, rem)); ptr += len; rem -= len;
+	unit_ptr = mk_lang_null; unit_len = 0;
+	switch(((mk_sl_speedometer_scale_t)(speedometer->m_scale)))
+	{
+		case mk_sl_speedometer_scale_e_b : unit_ptr = mk_lang_tchar_c("B/s" ); unit_len = 3; break;
+		case mk_sl_speedometer_scale_e_kb: unit_ptr = mk_lang_tchar_c("kB/s"); unit_len = 4; break;
+		case mk_sl_speedometer_scale_e_mb: unit_ptr = mk_lang_tchar_c("MB/s"); unit_len = 4; break;
+		case mk_sl_speedometer_scale_e_gb: unit_ptr = mk_lang_tchar_c("GB/s"); unit_len = 4; break;
+		case mk_sl_speedometer_scale_e_tb: unit_ptr = mk_lang_tchar_c("TB/s"); unit_len = 4; break;
+		case mk_sl_speedometer_scale_e_dummy_end: mk_lang_assert_false(); break;
+		default: mk_lang_assert_false(); break;
+	}
+	if(!(rem >= 1 + unit_len)){ return 0; }
+	ptr[0] = mk_lang_tchar_c(' '); ptr += 1; rem -= 1;
+	mk_lang_tchar_memcpy_fn(ptr, unit_ptr, ((mk_lang_types_usize_t)(unit_len))); ptr += unit_len; rem -= unit_len;
+	if(!(rem >= 2)){ return 0; }
+	ptr[0] = mk_lang_tchar_c(' '); ptr += 1; rem -= 1;
+	ptr[0] = mk_lang_tchar_c("|/-\\")[speedometer->m_tick % 4]; ptr += 1; rem -= 1;
+	len = str_len - rem;
+	*out_len = len;
+	*reported = mk_lang_true;
+	return 0;
+}
+
+mk_lang_nodiscard static mk_lang_inline mk_lang_types_sint_t mk_sl_speedometer_pr_report(mk_sl_speedometer_pt const speedometer, mk_lang_tchar_pt const str_buf, mk_lang_types_sint_t const str_len, mk_lang_types_bool_pt const reported, mk_lang_types_sint_pt const out_len) mk_lang_noexcept
+{
+	mk_sl_stopwatch_duration_t time_diff_dur;
+	mk_lang_types_ulong_t time_treshold_ul;
+	mk_sl_stopwatch_duration_t time_treshold_dur;
+	mk_lang_types_bool_t want;
+	mk_lang_types_sint_t err;
+
+	mk_lang_static_assert(mk_lang_countof(speedometer->m_amounts) == mk_lang_countof(speedometer->m_timestamps));
+	mk_lang_static_assert(mk_lang_countof(speedometer->m_amounts) >= 2);
+	mk_lang_static_assert(mk_lang_countof(speedometer->m_amounts) <= 16);
+
+	mk_lang_assert(speedometer);
+	mk_lang_assert(str_buf || str_len == 0);
+	mk_lang_assert(str_len >= 0);
+	mk_lang_assert(reported);
+	mk_lang_assert(out_len);
+
 	mk_sl_stopwatch_timestamp_get_duration(&speedometer->m_lat_reported, &speedometer->m_lat_append, &time_diff_dur);
 	time_treshold_ul = 333ul * 1000ul * 1000ul; mk_sl_stopwatch_duration_from_bi_ulong(&time_treshold_dur, &time_treshold_ul);
 	want = mk_sl_stopwatch_duration_gt(&time_diff_dur, &time_treshold_dur);
 	if(want)
 	{
-		speedometer->m_lat_reported = speedometer->m_lat_append;
-		++speedometer->m_tick;
-		kilobyte_ui = 1ul * 1024ul; mk_sl_fxp_128_64_64_from_bi_uint(&kilobyte_fxp, &kilobyte_ui);
-		speed_treshold_ui = 999u; mk_sl_fxp_128_64_64_from_bi_uint(&speed_treshold_fxp, &speed_treshold_ui);
-		amount_cui = speedometer->m_amounts[(speedometer->m_idx + 0) % mk_lang_countof(speedometer->m_amounts)];
-		n = mk_lang_countof(speedometer->m_amounts);
-		for(i = 1; i != n; ++i)
-		{
-			mk_sl_cui_uint64_add2_wrap_cid_cod(&amount_cui, &speedometer->m_amounts[(speedometer->m_idx + i) % mk_lang_countof(speedometer->m_amounts)]);
-		}
-		mk_sl_fxp_128_64_64_from_uint64(&amount_fxp, &amount_cui);
-		mk_sl_stopwatch_timestamp_get_duration(&speedometer->m_timestamps[(speedometer->m_idx + 1) % mk_lang_countof(speedometer->m_timestamps)], &speedometer->m_lat_append, &time_diff_dur);
-		mk_sl_speedometer_pr_fxp_128_64_64_from_duration(&time_diff_fxp_nanos, &time_diff_dur);
-		second_ul = 1ul * 1000ul * 1000ul * 1000ul; mk_sl_fxp_128_64_64_from_bi_ulong(&second_fxp, &second_ul);
-		mk_sl_fxp_128_64_64_div3_wrap(&time_diff_fxp_nanos, &second_fxp, &time_diff_fxp_seconds);
-		mk_sl_fxp_128_64_64_div3_wrap(&amount_fxp, &time_diff_fxp_seconds, &speed_fxp);
-		speedometer->m_scale = ((mk_lang_types_uchar_t)(mk_sl_speedometer_scale_e_b));
-		while(mk_sl_fxp_128_64_64_gt(&speed_fxp, &speed_treshold_fxp) && ((mk_sl_speedometer_scale_t)(speedometer->m_scale)) != mk_sl_speedometer_scale_e_tb)
-		{
-			mk_sl_fxp_128_64_64_div2_wrap(&speed_fxp, &kilobyte_fxp);
-			speedometer->m_scale = ((mk_lang_types_uchar_t)(((mk_sl_speedometer_scale_t)(((mk_lang_types_sint_t)(speedometer->m_scale)) + 1))));
-		}
-		len = mk_sl_fxp_128_64_64_to_string_t(&speed_fxp, ptr, mk_lang_min(5, rem)); if(!(len >= 1)){ return 0; } mk_lang_assert(len >= 1); mk_lang_assert(len <= mk_lang_min(5, rem)); ptr += len; rem -= len;
-		unit_ptr = mk_lang_null; unit_len = 0;
-		switch(((mk_sl_speedometer_scale_t)(speedometer->m_scale)))
-		{
-			case mk_sl_speedometer_scale_e_b : unit_ptr = mk_lang_tchar_c("B/s" ); unit_len = 3; break;
-			case mk_sl_speedometer_scale_e_kb: unit_ptr = mk_lang_tchar_c("kB/s"); unit_len = 4; break;
-			case mk_sl_speedometer_scale_e_mb: unit_ptr = mk_lang_tchar_c("MB/s"); unit_len = 4; break;
-			case mk_sl_speedometer_scale_e_gb: unit_ptr = mk_lang_tchar_c("GB/s"); unit_len = 4; break;
-			case mk_sl_speedometer_scale_e_tb: unit_ptr = mk_lang_tchar_c("TB/s"); unit_len = 4; break;
-			case mk_sl_speedometer_scale_e_dummy_end: mk_lang_assert_false(); break;
-			default: mk_lang_assert_false(); break;
-		}
-		if(!(rem >= 1 + unit_len)){ return 0; }
-		ptr[0] = mk_lang_tchar_c(' '); ptr += 1; rem -= 1;
-		mk_lang_tchar_memcpy_fn(ptr, unit_ptr, ((mk_lang_types_usize_t)(unit_len))); ptr += unit_len; rem -= unit_len;
-		if(!(rem >= 2)){ return 0; }
-		ptr[0] = mk_lang_tchar_c(' '); ptr += 1; rem -= 1;
-		ptr[0] = mk_lang_tchar_c("|/-\\")[speedometer->m_tick % 4]; ptr += 1; rem -= 1;
-		len = str_len - rem;
-		*out_len = len;
+		err = mk_sl_speedometer_pr_report_impl(speedometer, str_buf, str_len, reported, out_len); mk_lang_check_rereturn(err);
 	}
 	*reported = want;
 	return 0;
