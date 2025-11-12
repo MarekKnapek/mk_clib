3008eae39d53729876ece5e2a7bbdd94ebd8c482
Marek Knápek <knapek.mar@gmail.com>
2025-11-12 14:25:34 +0100

    warning C4865

---
+++ mk_clib/src/mk_lang_warning_msvc_push_c4865.h
@@ -0,0 +1,7 @@
+#include "mk_lang_warning_msvc_push_c5031.h"
+#if defined _MSC_VER && defined _MSC_FULL_VER
+#pragma warning(push)
+#if _MSC_VER >= 1950
+#pragma warning(disable:4865) /* warning C4865: 'xxx': the underlying type will change from 'int' to 'unsigned int' when '/Zc:enumTypes' is specified on the command line */
+#endif
+#endif
